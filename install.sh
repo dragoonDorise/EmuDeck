bec5df1a090e0057200cd8d2e2bb403b7f66ba2e
Rodrigo <rodrigosedano@me.com>
2022-04-03 19:07:17 +0200

    = fixes

--- install.sh
+++ install.sh
@@ -19,8 +19,7 @@ zenity --question \
 	   --cancel-label="Internal Storage" \
 	   --text="${text}" &>> /dev/null
 ans=$?
-if [ $ans -eq 0 ]
-then
+if [ $ans -eq 0 ]; then
 	echo "Storage: SD" &>> ~/emudek.log
 	destination="SD"
 else
@@ -117,7 +116,7 @@ fi
 FILE=$toolsPath/EmulationStation-DE-x64_SteamDeck.AppImage
 if [ -f "$FILE" ]; then
 	echo "" &>> /dev/null
-	$doESDE == true
+	$doESDE=true
 else
 	text="Do you want to install <span weight=\"bold\" foreground=\"red\">EmulationStation DE</span>?"
 	zenity --question \
@@ -127,8 +126,7 @@ else
 	   	--cancel-label="No" \
 	   	--text="${text}" &>> /dev/null
 	ans=$?
-	if [ $ans -eq 0 ]
-	then
+	if [ $ans -eq 0 ]; then
 		$doESDE=true
 		echo "ESDE: Yes" &>> ~/emudek.log
 		echo -e "${BOLD}Installing EmulationStation Desktop Edition${NONE}"
@@ -166,7 +164,6 @@ flatpak install flathub org.yuzu_emu.yuzu -y &>> ~/emudek.log
 echo -e "Installing Flatseal (RPCS3 FIX)"
 flatpak install flathub com.github.tchx84.Flatseal -y &>> ~/emudek.log
 
-
 #Cemu
 echo -e "Installing Cemu"
 if [ $destination == "SD" ]; then
@@ -185,8 +182,6 @@ else
 	rm -rf $romsPath/wiiu/tmp &>> ~/emudek.log
 	rm -f $romsPath/wiiu/cemu_1.26.2.zip &>> ~/emudek.log
 fi
-
-
 
 echo -e ""
 
@@ -237,7 +232,7 @@ if [ $doRA == true ]; then
 	raUrl="https://buildbot.libretro.com/nightly/linux/x86_64/latest/"
 	raCorePath=""
 	RAcores=(bsnes_hd_beta_libretro.so flycast_libretro.so gambatte_libretro.so genesis_plus_gx_libretro.so genesis_plus_gx_wide_libretro.so mednafen_lynx_libretro.so mednafen_ngp_libretro.so mednafen_wswan_libretro.so melonds_libretro.so mesen_libretro.so mgba_libretro.so mupen64plus_next_libretro.so nestopia_libretro.so picodrive_libretro.so ppsspp_libretro.so snes9x_libretro.so stella_libretro.so yabasanshiro_libretro.so yabause_libretro.so yabause_libretro.so mame2003_plus_libretro.so melonds_libretro.so fbneo_libretro.so bluemsx_libretro.so desmume_libretro.so sameboy_libretro.so gearsystem_libretro.so mednafen_saturn_libretro.so)
-	echo -e "${BOLD}Downloading RetroArch Cores for Emudek${NONE}"
+	echo -e "${BOLD}Downloading RetroArch Cores for EmuDeck${NONE}"
 	for i in "${RAcores[@]}"
 	do
 		FILE=/home/deck/.var/app/org.libretro.RetroArch/config/retroarch/cores/${i}
@@ -249,7 +244,7 @@ if [ $doRA == true ]; then
 			echo -e "${i}...${GREEN}Downloaded!${NONE}"	
 		fi
 	done
-	
+	echo $doESDE;
 	if [ $doESDE == true ]; then
 		RAcores=(81_libretro.so atari800_libretro.so bluemsx_libretro.so chailove_libretro.so fbneo_libretro.so freechaf_libretro.so freeintv_libretro.so fuse_libretro.so gearsystem_libretro.so gw_libretro.so hatari_libretro.so lutro_libretro.so mednafen_pcfx_libretro.so mednafen_vb_libretro.so mednafen_wswan_libretro.so mu_libretro.so neocd_libretro.so nestopia_libretro.so nxengine_libretro.so o2em_libretro.so picodrive_libretro.so pokemini_libretro.so prboom_libretro.so prosystem_libretro.so px68k_libretro.so quasi88_libretro.so scummvm_libretro.so squirreljme_libretro.so theodore_libretro.so uzem_libretro.so vecx_libretro.so vice_xvic_libretro.so virtualjaguar_libretro.so x1_libretro.so mednafen_lynx_libretro.so mednafen_ngp_libretro.so mednafen_pce_libretro.so mednafen_pce_fast_libretro.so mednafen_psx_libretro.so mednafen_psx_hw_libretro.so mednafen_saturn_libretro.so mednafen_supafaust_libretro.so mednafen_supergrafx_libretro.so blastem_libretro.so bluemsx_libretro.so bsnes_libretro.so bsnes_mercury_accuracy_libretro.so cap32_libretro.so citra2018_libretro.so citra_libretro.so crocods_libretro.so desmume2015_libretro.so desmume_libretro.so dolphin_libretro.so dosbox_core_libretro.so dosbox_pure_libretro.so dosbox_svn_libretro.so fbalpha2012_cps1_libretro.so fbalpha2012_cps2_libretro.so fbalpha2012_cps3_libretro.so fbalpha2012_libretro.so fbalpha2012_neogeo_libretro.so fceumm_libretro.so fbneo_libretro.so flycast_libretro.so fmsx_libretro.so frodo_libretro.so gambatte_libretro.so gearboy_libretro.so gearsystem_libretro.so genesis_plus_gx_libretro.so genesis_plus_gx_wide_libretro.so gpsp_libretro.so handy_libretro.so kronos_libretro.so mame2000_libretro.so mame2003_plus_libretro.so mame2010_libretro.so mame_libretro.so melonds_libretro.so mesen_libretro.so mesen-s_libretro.so mgba_libretro.so mupen64plus_next_libretro.so nekop2_libretro.so np2kai_libretro.so nestopia_libretro.so parallel_n64_libretro.so pcsx2_libretro.so pcsx_rearmed_libretro.so picodrive_libretro.so ppsspp_libretro.so puae_libretro.so quicknes_libretro.so race_libretro.so sameboy_libretro.so smsplus_libretro.so snes9x2010_libretro.so snes9x_libretro.so stella2014_libretro.so stella_libretro.so tgbdual_libretro.so vbam_libretro.so vba_next_libretro.so vice_x128_libretro.so vice_x64_libretro.so vice_x64sc_libretro.so vice_xscpu64_libretro.so yabasanshiro_libretro.so yabause_libretro.so bsnes_hd_beta_libretro.so swanstation_libretro.so)
 		echo -e "${BOLD}Downloading RetroArch Cores for EmulationStation DE${NONE}"
