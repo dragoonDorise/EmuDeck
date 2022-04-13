001f9ab73785fabfbcc9e45ebcba009556b34e9e
godsbane <godsbane@gmail.com>
2022-04-13 02:41:16 -0400

    clobbered the sed s|. oops.

--- install.sh
+++ install.sh
@@ -307,9 +307,9 @@ mkdir -p ~/.config/steam-rom-manager/userData/
 cp ~/dragoonDoriseTools/EmuDeck/configs/steam-rom-manager/userData/userConfigurations.json ~/.config/steam-rom-manager/userData/userConfigurations.json
 sleep 3
 
-sed -i "/run/media/mmcblk0p1/Emulation/roms/|${romsPath}|g" ~/.config/steam-rom-manager/userData/userConfigurations.json
+sed -i "s|/run/media/mmcblk0p1/Emulation/roms/|${romsPath}|g" ~/.config/steam-rom-manager/userData/userConfigurations.json
 
-sed -i "/run/media/mmcblk0p1/Emulation/tools/|${toolsPath}|g" ~/.config/steam-rom-manager/userData/userConfigurations.json
+sed -i "s|/run/media/mmcblk0p1/Emulation/tools/|${toolsPath}|g" ~/.config/steam-rom-manager/userData/userConfigurations.json
 
 echo -e "${GREEN}OK!${NONE}"
 
