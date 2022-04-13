6ed0684d6aac178b88a503cfc8652df20fed0796
godsbane <godsbane@gmail.com>
2022-04-12 22:50:05 -0400

    Fix SD Card detection and references to mmcblk0p1

--- install.sh
+++ install.sh
@@ -88,27 +88,32 @@ toolsPathSed="\/home\/deck\/Emulation\/tools\/"
 romsPathSed="\/home\/deck\/Emulation\/roms\/"
 biosPath="/home/deck/Emulation/bios/"
 biosPathSed="\/home\/deck\/Emulation\/bios\/"
-if [ $destination == "SD" ]; then
-	#Get SD Card name
-	sdCard=$(ls /run/media | grep -ve '^deck$' | head -n1)
-	emulationPath="/run/media/${sdCard}/Emulation/"
-	romsPath="/run/media/${sdCard}/Emulation/roms/"
-	toolsPath="/run/media/${sdCard}/Emulation/tools/"
-	toolsPathSed="\/run\/media\/${sdCard}\/Emulation\/tools\/"
-	romsPathSed="\/run\/media\/${sdCard}\/Emulation\/roms\/"
-	biosPath="/run/media/${sdCard}/Emulation/bios/"
-	biosPathSed="\/run\/media\/${sdCard}\/Emulation\/bios\/"
-	
 
-	if [ $sdCard != "mmcblk0p1" ]; then		
+if [ $destination == "SD" ]; then
+	#check if sd card exists
+	if [ -b "/dev/mmcblk0p1" ]; then
+		#Get SD Card name. use findmnt to explicitly find the first sd card
+    	sdCardFull=$(findmnt -n --raw --evaluate --output=target -S /dev/mmcblk0p1)
+		echo "SD Card found; installing to /dev/mmcblk0p1 mounted on $sdCardFull"
+		sdCard=$(ls /run/media | grep -ve '^deck$' | head -n1)
+	else
 		text="You need to format your SD Card using Steam UI.<br>EmuDeck wont work if your SD card is not in ext4 format<br>Please come back when your SD Card is ready"
 		zenity --error \
 			   --title="EmuDeck ERROR" \
 			   --width=250 \	   
 			   --text="${text}" &>> /dev/null
-		exit	   	
-	fi	
+		exit
+	fi
+
+	emulationPath="${sdCardFull}/Emulation/"
+	romsPath="${sdCardFull}/Emulation/roms/"
+	toolsPath="${sdCardFull}/Emulation/tools/"
+	toolsPathSed="\/run\/media\/${sdCard}\/Emulation\/tools\/"
+	romsPathSed="\/run\/media\/${sdCard}\/Emulation\/roms\/"
+	biosPath="${sdCardFull}/Emulation/bios/"
+	biosPathSed="\/run\/media\/${sdCard}\/Emulation\/bios\/"
 	
+
 fi
 
 mkdir -p $emulationPath
@@ -309,16 +314,16 @@ mkdir -p ~/.config/steam-rom-manager/userData/
 cp ~/dragoonDoriseTools/EmuDeck/configs/steam-rom-manager/userData/userConfigurations.json ~/.config/steam-rom-manager/userData/userConfigurations.json
 sleep 3
 
-sed -i "s/\/run\/media\/mmcblk0p1\/Emulation\/roms\//${romsPathSed}/g" ~/.config/steam-rom-manager/userData/userConfigurations.json
+sed -i "s/\/run\/media\/${sdCard}\/Emulation\/roms\//${romsPathSed}/g" ~/.config/steam-rom-manager/userData/userConfigurations.json
 
-sed -i "s/\/run\/media\/mmcblk0p1\/Emulation\/tools\//${toolsPathSed}/g" ~/.config/steam-rom-manager/userData/userConfigurations.json
+sed -i "s/\/run\/media\/${sdCard}\/Emulation\/tools\//${toolsPathSed}/g" ~/.config/steam-rom-manager/userData/userConfigurations.json
 
 echo -e "${GREEN}OK!${NONE}"
 
 echo -ne "${BOLD}Configuring EmulationStation DE...${NONE}"
 mkdir -p ~/.emulationstation/
 cp ~/dragoonDoriseTools/EmuDeck/configs/emulationstation/es_settings.xml ~/.emulationstation/es_settings.xml
-sed -i "s/\/run\/media\/mmcblk0p1\/Emulation\/roms\//${romsPathSed}/g" ~/.emulationstation/es_settings.xml
+sed -i "s/\/run\/media\/${sdCard}\/Emulation\/roms\//${romsPathSed}/g" ~/.emulationstation/es_settings.xml
 #sed -i "s/name=\"ROMDirectory\" value=\"/name=\"ROMDirectory\" value=\"${romsPathSed}/g" ~/.emulationstation/es_settings.xml
 echo -e "${GREEN}OK!${NONE}"
 
@@ -428,7 +433,7 @@ if [ $doPCSX2 == true ]; then
 	fi
 	rsync -avhp ~/dragoonDoriseTools/EmuDeck/configs/net.pcsx2.PCSX2/ ~/.var/app/net.pcsx2.PCSX2/ &>> ~/emudeck/emudeck.log
 	#Bios Fix
-	sed -i "s/\/run\/media\/mmcblk0p1\/Emulation\/bios\//${biosPathSed}/g" ~/.var/app/net.pcsx2.PCSX2/config/PCSX2/inis/PCSX2_ui.ini &>> ~/emudeck/emudeck.log
+	sed -i "s/\/run\/media\/${sdCard}\/Emulation\/bios\//${biosPathSed}/g" ~/.var/app/net.pcsx2.PCSX2/config/PCSX2/inis/PCSX2_ui.ini &>> ~/emudeck/emudeck.log
 fi
 if [ $doRPCS3 == true ]; then
 	FOLDER=~/.var/app/net.rpcs3.RPCS3/config_bak
@@ -466,7 +471,7 @@ if [ $doDuck == true ]; then
 	fi
 	rsync -avhp ~/dragoonDoriseTools/EmuDeck/configs/org.duckstation.DuckStation/ ~/.var/app/org.duckstation.DuckStation/ &>> ~/emudeck/emudeck.log
 	sleep 3
-	sed -i "s/\/run\/media\/mmcblk0p1\/Emulation\/bios\//${biosPathSed}/g" ~/.var/app/org.duckstation.DuckStation/data/duckstation/settings.ini
+	sed -i "s/\/run\/media\/${sdCard}\/Emulation\/bios\//${biosPathSed}/g" ~/.var/app/org.duckstation.DuckStation/data/duckstation/settings.ini
 fi
 if [ $doYuzu == true ]; then
 	FOLDER=~/.var/app/org.yuzu_emu.yuzu/config
