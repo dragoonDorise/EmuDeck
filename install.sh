39070eca9702b5b54337d2d86a3a18a58b4ec7d3
godsbane <godsbane@gmail.com>
2022-05-04 00:24:19 -0400

    fix double slashes

--- install.sh
+++ install.sh
@@ -1413,24 +1413,24 @@ fi
 #fi
 
 if [ $doInstallCHD == true ]; then
-	mkdir -p  $toolsPath/chdconv/
-	rsync -avhp ~/dragoonDoriseTools/chdconv/ $toolsPath/chdconv/ &>> ~/emudeck/emudeck.log
+	mkdir -p  "$toolsPath"chdconv/
+	rsync -avhp ~/dragoonDoriseTools/chdconv/ "$toolsPath"chdconv/ &>> ~/emudeck/emudeck.log
 	
 	rm -rf ~/Desktop/EmuDeckCHD.desktop &>> /dev/null
 	echo "#!/usr/bin/env xdg-open
 	[Desktop Entry]
 	Name=EmuDeck CHD Script
-	Exec=bash $toolsPath/chdconv/chddeck.sh
+	Exec=bash "$toolsPath"chdconv/chddeck.sh
 	Icon=steamdeck-gaming-return
 	Terminal=true
 	Type=Application
 	StartupNotify=false" > ~/Desktop/EmuDeckCHD.desktop
 	chmod +x ~/Desktop/EmuDeckCHD.desktop	
-	chmod +x $toolsPath/chdconv/chddesk.sh
-	chmod +x $toolsPath/chdconv/chdman5
+	chmod +x "$toolsPath"chdconv/chddesk.sh
+	chmod +x "$toolsPath"chdconv/chdman5
 	#update the paths in the script
-	sed -i "s|/run/media/mmcblk0p1/Emulation/roms/|${romsPath}|g" $toolsPath/chdconv/chddesk.sh
-	sed -i "s|/run/media/mmcblk0p1/Emulation/tools/|${toolsPath}|g" $toolsPath/chdconv/chddesk.sh
+	sed -i "s|/run/media/mmcblk0p1/Emulation/roms/|${romsPath}|g" "$toolsPath"chdconv/chddesk.sh
+	sed -i "s|/run/media/mmcblk0p1/Emulation/tools/|${toolsPath}|g" "$toolsPath"chdconv/chddesk.sh
 
 fi
 
