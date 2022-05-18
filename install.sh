9a7e6641b71aba9e73c16e9682c4ef320d1658ab
Godsbane <godsbane@gmail.com>
2022-05-18 10:06:11 -0400

    Fix Gamelist Entries via function

--- install.sh
+++ install.sh
@@ -111,10 +111,15 @@ setESDEEmus(){
 	if [ ! -f "$FILE" ]; then
 		mkdir -p ~/.emulationstation/gamelists/$system && cp ~/dragoonDoriseTools/EmuDeck/configs/emulationstation/gamelists/$system/gamelist.xml $FILE
 	else
+		gamelistFound=$(grep -rnw $FILE -e 'gameList')
+		if [[ gamelistFound == '' ]]; then
+			echo "<gameList />" >> $FILE
+		fi
 		alternativeEmu=$(grep -rnw $FILE -e 'alternativeEmulator')
 		if [[ $alternativeEmu == '' ]]; then
 			echo "<alternativeEmulator><label>$emu</label></alternativeEmulator>" >> $FILE
 		fi
+		sed -i "s|<?xml version=\"1.0\">|<?xml version=\"1.0\">|g" $FILE
 	fi
 }
 testLocationValid(){
