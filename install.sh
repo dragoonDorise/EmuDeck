5add09659b502cac1f512a71608eb526ce37b7b0
Ramsey Hathout <godsbane@gmail.com>
2022-05-19 16:33:35 -0400

    Gamelist hotfix (#204)
    * Fix Gamelist.XML files for ones added later.
    
    * Fix Gamelist Entries via function
    
    * sed gamelist into xml missing the line
    
    * Fix gamelistFound variable

--- install.sh
+++ install.sh
@@ -109,10 +109,15 @@ setESDEEmus(){
 	if [ ! -f "$FILE" ]; then
 		mkdir -p ~/.emulationstation/gamelists/$system && cp ~/dragoonDoriseTools/EmuDeck/configs/emulationstation/gamelists/$system/gamelist.xml $FILE
 	else
+		gamelistFound=$(grep -rnw $FILE -e 'gameList')
+		if [[ $gamelistFound == '' ]]; then
+		    sed -i -e '$a\<gameList />' $FILE
+		fi
 		alternativeEmu=$(grep -rnw $FILE -e 'alternativeEmulator')
 		if [[ $alternativeEmu == '' ]]; then
 			echo "<alternativeEmulator><label>$emu</label></alternativeEmulator>" >> $FILE
 		fi
+		sed -i "s|<?xml version=\"1.0\">|<?xml version=\"1.0\"?>|g" $FILE
 	fi
 }
 
