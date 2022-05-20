3d815724a8a85162767bb4d1276024e4a161992c
Erik-Handeland <erikhandeland@gmail.com>
2022-05-20 15:39:42 -0700

    Update steamCloudSaves.sh

--- tools/steamCloudSaves.sh
+++ tools/steamCloudSaves.sh
@@ -2,14 +2,14 @@
 
 # Why?
 # Steam RetroArch allows you to save your game states and saves with steam cloud saves which is nice when 
-# switching between your steamdeck and desktop. Without having to deal with google drive or dropbox.
-# You can also gain acess to steam only features like online coop and remote play.
+# switching between your steam deck and desktop. Without having to deal with google drive or dropbox.
+# You can also gain access to steam only features like online coop and remote play.
 
 # Requirements:
-# RetroArch must be installed on SD card and not internal storage 
+# RetroArch must be installed on an SD card and not on internal storage 
 # EmuDeck must be installed on SD card as well
 # make sure you copy over any saves or states in the Flatpack version of RetroArch to the steam version 
-# as any current saves will be over written with that of the steam verstion
+# as any current saves will be overwritten with that of the steam version
 
 # steps:
 # Install RetroArch
@@ -17,25 +17,25 @@
 # Run this script
 
 
-# What will happend:
+# What will happen:
 # Will create symbolic links between Flatpak RetroArch and Steam RetroArch
 # Current folders shared:
-    # cheats (From Steam)
-    # config (From Emu)
-    # cores  (From Emu)
-    # filters (From Steam)
-    # playlist (From Steam)
-    # saves (From Steam)
-    # screenshots (From Steam)
-    # states (From Steam)
-    # thumbnails (From Steam)
+ # cheats (From Steam)
+ # config (From Emu)
+ # cores (From Emu)
+ # filters (From Steam)
+ # playlist (From Steam)
+ # saves (From Steam)
+ # screenshots (From Steam)
+ # states (From Steam)
+ # thumbnails (From Steam)
 # ln -s /<path to file/folder to be linked> <path of the link to be created>
 
-# After running I recomend open EmuDeck steam in scanning the emulators forlder
-# Next you can Go to main menu -> online updater and download all of thumbnails for you roms
-# Now if you launce Steam Retroarch and load in your roms all the thumbnails should also be installed
+# After running I recommend opening EmuDeck steam in scanning the emulators folder
+# Next you can Go to main menu -> online updater and download all of the thumbnails for you roms
+# Now if you launch Steam Retroarch and load in your roms all the thumbnails should also be installed
 
-# TODO does emudeck actullt install to SSD instead of SD card?
+# TODO Could add option for other install locations
 readonly STEAM=/run/media/mmcblk0p1/steamapps/common/RetroArch
 readonly EMUDECK=/home/deck/.var/app/org.libretro.RetroArch/config/retroarch
 
