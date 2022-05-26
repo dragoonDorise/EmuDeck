a57312f76789322e35596de170b3d50c8d48c863
Godsbane <godsbane@gmail.com>
2022-05-26 00:12:56 -0400

    Add confirmation on return to game mode

--- install.sh
+++ install.sh
@@ -1808,17 +1808,26 @@ finished=true
 text="`printf "<b>Done!</b>\n\nRemember to add your games here:\n<b>${romsPath}</b>\nAnd your Bios (PS1, PS2, Yuzu) here:\n<b>${biosPath}</b>\n\nOpen Steam Rom Manager on your Desktop to add your games to your SteamUI Interface.\n\nThere is a bug in RetroArch that if you are using Bezels you can not set save configuration files unless you close your current game. Use overrides for your custom configurations or use expert mode to disabled them\n\nIf you encounter any problem please visit our Discord:\n<b>https://discord.gg/b9F7GpXtFP</b>\n\nTo Update EmuDeck in the future, just run this App again.\n\nEnjoy!"`"
 
 zenity --question \
-		 --title="EmuDeck" \
-		 --width=450 \
-		 --ok-label="Open Steam Rom Manager" \
-		 --cancel-label="Exit" \
-		 --text="${text}" 2>/dev/null
+         --title="EmuDeck" \
+         --width=450 \
+         --ok-label="Open Steam Rom Manager" \
+         --cancel-label="Exit" \
+         --text="${text}" 2>/dev/null
 ans=$?
 if [ $ans -eq 0 ]; then
-	kill -9 `pidof steam`
+	kill -15 `pidof steam`
 	cd ${toolsPath}/srm
 	./Steam-ROM-Manager.AppImage
-	qdbus org.kde.Shutdown /Shutdown org.kde.Shutdown.logout
+	zenity --question \
+		 --title="EmuDeck" \
+		 --width=350 \
+		 --text="Return to Game Mode?" \
+		 --ok-label="Yes" \
+		 --cancel-label="No" 2>/dev/null
+	ans2=$?
+	if [ $ans2 -eq 0 ]; then
+		qdbus org.kde.Shutdown /Shutdown org.kde.Shutdown.logout
+	fi
 	exit
 else
 	exit
