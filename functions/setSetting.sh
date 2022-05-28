c30a2de12c7f970f56b6fc8da68cdc17faaeadd3
Rodrigo <rodrigosedano@me.com>
2022-05-29 01:13:58 +0200

    Fix setSettings

--- functions/setSetting.sh
+++ functions/setSetting.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 setSetting () {
 	var=$1
-	new_val=$2
-	file=~/emudeck/settings.sh
-	sed -i "s/^$var *= *.*/$var = $new_val/; s/^$var [^=]*$/$var $new_val/" "$file" | grep $var 
+	new_val=$2	
+	file=~/emudeck/settings.sh	
+	sed -i "s|^$var *= *.*|$var=$new_val|; s|^$var [^=]*$|$var $new_val|" "$file"
+	#Update values
+	source ~/emudeck/settings.sh
 }
