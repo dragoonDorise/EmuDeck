ba6ae6cde91a20b1ed1325661491f167d2c4365d
Rodrigo <rodrigosedano@me.com>
2022-05-28 22:52:27 +0200

    settings.sh

---
+++ settings.sh
@@ -0,0 +1,63 @@
+
+#Expert mode off by default
+expert=false
+
+#Update all systems by default
+doUpdateRA=true
+doUpdateDolphin=true
+doUpdatePCSX2=true
+doUpdateRPCS3=true
+doUpdateYuzu=true
+doUpdateCitra=true
+doUpdateDuck=true
+doUpdateCemu=true
+doUpdateXenia=false
+doUpdateRyujinx=true
+doUpdatePrimeHacks=true
+doUpdatePPSSPP=true
+doUpdateXemu=true
+doUpdateSRM=true
+#doUpdateMelon=true
+
+#Install all systems by default
+doInstallSRM=true
+doInstallESDE=true
+doInstallRA=false
+doInstallDolphin=false
+doInstallPCSX2=false
+doInstallRPCS3=false
+doInstallYuzu=false
+doInstallCitra=false
+doInstallDuck=false
+doInstallCemu=false
+doInstallXenia=false
+doInstallPrimeHacks=false
+doInstallPPSSPP=false
+doInstallXemu=false
+#doInstallMelon=false
+doInstallCHD=false
+doInstallPowertools=false
+doInstallGyro=false
+installString='Installing'
+
+#Default RetroArch configuration 
+RABezels=true
+RAautoSave=false
+SNESAR=43
+
+#Default widescreen
+duckWide=true
+DolphinWide=true
+DreamcastWide=true
+BeetleWide=true
+
+#Default installation folders
+emulationPath=~/Emulation/
+romsPath=~/Emulation/roms/
+toolsPath=~/Emulation/tools/
+biosPath=~/Emulation/bios/
+savesPath=~/Emulation/saves/
+
+#Default ESDE Theme
+esdeTheme="EPICNOIR"
+
