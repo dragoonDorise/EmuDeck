6e08e9bae2a6782ffadf0187476e1d2fa13d075b
Godsbane <godsbane@gmail.com>
2022-06-03 02:07:37 -0400

    Only do holo warning if the user actually needs to use sudo

--- functions/setUpHolo.sh
+++ functions/setUpHolo.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 setUpHolo(){		
-	text="$(printf "Hey! This is not an SteamDeck. EmuDeck can work just fine, but you need to have a valid user account\n\nThe script will ask for your password to make sure everything works as expected.")"
-	 zenity --info \
-	--title="EmuDeck" \
-	--width=450 \
-	--text="${text}" 2>/dev/null
-	
+
 	#Ensure the dependencies are installed before proceeding.
 	for package in packagekit-qt5 flatpak rsync unzip jq
 	do
@@ -15,6 +10,11 @@ setUpHolo(){
 	#The user must be in the wheel group to install flatpaks successfully.
 	wheel=$(awk '/'${USER}'/ {if ($1 ~ /wheel/) print}' /etc/group)
 	if [[ ! "${wheel}" =~ ${USER} ]]; then
+		text="$(printf "Hey! This is not an SteamDeck. EmuDeck can work just fine, but you need to have a valid user account\n\nThe script will ask for your password to make sure everything works as expected.")"
+		zenity --info \
+		--title="EmuDeck" \
+		--width=450 \
+		--text="${text}" 2>/dev/null
 		sudo usermod -a -G wheel ${USER} 
 		newgrp wheel
 	fi
