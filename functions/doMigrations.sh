f0c60178973180377cfd6be295f238050937b1ee
Godsbane <godsbane@gmail.com>
2022-06-04 01:02:30 -0400

    replaced some ~ and /home/deck with $HOME

--- functions/doMigrations.sh
+++ functions/doMigrations.sh
@@ -10,29 +10,29 @@ doMigrations(){
 		echo -e ""
 		setMSG "Moving Xemu HDD and EEPROM to the Emulation/storage folder"			
 		echo -e ""
-		mv "$HOME/.var/app/app.xemu.xemu/data/xemu/xemu/xbox_hdd.qcow2" $storagePath/xemu 
-		mv "$HOME/.var/app/app.xemu.xemu/data/xemu/xemu/eeprom.bin" $storagePath/xemu 
+		mv "$HOME/.var/app/app.xemu.xemu/data/xemu/xemu/xbox_hdd.qcow2" $storagePath/xemu
+		mv "$HOME/.var/app/app.xemu.xemu/data/xemu/xemu/eeprom.bin" $storagePath/xemu
 
 		flatpak override app.xemu.xemu --filesystem="$storagePath"xemu:rw --user
 
-        mv -f $savesPath/xemu/* $storagePath/xemu/
+        rsync -av $savesPath/xemu $storagePath && rm -rf $savesPath/xemu/
 
-        sed -i "s|$savesPath|${storagePath}|g" ~/.var/app/app.xemu.xemu/data/xemu/xemu/xemu.toml
+        sed -i "s|$savesPath|${storagePath}|g" "$HOME/.var/app/app.xemu.xemu/data/xemu/xemu/xemu.toml"
 	fi
 
 	#RPCS3 files to storage
 	if [ -d "$savesPath/rpcs3/dev_hdd0/savedata" ] || [ -d "$HOME/.var/app/net.rpcs3.RPCS3/config/rpcs3/dev_hdd0" ]; then	
-		if [ $destination != "$home" ]; then
-			text="$(printf "Moving rpcs3 hdd0 to the Emulation/Saves folder\n\nDepending on how many pkgs you have installed, this may take a while.<b>If you do not have enough available space in your chosen location this will fail, clean up your SD Card and run EmuDeck Again.</b>")"
-		else	
-			text="$(printf "Moving rpcs3 hdd0 to the Emulation/Saves folder\n\nDepending on how many pkgs you have installed, this may take a while.")"
-		fi
-	
+
+    	echo -e ""
+		setMSG "Moving rpcs3 HDD to the Emulation/storage folder"			
+		echo -e ""
+
 		mkdir -p "$storagePath/rpcs3" 
-		rsync -av ~/.var/app/net.rpcs3.RPCS3/config/rpcs3/dev_hdd0 "$storagePath"/rpcs3/ && rm -rf ~/.var/app/net.rpcs3.RPCS3/config/rpcs3/dev_hdd0 
+		rsync -av "$HOME/.var/app/net.rpcs3.RPCS3/config/rpcs3/dev_hdd0" "$storagePath"rpcs3/ && rm -rf "$HOME/.var/app/net.rpcs3.RPCS3/config/rpcs3/dev_hdd0"
+        rsync -av "$savesPath"rpcs3/dev_hdd0 "$storagePath"rpcs3/ && rm -rf "$HOME/.var/app/net.rpcs3.RPCS3/config/rpcs3/dev_hdd0"
 		#update config file for the new loc $(emulatorDir) is in the file. made this annoying.
-		sed -i "'s|$(EmulatorDir)dev_hdd0/|'$storagePath'/rpcs3/dev_hdd0/|g'" /home/deck/.var/app/net.rpcs3.RPCS3/config/rpcs3/vfs.yml 
-        sed -i "'s|'$savesPath'/rpcs3/dev_hdd0/|'$storagePath'/rpcs3/dev_hdd0/|g'" /home/deck/.var/app/net.rpcs3.RPCS3/config/rpcs3/vfs.yml
+		sed -i "'s|$(EmulatorDir)dev_hdd0/|'$storagePath'/rpcs3/dev_hdd0/|g'" $HOME/.var/app/net.rpcs3.RPCS3/config/rpcs3/vfs.yml 
+        sed -i "'s|'$savesPath'/rpcs3/dev_hdd0/|'$storagePath'/rpcs3/dev_hdd0/|g'" $HOME/.var/app/net.rpcs3.RPCS3/config/rpcs3/vfs.yml
 	fi
 
 
