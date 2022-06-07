533b8f07ca2bc2b3ae9152b0070a9840816d7f65
Godsbane <godsbane@gmail.com>
2022-06-06 23:44:22 -0400

    make setsetting update older settings files for new variables

--- functions/setSetting.sh
+++ functions/setSetting.sh
@@ -2,8 +2,15 @@
 setSetting () {
 	var=$1
 	new_val=$2	
-	file=~/emudeck/settings.sh	
-	sed -i "s|^$var *= *.*|$var=$new_val|; s|^$var [^=]*$|$var $new_val|" "$file"
+	file=~/emudeck/settings.sh
+	settingExists=$(grep -rnw $file -e $var)
+	if [[ $settingExists == '' ]]; then
+		#insert setting to end
+		sed -i -e '$a\'"$var=$new_val" $FILE
+	elif [[ ! $settingExists == '' ]]; then
+		#update setting
+		sed -i "s|^$var *= *.*|$var=$new_val|; s|^$var [^=]*$|$var $new_val|" "$file"
+	fi
 	#Update values
 	source ~/emudeck/settings.sh
 }
