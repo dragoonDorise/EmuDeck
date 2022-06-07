b6ecaf2d86cd492fc913180a1bea6b6d0eb5cf44
Godsbane <godsbane@gmail.com>
2022-06-07 13:08:54 -0400

    add in flag for migration completion add in question for user about migration add in flag for migration once completed or declined forever

--- functions/migrateAndLinkConfig.sh
+++ functions/migrateAndLinkConfig.sh
@@ -3,81 +3,108 @@ migrateAndLinkConfig(){
 
 emu=$1
 migrationTable=$2
-n=$(( ${#migrationTable[@]} - 1 ))
-#odd should be flatpak
-#even should be appimage
-#determine plan based on first pair
-if [[ ! -e ${migrationTable[0]} ]]; then
-#no flatpak data. nothing to do. (or should we link it?)
-echo "No flatpak data found, continuing."
-elif [[ -d ${migrationTable[0]} && ! -L ${migrationTable[0]} && -d ${migrationTable[1]}  && ! -L ${migrationTable[1]} ]]; then
-	#both locations exist as directories, flatpak not a symlink
-	#ask user which to keep
-    text="`printf "Data was found for both the appimage and flatpak for ${emu}.\nWe will be using the AppImage from now on.\nPlease choose which data to keep."`"
-    ans=$(zenity --info --title "Migrate "${emu}" Data?" \
-      --text="${text}" \
-      --width=300 \
-      --ok-label "Don't do anything with my stuff" \
-      --extra-button "Keep AppImage Data" \
-      --extra-button "Migrate Flatpak Data" 2>/dev/null)
+#step 1 should be attempt to unlink everything that gets passed in so we can start fresh.
+#for path in $migrationTable[@]
+#do
+#    if [ -L $path ]; then
+#        echo unlinking $path
+#        unlink $path
+#    fi
+#done
+migrationFlag="$HOME/emudeck/.${emu}MigrationCompleted"
+
+#check if we have a nomigrateflag for $emu
+if [ ! -f "$migrationFlag" ]; then
+    #ask user before migrating data
+    text="`printf "We would like to migrate data from the flatpak to the AppImage for ${emu}."`"
+    doMigrate=$(zenity --info --title "Migrate "${emu}" Data?" \
+        --text="${text}" \
+        --width=300 \
+        --ok-label "Leave ${emu} alone this time" \
+        --extra-button "Leave ${emu} alone forever" \
+        --extra-button "Migrate Data" 2>/dev/null)
     rc=$?
-    if [[ ! $ans == "" ]]; then #user didn't cancel
-        echo "User Chose: $ans"
-        for ((i=0; i<=n; i=(i+2))) { # for each pair of dirs
+    if [ "$doMigrate" == "Migrate Data" ]; then
+        n=$(( ${#migrationTable[@]} - 1 ))
+        #odd should be flatpak
+        #even should be appimage
+        #determine plan based on first pair
+        if [[ ! -e ${migrationTable[0]} ]]; then
+        #no flatpak data. nothing to do. (or should we link it?)
+        echo "No flatpak data found, continuing."
+        elif [[ -d ${migrationTable[0]} && -d ${migrationTable[1]} ]]; then
+            #both locations exist
+            #ask user which to keep
+            text="`printf "Data was found for both the appimage and flatpak for ${emu}.\nWe will be using the AppImage from now on.\nPlease choose which data to keep."`"
+            ans=$(zenity --info --title "Migrate "${emu}" Data?" \
+            --text="${text}" \
+            --width=300 \
+            --ok-label "Don't do anything with my stuff" \
+            --extra-button "Keep AppImage Data" \
+            --extra-button "Migrate Flatpak Data" 2>/dev/null)
+            rc=$?
+            if [[ ! $ans == "" ]]; then #user didn't cancel
+                echo "User Chose: $ans"
+                for ((i=0; i<=n; i=(i+2))) { # for each pair of dirs
 
-            if [[ $ans == "Migrate Flatpak Data" ]]; then
-                fromDir=${migrationTable[i]}
-                toDir=${migrationTable[i+1]}
-                echo  "Migrating ${fromDir} to ${toDir}"
+                    if [[ $ans == "Migrate Flatpak Data" ]]; then
+                        fromDir=${migrationTable[i]}
+                        toDir=${migrationTable[i+1]}
+                        echo  "Migrating ${fromDir} to ${toDir}"
 
-                #backup destination location, delete it, then sync original over
-                mv "$toDir" "$toDir.orig" && mkdir -p $toDir && rsync -av "${fromDir}/" "${toDir}"
-                cd ${fromDir}/..
-                #backup and remove original
-                mv "${fromDir}" "${fromDir}.orig" && rm -rf "${fromDir}"
+                        #backup destination location, delete it, then sync original over
+                        mv "$toDir" "$toDir.orig" && mkdir -p $toDir && rsync -av "${fromDir}/" "${toDir}"
+                        cd ${fromDir}/..
+                        #backup and remove original
+                        mv "${fromDir}" "${fromDir}.orig" && rm -rf "${fromDir}"
 
-                #link .config to .var so flatpak still works
-                ln -sfn ${toDir} .
+                        #link .config to .var so flatpak still works
+                        ln -sfn ${toDir} .
 
-            elif [[ $ans == "Keep AppImage Data" ]]; then
-                fromDir=${migrationTable[i+1]}
-                toDir=${migrationTable[i]}
-                cd ${toDir}/..
-                #backup flatpak data
-                mv "${toDir}" "${toDir}.orig"
-                #link appimage data to flatpak folder
-                ln -sfn "${fromDir}" .
+                    elif [[ $ans == "Keep AppImage Data" ]]; then
+                        fromDir=${migrationTable[i+1]}
+                        toDir=${migrationTable[i]}
+                        cd ${toDir}/..
+                        #backup flatpak data
+                        mv "${toDir}" "${toDir}.orig"
+                        #link appimage data to flatpak folder
+                        ln -sfn "${fromDir}" .
 
+                    else
+                        echo "Something went wrong"
+                        exit
+                    fi
+
+                }
             else
-                echo "Something went wrong"
-                exit
+                echo "User doesn't want migration at this time."
             fi
 
-        }
-    else
-        echo "User doesn't want migration."
+        elif [[ -L ${migrationTable[0]} && -d ${migrationTable[0]} && -d ${migrationTable[1]} ]]; then
+            echo "Flatpak already linked"
+        elif [[ -d ${migrationTable[0]} && ! -e ${migrationTable[1]} ]]; then
+            echo "No AppImage data found, but flatpak data found. New AppImage install."
+            for ((i=0; i<=n; i=(i+2))) { # for each pair of dirs
+
+
+                    fromDir=${migrationTable[i]}
+                    toDir=${migrationTable[i+1]}
+                    echo  "Migrating ${fromDir} to ${toDir}"
+                    cd ${fromDir}/..
+                    #backup destination location, delete it, then sync original over
+                    mkdir -p ${toDir} && rmdir ${toDir} #make the path for todir, but remove the end folder
+                    mv "${fromDir}" "${toDir}" #move the original to the new location
+                    #link .config to .var so flatpak still works
+                    ln -sfn ${toDir} .
+
+            }
+        else
+            echo "do nothing"
+        fi
+        touch $migrationFlag
+    elif [ $doMigrate == "Leave ${emu} alone forever" ]; then
+        touch $migrationFlag
     fi
-
-elif [[ -L ${migrationTable[0]} && -d ${migrationTable[0]} && -d ${migrationTable[1]} ]]; then
-    echo "Flatpak already linked"
-elif [[ -d ${migrationTable[0]} && ! -e ${migrationTable[1]} ]]; then
-    echo "No AppImage data found, but flatpak data found. New AppImage install."
-    for ((i=0; i<=n; i=(i+2))) { # for each pair of dirs
-
-
-            fromDir=${migrationTable[i]}
-            toDir=${migrationTable[i+1]}
-            echo  "Migrating ${fromDir} to ${toDir}"
-            cd ${fromDir}/..
-            #backup destination location, delete it, then sync original over
-            mkdir -p ${toDir} && rmdir ${toDir} #make the path for todir, but remove the end folder
-            mv "${fromDir}" "${toDir}" #move the original to the new location
-            #link .config to .var so flatpak still works
-            ln -sfn ${toDir} .
-
-    }
-else
-    echo "do nothing"
 fi
 
 }
