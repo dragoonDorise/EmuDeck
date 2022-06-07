ed2b123e66ac81e255d94dd84383b25be92cccec
Godsbane <godsbane@gmail.com>
2022-06-07 01:17:50 -0400

    restructured migration for xemu and rpcs3

--- functions/doMigrations.sh
+++ functions/doMigrations.sh
@@ -1,40 +1,54 @@
 #!/bin/bash
 doMigrations(){
 
-
-
 	##nonstandard##
-	#Xemu files to storage
-	if [ -f "$savesPath/xemu/xbox_hdd.qcow2" ] || [ -f "$HOME/.var/app/app.xemu.xemu/data/xemu/xemu/xbox_hdd.qcow2" ] ; then		
+	#Xemu files to storage if we have a xemu app folder
+	if [ ! -f "$storagePath/xemu/xbox_hdd.qcow2" ] && [ -d "$HOME/.var/app/app.xemu.xemu" ]; then 
+
+		echo "xbox hdd does not exist in storagepath."
 		mkdir -p "$storagePath/xemu"
+		flatpak override app.xemu.xemu --filesystem="$storagePath"xemu:rw --user
+
+		xemuConf="$HOME/.var/app/app.xemu.xemu/data/xemu/xemu/xemu.toml"
+		xemuHDDLine="hdd_path = '${storagePath}xemu/xbox_hdd.qcow2'"
+		xemuEEPLine="eeprom_path = '${storagePath}xemu/eeprom.bin'"
+		sed -i "/hdd_path/c\\${xemuHDDLine}" $xemuConf
+		sed -i "/eeprom_path/c\\${xemuEEPLine}" $xemuConf
+
 		echo -e ""
 		setMSG "Moving Xemu HDD and EEPROM to the Emulation/storage folder"			
 		echo -e ""
-		mv "$HOME/.var/app/app.xemu.xemu/data/xemu/xemu/xbox_hdd.qcow2" $storagePath/xemu/
-		mv "$HOME/.var/app/app.xemu.xemu/data/xemu/xemu/eeprom.bin" $storagePath/xemu/
+		
+		if [ -f "${savesPath}xemu/xbox_hdd.qcow2" ]; then
+			mv -f ${savesPath}xemu/* ${storagePath}xemu/ && rm -rf ${savesPath}xemu/
 
-		flatpak override app.xemu.xemu --filesystem="$storagePath"xemu:rw --user
+		elif [ -f "$HOME/.var/app/app.xemu.xemu/data/xemu/xemu/xbox_hdd.qcow2" ]; then
+			mv "$HOME/.var/app/app.xemu.xemu/data/xemu/xemu/xbox_hdd.qcow2" $storagePath/xemu/
+			mv "$HOME/.var/app/app.xemu.xemu/data/xemu/xemu/eeprom.bin" $storagePath/xemu/
 
-        rsync -av $savesPath/xemu $storagePath && rm -rf $savesPath/xemu/
-
-        sed -i "s|$savesPath|${storagePath}|g" "$HOME/.var/app/app.xemu.xemu/data/xemu/xemu/xemu.toml"
+		fi
 	fi
 
 	#RPCS3 files to storage
-	if [ -d "$savesPath/rpcs3/dev_hdd0" ] || [ -d "$HOME/.var/app/net.rpcs3.RPCS3/config/rpcs3/dev_hdd0" ]; then	
+	if [ ! -d "$storagePath"rpcs3/dev_hdd0 ] && [ -d "$HOME/.var/app/net.rpcs3.RPCS3/" ];then
 
-    	echo -e ""
+		#update config file for the new loc $(emulatorDir) is in the file. made this annoying.
+		rpcs3VFSConf="$HOME/.var/app/net.rpcs3.RPCS3/config/rpcs3/vfs.yml"
+		rpcs3DevHDD0Line="/dev_hdd0/: ${storagePath}rpcs3/dev_hdd0/"
+		sed -i "/dev_hdd0/c\\${rpcs3DevHDD0Line}" $rpcs3VFSConf 
+		echo -e ""
 		setMSG "Moving rpcs3 HDD to the Emulation/storage folder"			
 		echo -e ""
-
 		mkdir -p "$storagePath/rpcs3" 
-		rsync -av "$HOME/.var/app/net.rpcs3.RPCS3/config/rpcs3/dev_hdd0" "$storagePath"rpcs3/ && rm -rf "$HOME/.var/app/net.rpcs3.RPCS3/config/rpcs3/dev_hdd0"
-        rsync -av "$savesPath"rpcs3/dev_hdd0 "$storagePath"rpcs3/ && rm -rf "$HOME/.var/app/net.rpcs3.RPCS3/config/rpcs3/dev_hdd0"
-		#update config file for the new loc $(emulatorDir) is in the file. made this annoying.
-		sed -i 's|$(EmulatorDir)dev_hdd0/|'$storagePath'/rpcs3/dev_hdd0/|g' $HOME/.var/app/net.rpcs3.RPCS3/config/rpcs3/vfs.yml 
-        sed -i 's|'$savesPath'/rpcs3/dev_hdd0/|'$storagePath'/rpcs3/dev_hdd0/|g' $HOME/.var/app/net.rpcs3.RPCS3/config/rpcs3/vfs.yml
-	fi
 
+		if [ -d "$savesPath/rpcs3/dev_hdd0" ]; then
+			mv -f "$savesPath"rpcs3/dev_hdd0 "$storagePath"rpcs3/
+
+		elif [ -d "$HOME/.var/app/net.rpcs3.RPCS3/config/rpcs3/dev_hdd0" ]; then	
+			rsync -av "$HOME/.var/app/net.rpcs3.RPCS3/config/rpcs3/dev_hdd0" "$storagePath"rpcs3/ && rm -rf "$HOME/.var/app/net.rpcs3.RPCS3/config/rpcs3/dev_hdd0"
+
+		fi
+	fi
 
     #yuzu flatpak to appimage
     emu="Yuzu"
