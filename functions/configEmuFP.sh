fe864ae1c304a722c214c9b9b9285b011eb44350
Livedeht <godsbane@gmail.com>
2022-06-30 20:31:32 -0400

    add overwrite parameter to configEmuFP.sh Overwrite = true will backup the files and replace with ours. no overwrite param will only copy files that don't exist and ignore others.

--- functions/configEmuFP.sh
+++ functions/configEmuFP.sh
@@ -2,20 +2,17 @@
 configEmuFP(){		
 	
 	name=$1
-	ID=$2	
+	ID=$2
+	overwrite=$3
 	
-	FOLDER=~/.var/app/"${ID}"/config_bak
-	if [ ! -d "$FOLDER" ]; then
-		setMSG "Backing up ${name} Config..."
-		cp -r ~/.var/app/"${ID}"/config ~/.var/app/"${ID}"/config_bak		
-	fi
+	if [[ ! $overwrite == 'true' ]]; then
+        overwrite="--ignore-existing"
+    else
+        overwrite="--backup --suffix=.bak"
+    fi	
+	setMSG "Updating $name Config using $overwrite"	
 	
-	FOLDER=~/.var/app/"${ID}"/data_bak
-	if [ ! -d "$FOLDER" ]; then
-		setMSG "Backing up ${name} Data..."
-		cp -r ~/.var/app/"${ID}"/data ~/.var/app/"${ID}"/data_bak		
-	fi
+	rsync -avhp --mkpath "$HOME/dragoonDoriseTools/EmuDeck/configs/${ID}" "$HOME/.var/app/" $overwrite
 	
-	rsync -avhp ~/dragoonDoriseTools/EmuDeck/configs/"${ID}"/ ~/.var/app/"${ID}"/ 	
-	
+
 }
