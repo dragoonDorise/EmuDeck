628e22e62b931f2e8fd3d0088e4453da45358ac8
godsbane <godsbane@gmail.com>
2022-07-10 04:13:53 -0400

    more

--- functions/EmuScripts/emuDeckXenia.sh
+++ functions/EmuScripts/emuDeckXenia.sh
@@ -1,130 +1,99 @@
 #!/bin/bash
 #variables
-Cemu_emuName="cemu"
-Cemu_emuType="windows"
-Cemu_emuPath="${romsPath}wiiu/cemu.exe"
-Cemu_releaseURL="https://cemu.info/releases/cemu_1.26.2.zip"
-Cemu_cemuSettings="${romsPath}wiiu/settings.xml"
+Xenia_emuName="Xenia"
+Xenia_emuType="windows"
+Xenia_emuPath="${romsPath}xbox360/Xenia.exe"
+Xenia_releaseURL="https://github.com/xenia-project/release-builds-windows/releases/latest/download/xenia_master.zip"
+Xenia_XeniaSettings="${romsPath}xbox360/settings.xml"
 
 #cleanupOlderThings
-Cemu.cleanup(){
+Xenia.cleanup(){
 	echo "NYI"
 }
 
 #Install
-Cemu.install(){
-	setMSG "Installing Cemu"		
-	FILE="${romsPath}/wiiu/Cemu.exe"	
-	if [ -f "$FILE" ]; then
-		echo "Cemu.exe already exists"
-	else
-		curl $Cemu_releaseURL --output "$romsPath"wiiu/cemu.zip 
-        mkdir -p "$romsPath"wiiu/tmp
-        unzip -o "$romsPath"wiiu/cemu.zip -d "$romsPath"wiiu/tmp
-        mv "$romsPath"wiiu/tmp/cemu_*/ "$romsPath"wiiu/tmp/cemu/
-        rsync -avzh "$romsPath"wiiu/tmp/cemu/ "$romsPath"wiiu/
-        rm -rf "$romsPath"wiiu/tmp 
-        rm -f "$romsPath"wiiu/cemu.zip
-	fi
+Xenia.install(){
+	setMSG "Installing Xenia"		
 
-	cp "$EMUDECKGIT/tools/launchers/cemu.sh" "${toolsPath}"launchers/cemu.sh
-	sed -i "s|/run/media/mmcblk0p1/Emulation/tools|${toolsPath}|" "${toolsPath}"launchers/cemu.sh
-	sed -i "s|/run/media/mmcblk0p1/Emulation/roms/wiiu|${romsPath}wiiu|" "${toolsPath}"launchers/cemu.sh
-	chmod +x "${toolsPath}"launchers/cemu.sh
-	
+	curl -L "$Xenia_releaseURL" --output "$romsPath"xbox360/xenia_master.zip 
+	mkdir -p "$romsPath"xbox360/tmp
+	unzip -o "$romsPath"xbox360/xenia_master.zip -d "$romsPath"xbox360/tmp 
+	mv "$romsPath"xbox360/tmp/* "$romsPath"xbox360 
+	rm -rf "$romsPath"xbox360/tmp 
+	rm -f "$romsPath"xbox360/xenia_master.zip 		
 }
 
 #ApplyInitialSettings
-Cemu.init(){
-	setMSG "Setting up Cemu"	
-	rsync -avhp $EMUDECKGIT/configs/info.cemu.Cemu/data/cemu/ "${romsPath}wiiu"
-    Cemu.setEmulationFolder
-	Cemu.setupSaves
+Xenia.init(){
+	setMSG "Initializing Xenia Config"
+	rsync -avhp $EMUDECKGIT/configs/xenia/ "$romsPath"/xbox360 
 }
 
 #update
-Cemu.update(){
-	setMSG "Updating Cemu Config"	
-	Cemu_cemuSettings="${romsPath}wiiu/settings.xml"
-    if [ -f $Cemu_cemuSettings ]; then
-	    mv -f $Cemu_cemuSettings $Cemu_cemuSettings.bak #retain cemusettings if it exists to stop wiping peoples mods. Just insert our search path for installed games.
-	fi
-    rsync -avhp $EMUDECKGIT/configs/info.cemu.Cemu/data/cemu/ "${romsPath}wiiu"
-	rm $Cemu_cemuSettings
-	mv -f $Cemu_cemuSettings.bak $Cemu_cemuSettings
-    Cemu.setEmulationFolder
-	Cemu.setupSaves
+Xenia.update(){
+	echo "NYI"
 }
 
 #ConfigurePaths
-Cemu.setEmulationFolder(){
-    Cemu_cemuSettings="${romsPath}wiiu/settings.xml"
-	if [[ -f "${Cemu_cemuSettings}" ]]; then
-		gamePathEntryFound=$(grep -rnw $Cemu_cemuSettings -e "z:${romsPath}wiiu/roms")
-		if [[ $gamePathEntryFound == '' ]]; then 
-			xmlstarlet ed --inplace  --subnode "content/GamePaths" --type elem -n Entry -v "z:${romsPath}wiiu/roms" $Cemu_cemuSettings
-		fi
-	fi
+Xenia.setEmulationFolder(){
+	echo "NYI"
 }
 
 #SetupSaves
-Cemu.setupSaves(){
-	unlink "${savesPath}Cemu/saves" # Fix for previous bad symlink
-	linkToSaveFolder Cemu saves "${romsPath}wiiu/mlc01/usr/save"
+Xenia.setupSaves(){
+	echo "NYI"
 }
 
 
 #SetupStorage
-Cemu.setupStorage(){
+Xenia.setupStorage(){
 	echo "NYI"
 }
 
 
 #WipeSettings
-Cemu.wipeSettings(){
-		echo "NYI"
-   # rm -rf "${romPath}wiiu/"
-   # prob not cause roms are here
+Xenia.wipeSettings(){
+	echo "NYI"
 }
 
 
 #Uninstall
-Cemu.uninstall(){
-    rm -rf "${Cemu_emuPath}"
+Xenia.uninstall(){
+    rm -rf "${Xenia_emuPath}"
 }
 
 #setABXYstyle
-Cemu.setABXYstyle(){
-    	echo "NYI"
+Xenia.setABXYstyle(){
+    echo "NYI"
 }
 
 #Migrate
-Cemu.migrate(){
+Xenia.migrate(){
    	echo "NYI" 
 }
 
 #WideScreenOn
-Cemu.wideScreenOn(){
-echo "NYI"
+Xenia.wideScreenOn(){
+	echo "NYI"
 }
 
 #WideScreenOff
-Cemu.wideScreenOff(){
-echo "NYI"
+Xenia.wideScreenOff(){
+	echo "NYI"
 }
 
 #BezelOn
-Cemu.bezelOn(){
-echo "NYI"
+Xenia.bezelOn(){
+	echo "NYI"
 }
 
 #BezelOff
-Cemu.bezelOff(){
+Xenia.bezelOff(){
 	echo "NYI"
 }
 
 #finalExec - Extra stuff
-Cemu.finalize(){
-    Cemu.cleanup
+Xenia.finalize(){
+    Xenia.cleanup
 }
 
