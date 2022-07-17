211598d798e8af1bf8603c06564e040efd7a31b9
godsbane <godsbane@gmail.com>
2022-07-17 01:44:42 -0400

    add touch /home/deck/.steam/steam/.cef-enable-remote-debugging  to pluginloader install to enable cef_debugging.

--- functions/ToolScripts/emuDeckPlugins.sh
+++ functions/ToolScripts/emuDeckPlugins.sh
@@ -4,14 +4,15 @@ Plugins_installPluginLoader(){
    local PluginLoader_releaseURL="https://github.com/SteamDeckHomebrew/PluginLoader/raw/main/dist/install_release.sh"
    mkdir -p $HOME/homebrew
    sudo chown -R deck:deck $HOME/homebrew
-   curl -L $PluginLoader_releaseURL | sh	
+   curl -L $PluginLoader_releaseURL | sh
+   touch /home/deck/.steam/steam/.cef-enable-remote-debugging
 }
 
 Plugins_installPowerTools(){	
    sudo rm -rf ~/homebrew/plugins/PowerTools
    sudo git clone https://github.com/NGnius/PowerTools.git ~/homebrew/plugins/PowerTools 
    cd ~/homebrew/plugins/PowerTools
-   sudo git checkout tags/v0.6.0
+   sudo git checkout tags/v0.7.0
 }
 
 Plugins_installSteamDeckGyroDSU(){
