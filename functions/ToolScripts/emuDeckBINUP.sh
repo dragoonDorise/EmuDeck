20906f93ac68d1ce1b720c0a92d92bf658a06c0d
godsbane <godsbane@gmail.com>
2022-07-23 14:21:44 -0400

    binup and chd use new shortcut fn

--- functions/ToolScripts/emuDeckBINUP.sh
+++ functions/ToolScripts/emuDeckBINUP.sh
@@ -2,43 +2,19 @@
 
 BINUP_toolName="EmuDeck Tool Updater"
 BINUP_toolType="script"
-BINUP_toolPath="${toolsPath}binupdate/binupdate.sh"
+BINUP_toolPath="${toolsPath}/binupdate/binupdate.sh"
+BINUP_Shortcutlocation="$HOME/Desktop/SteamRomManager.desktop"
 
 
 
 BINUP_install(){		
 	
-	rsync -avhp --mkpath "$EMUDECKGIT/tools/binupdate" "$toolsPath"
+	rsync -avhp --mkpath "$EMUDECKGIT/tools/binupdate" "$toolsPath/"
 
 	chmod +x "$BINUP_toolPath"
 	#update the paths in the script
 	sed -i "s|/run/media/mmcblk0p1/Emulation/roms|${romsPath}|g" "$BINUP_toolPath"
-	sed -i "s|/run/media/mmcblk0p1/Emulation/tools/|${toolsPath}|g" "$BINUP_toolPath"	
+	sed -i "s|/run/media/mmcblk0p1/Emulation/tools|${toolsPath}|g" "$BINUP_toolPath"	
 
-	BINUP_createDesktopShortcut
+	createDesktopShortcut "$BINUP_Shortcutlocation" "$BINUP_toolName" "bash $BINUP_toolPath"
 }
-
-
-
-BINUP_createDesktopShortcut(){
-
-	BINUP_Shortcutlocation=$1
-
-	if [[ "$BINUP_Shortcutlocation" == "" ]]; then
-
-		BINUP_Shortcutlocation="$HOME/Desktop/SteamRomManager.desktop"
-	
-
-	fi
-
-	echo "#!/usr/bin/env xdg-open
-	[Desktop Entry]
-	Name="$BINUP_toolName"
-	Exec=bash "$BINUP_toolPath"
-	Icon=steamdeck-gaming-return
-	Terminal=true
-	Type=Application
-	StartupNotify=false" > $BINUP_Shortcutlocation
-	chmod +x $BINUP_Shortcutlocation
-}
-
