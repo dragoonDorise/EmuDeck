9dcf34082463123067b68abae0e651f1f6f6c0ad
godsbane <godsbane@gmail.com>
2022-07-26 01:30:24 -0400

    log out installEmuAI

--- functions/installEmuAI.sh
+++ functions/installEmuAI.sh
@@ -8,15 +8,19 @@ installEmuAI(){
     if [[ $altName == "" ]]; then
         altName=$name
     fi
-	
+	echo $name
+    echo $url
+    echo $altName
+
+    rm -f "$HOME/Applications/$altName.AppImage" 
     mkdir -p "$HOME/Applications"
-    curl -Lo "$HOME/Applications/$altName.AppImage" "$url"
+    curl -L "$url" -o "$HOME/Applications/$altName.AppImage" 
 	chmod +x "$HOME/Applications/$altName.AppImage" 
 
 
     shName=$(echo "$name" | awk '{print tolower($0)}')
     
-    find "${toolsPath}/launchers/" -type f -iname "$shName.sh" | while read f; do echo "deleting $f"; rm -f "$f"; done;
+    find "${toolsPath}/launchers/" -type f -iname "$shName.sh" | while read -r f; do echo "deleting $f"; rm -f "$f"; done;
 	cp -v "${EMUDECKGIT}/tools/launchers/${shName}.sh" "${toolsPath}/launchers/${shName}.sh"	
 	chmod +x "${toolsPath}/launchers/${shName}.sh"
     
