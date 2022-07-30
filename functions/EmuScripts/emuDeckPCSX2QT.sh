47bcb35b60c968b8358d7817602bc67b20c506d5
Rodrigo <rodrigosedano@me.com>
2022-07-31 00:12:01 +0200

    Revert "PCSX2QT ws_cheats - commented"
    This reverts commit 657479b54d2a7a0f7175e6d970c209ad99ecc457.

--- functions/EmuScripts/emuDeckPCSX2QT.sh
+++ functions/EmuScripts/emuDeckPCSX2QT.sh
@@ -24,9 +24,6 @@ PCSX2QT_init(){
 	PCSX2QT_setupStorage
 	PCSX2QT_setupSaves #
 	PCSX2QT_addSteamInputProfile #
-			
-	#WideScreen codes
-	#rsync -r --ignore-existing  "$EMUDECKGIT/configs/net.pcsx2.PCSX2/config/PCSX2/cheats_ws/" "$HOME/.config/PCSX2/cheats_ws/"
 }
 
 #update
@@ -70,6 +67,7 @@ PCSX2QT_setEmulationFolder(){
 	changeLine "$texturesDirOpt" "$newtexturesDirOpt" "$PCSX2QT_configFile"
 	changeLine "$coversDirOpt" "$newcoversDirOpt" "$PCSX2QT_configFile"
 	changeLine "$recursivePathsDirOpt" "$newrecursivePathsDirOpt" "$PCSX2QT_configFile"
+
 
 }
 
