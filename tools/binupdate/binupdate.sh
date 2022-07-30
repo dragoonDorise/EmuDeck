2699dcfb2e538e737de1e6221a8aeb3d7071cfa1
godsbane <godsbane@gmail.com>
2022-07-30 04:06:14 -0400

    binary updater now sources the functions and vars. binary updater reports what it did afterwards.

--- tools/binupdate/binupdate.sh
+++ tools/binupdate/binupdate.sh
@@ -1,89 +1,16 @@
 #!/bin/bash
 #while this is in testing, i'm copying in the functions. once we leave the original repo in place and don't delete it, i'd like to use the functions we already made.
-installESDE(){		
 
+source "$HOME/emudeck/backend/functions/all.sh"
+if [ "$?" == "1" ]; then
+    echo "functions could not be loaded."
+    zenity --error \
+    --text="EmuDeck Functions could not be loaded. Please re-run Emudeck install."
+    exit
+fi
 
-    #New repo
-
-    curl https://gitlab.com/es-de/emulationstation-de/-/raw/master/es-app/assets/latest_steam_deck_appimage.txt --output "$toolsPath/latesturl.txt"
-    latestURL=$(grep "https://gitlab" "$toolsPath/latesturl.txt")
-
-    curl "$latestURL" --output "$toolsPath/EmulationStation-DE-x64_SteamDeck.AppImage"
-    rm "$toolsPath/latesturl.txt/"
-    chmod +x "$toolsPath/EmulationStation-DE-x64_SteamDeck.AppImage"
-
-}
-
-PCSX2QT_install(){
-	echo "Begin PCSX2-QT Install"
-	installEmuAI "pcsx2-Qt" "$(getReleaseURLGH "PCSX2/pcsx2" "Qt.AppImage")" #pcsx2-Qt.AppImage
-}
-
-installSRM(){		
-	#setMSG "${installString} Steam Rom Manager"
-	rm -f ~/Desktop/Steam-ROM-Manager-2.3.29.AppImage
-	rm -f ~/Desktop/Steam-ROM-Manager.AppImage
-	mkdir -p "${toolsPath}"/srm
-	curl -L "$(curl -s https://api.github.com/repos/SteamGridDB/steam-rom-manager/releases/latest | grep -E 'browser_download_url.*AppImage' | grep -ve 'i386' | cut -d '"' -f 4)" --output "${toolsPath}/srm/Steam-ROM-Manager.AppImage"
-	chmod +x "${toolsPath}"/srm/Steam-ROM-Manager.AppImage	
-}
-	#paths update via sed in main script
-	romsPath="/run/media/mmcblk0p1/Emulation/roms"
-	toolsPath="/run/media/mmcblk0p1/Emulation/tools"
-	scriptPath="${toolsPath}/binupdate"
-	
-	#initialize log
-	TIMESTAMP=$(date "+%Y%m%d_%H%M%S")
-	LOGFILE="${scriptPath}/binupdate-$TIMESTAMP.log"
-	exec > >(tee "${LOGFILE}") 2>&1
-	
-    binTable=()
-    binTable+=(TRUE "EmulationStation-DE" "esde")
-    binTable+=(TRUE "Steam Rom Manager" "srm")
-    binTable+=(TRUE "Nintendo Switch Emu" "yuzu")
-    binTable+=(TRUE "Nintendo WiiU Emu" "cemu")
-    binTable+=(FALSE "Xbox 360 Emu - TESTING ONLY" "xenia")
-
-#Binary selector
-    text="$(printf "What tools do you want to get the latest version of?\n This tool will simply overwrite what you have with the newest available.")"
-    binsToDL=$(zenity --list \
-            --title="EmuDeck" \
-            --height=500 \
-            --width=250 \
-            --ok-label="OK" \
-            --cancel-label="Exit" \
-            --text="${text}" \
-            --checklist \
-            --column="Select" \
-            --column="System" \
-            --column="Name" \
-            --print-column=3 \
-            "${binTable[@]}" 2>/dev/null)
-    ans=$?
-
-    if [ $ans -eq 0 ]; then
-        echo "User selected: $binsToDL"
-        if [[ "$binsToDL" == *"esde"* ]]; then
-            installESDE
-        fi
-        if [[ "$binsToDL" == *"srm"* ]]; then
-            installSRM
-        fi
-        if [[ "$binsToDL" == *"yuzu"* ]]; then
-            mkdir -p "$HOME/Applications"
-            rm "$HOME/Applications/yuzu.AppImage"
-            url="$(curl -sL https://api.github.com/repos/yuzu-emu/yuzu-mainline/releases/latest | jq -r ".assets[].browser_download_url" | grep .AppImage\$)"            
-            curl -Lo "$HOME/Applications/yuzu.AppImage" "$url"
-        fi
-        if [[ "$binsToDL" == *"pcsx2-qt"* ]]; then
-            mkdir -p "$HOME/Applications"
-            rm "$HOME/Applications/pcsx2-Qt.AppImage"
-            url="$(curl -sL https://api.github.com/repos/yuzu-emu/yuzu-mainline/releases/latest | jq -r ".assets[].browser_download_url" | grep .AppImage\$)"            
-            curl -Lo "$HOME/Applications/yuzu.AppImage" "$url"
-        fi
-        if [[ "$binsToDL" == *"cemu"* ]]; then
-
-            releasesStr=$(curl -sL https://cemu.info | awk 'BEGIN{
+updateCemu(){
+     releasesStr=$(curl -sL https://cemu.info | awk 'BEGIN{
             RS="</a>"
             IGNORECASE=1
             }
@@ -126,14 +53,100 @@ installSRM(){
             rsync -avzh "$romsPath/wiiu/tmp/cemu/" "$romsPath/wiiu/"
             rm -rf "$romsPath/wiiu/tmp" 
             rm -f "$romsPath/wiiu/cemu.zip"	
+}
+
+
+    #begin script
+	#paths update via sed in main script
+    #source the all.sh, these should be pulled correctly!
+	scriptPath="${toolsPath}/binupdate"
+	
+	#initialize log
+	TIMESTAMP=$(date "+%Y%m%d_%H%M%S")
+	LOGFILE="${scriptPath}/binupdate-$TIMESTAMP.log"
+	exec > >(tee "${LOGFILE}") 2>&1
+	
+    binTable=()
+    binTable+=(TRUE "EmulationStation-DE" "esde")
+    binTable+=(TRUE "Steam Rom Manager" "srm")
+    binTable+=(TRUE "Nintendo Switch Emu" "yuzu")
+    binTable+=(TRUE "Sony PlayStation 2 Emu" "pcsx2-qt")
+    binTable+=(TRUE "Nintendo WiiU Emu" "cemu")
+    binTable+=(FALSE "Xbox 360 Emu - TESTING ONLY" "xenia")
+
+#Binary selector
+    text="$(printf "What tools do you want to get the latest version of?\n This tool will simply overwrite what you have with the newest available.")"
+    binsToDL=$(zenity --list \
+            --title="EmuDeck" \
+            --height=500 \
+            --width=250 \
+            --ok-label="OK" \
+            --cancel-label="Exit" \
+            --text="${text}" \
+            --checklist \
+            --column="Select" \
+            --column="System" \
+            --column="Name" \
+            --print-column=3 \
+            "${binTable[@]}" 2>/dev/null)
+    ans=$?
+    messages=()
+    if [ $ans -eq 0 ]; then
+        echo "User selected: $binsToDL"
+        if [[ "$binsToDL" == *"esde"* ]]; then
+            ESDE_install
+            if [ "$?" == "0" ]; then
+                messages+=("EmulationStation-DE Updated Successfully")
+            else
+                messages+=("There was a problem updating EmulationStation-DE")
+            fi
+        fi
+        if [[ "$binsToDL" == *"srm"* ]]; then
+            SRM_install
+            if [ "$?" == "0" ]; then
+                messages+=("SteamRomManager Updated Successfully")
+            else
+                messages+=("There was a problem updating SteamRomManager")
+            fi
+        fi
+        if [[ "$binsToDL" == *"yuzu"* ]]; then
+            Yuzu_install
+            if [ "$?" == "0" ]; then
+                messages+=("Yuzu Updated Successfully")
+            else
+                messages+=("There was a problem updating Yuzu")
+            fi
+        fi
+        if [[ "$binsToDL" == *"pcsx2-qt"* ]]; then
+            PCSX2QT_install
+            if [ "$?" == "0" ]; then
+                messages+=("PCSX2-QT Updated Successfully")
+            else
+                messages+=("There was a problem updating PCSX2-QT")
+            fi
+        fi
+        if [[ "$binsToDL" == *"cemu"* ]]; then
+            updateCemu
+            if [ "$?" == "0" ]; then
+                messages+=("Cemu Updated Successfully")
+            else
+                messages+=("There was a problem updating Cemu")
+            fi
         fi
         if [[ "$binsToDL" == *"xenia"* ]]; then
-            curl -L https://github.com/xenia-project/release-builds-windows/releases/latest/download/xenia_master.zip --output "$romsPath"/xbox360/xenia_master.zip 
-            mkdir -p "$romsPath"/xbox360/tmp
-            unzip -o "$romsPath"/xbox360/xenia_master.zip -d "$romsPath"/xbox360/tmp 
-            mv "$romsPath"/xbox360/tmp/* "$romsPath"/xbox360 
-            rm -rf "$romsPath"/xbox360/tmp 
-            rm -f "$romsPath"/xbox360/xenia_master.zip 	
+            Xenia_install
+            if [ "$?" == "0" ]; then
+                messages+=("Xenia Updated Successfully")
+            else
+                messages+=("There was a problem updating Xenia")
+            fi
+        fi
+        if [[ ${#messages[@]} -gt 0 ]]; then
+            zenity  --list \
+                    --title="Update Status" \
+                    --text="" \
+                    --column="Messages"  \
+                        "${messages[@]}"
         fi
     fi
 
