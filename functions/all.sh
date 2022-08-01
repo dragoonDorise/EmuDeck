1c1d16b7a73dbe889fbb1680d29d952c4187f9ec
Livedeht <godsbane@gmail.com>
2022-08-01 03:48:30 -0400

    Add emudeck savesync (#306)
    * add emuDeckSaveSysnc fn
    
    * create service.
    
    * add expert mode option for savesync

--- functions/all.sh
+++ functions/all.sh
@@ -35,6 +35,7 @@ source "$EMUDECKGIT"/functions/ToolScripts/emuDeckPlugins.sh
 source "$EMUDECKGIT"/functions/ToolScripts/emuDeckSRM.sh
 source "$EMUDECKGIT"/functions/ToolScripts/emuDeckCHD.sh
 source "$EMUDECKGIT"/functions/ToolScripts/emuDeckBINUP.sh
+source "$EMUDECKGIT"/functions/ToolScripts/emuDeckSaveSync.sh
 
 #emuscripts
 source "$EMUDECKGIT"/functions/EmuScripts/emuDeckYuzu.sh
