f30f4e6afa67ce6213018094283c79705c163bf1
godsbane <godsbane@gmail.com>
2022-08-04 01:12:41 -0400

    add ryujinx to all.sh

--- functions/all.sh
+++ functions/all.sh
@@ -48,6 +48,7 @@ source "$EMUDECKGIT"/functions/EmuScripts/emuDeckCitra.sh
 source "$EMUDECKGIT"/functions/EmuScripts/emuDeckDolphin.sh 
 source "$EMUDECKGIT"/functions/EmuScripts/emuDeckPrimehack.sh
 source "$EMUDECKGIT"/functions/EmuScripts/emuDeckRetroArch.sh
+source "$EMUDECKGIT"/functions/EmuScripts/emuDeckRyujinx.sh
 source "$EMUDECKGIT"/functions/EmuScripts/emuDeckPPSSPP.sh
 source "$EMUDECKGIT"/functions/EmuScripts/emuDeckDuckStation.sh
 source "$EMUDECKGIT"/functions/EmuScripts/emuDeckXemu.sh
