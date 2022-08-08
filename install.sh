874ce2beafaae1e74dbebaf6a10b9cdf2a45a309
Rodrigo <rodrigosedano@me.com>
2022-08-08 16:24:38 +0200

    clone on Zenity or Whiptail

--- install.sh
+++ install.sh
@@ -77,7 +77,6 @@ sleep 1
 SECONDTIME="$HOME/emudeck/.finished"
 
 
-
 #
 ##
 ## set backend location
@@ -90,6 +89,23 @@ EMUDECKGIT="$HOME/emudeck/backend"
 echo 'Downloading files...'
 ##
 #
+if [ "$uiMode" == 'zenity' ] || [ "$uiMode" == 'whiptail']; then
+	#We create all the needed folders for installation
+	if [[ ! -e $EMUDECKGIT/.git/config ]]; then
+		mkdir -p "$EMUDECKGIT"
+	
+		#Cloning EmuDeck files
+		git clone https://github.com/dragoonDorise/EmuDeck.git "$EMUDECKGIT"
+	fi
+	
+	git status "$EMUDECKGIT" --porcelain
+	if [[ ! $noPull == true ]]; then
+		cd "$EMUDECKGIT"
+		git fetch origin  && git checkout origin/$branch  &&	git reset --hard origin/$branch && git clean -ffdx
+		
+	fi
+fi
+
 
 #
 ##
