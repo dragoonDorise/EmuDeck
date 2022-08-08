683a4f9949b6a76d71e1049ed5df4faed0ee89fe
Rodrigo <rodrigosedano@me.com>
2022-08-08 16:38:49 +0200

    wrong folder

--- install.sh
+++ install.sh
@@ -146,7 +146,7 @@ elif [ "$uiMode" == 'whiptail' ]; then
 else	
 	echo "Electron UI"	
 	#App Image detection & launch so older user can update just using the same old .desktop
-	if [[ ! -e "~/Applications/Applications/EmuDeck.AppImage" ]]; then
+	if [[ ! -e "~/Applications/EmuDeck.AppImage" ]]; then
 		mkdir -p ~/Applications
 		curl -L "$(curl -s https://api.github.com/repos/EmuDeck/emudeck-electron/releases/latest | grep -E 'browser_download_url.*AppImage' | cut -d '"' -f 4)" > ~/Applications/EmuDeck.AppImage && chmod +x ~/Applications/EmuDeck.AppImage && ~/Applications/EmuDeck.AppImage && exit
 	fi
