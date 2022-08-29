7f6d3bbaf4ba98450469dac83a175d19b708bf02
Rodrigo <rodrigosedano@me.com>
2022-08-29 16:47:43 +0200

    new icon outside of desktop

--- functions/createDesktopIcons.sh
+++ functions/createDesktopIcons.sh
@@ -7,18 +7,8 @@ createDesktopIcons(){
 	rm -rf ~/Desktop/EmuDeck.desktop 2>/dev/null
 	rm -rf ~/Desktop/EmuDeckSD.desktop 2>/dev/null
 	rm -rf ~/Desktop/EmuDeckBinUpdate 2>/dev/null
+	rm -rf ~/Desktop/EmuDeckApp.desktop 2>/dev/null
 
-	#Legacy Icon, just in case
-	echo "#!/usr/bin/env xdg-open
-	[Desktop Entry]
-	Name=EmuDeck (Legacy)
-	Exec=curl https://raw.githubusercontent.com/dragoonDorise/EmuDeck/main/install.sh | bash -s #-- SD
-	Icon=steamdeck-gaming-return
-	Terminal=true
-	Type=Application
-	StartupNotify=false" > ~/Desktop/EmuDeck.desktop
-	chmod +x ~/Desktop/EmuDeck.desktop
-	
 	#New EmuDeck icon, same place so people won't get confused
 	 echo "#!/usr/bin/env xdg-open
 	 [Desktop Entry]
@@ -27,7 +17,17 @@ createDesktopIcons(){
 	 Icon=steamdeck-gaming-return
 	 Terminal=false
 	 Type=Application
-	 StartupNotify=false" > ~/Desktop/EmuDeckAppImage.desktop
-	 chmod +x ~/Desktop/EmuDeckAppImage.desktop
+	 StartupNotify=false" > ~/Desktop/EmuDeck.desktop
+	 chmod +x ~/Desktop/EmuDeck.desktop
+	 
+	 echo "#!/usr/bin/env xdg-open
+	  [Desktop Entry]
+	  Name=EmuDeck
+	  Exec=$HOME/Applications/EmuDeck.AppImage
+	  Icon=steamdeck-gaming-return
+	  Terminal=false
+	  Type=Application
+	  StartupNotify=false" > $HOME/.local/share/applications/Cemu.desktop
+	  chmod +x $HOME/.local/share/applications/Cemu.desktop	
 	
 }
