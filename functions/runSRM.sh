fa1c4b075afe04d0dba21522ab4598fe8c61d3e9
Rodrigo <rodrigosedano@me.com>
2022-08-30 17:19:02 +0200

    runSRM

--- functions/runSRM.sh
+++ functions/runSRM.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 #We need this function to launch SRM from the electron app
 runSRM(){	
-	local path=$1
-	cd $path && ./Steam-ROM-Manager.AppImage
+	cd ~/Desktop/ && ./SteamRomManager.desktop
 }
