14c2600efdb91ddd9fe9051db9065e6a6a2dae5b
Rodrigo <rodrigosedano@me.com>
2022-09-16 22:26:16 +0200

    Creates ~/Applications on install

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -L "$(curl -s https://api.github.com/repos/EmuDeck/emudeck-electron/releases/latest | grep -E 'browser_download_url.*AppImage' | cut -d '"' -f 4)" > ~/Applications/EmuDeck.AppImage && chmod +x ~/Applications/EmuDeck.AppImage && kill -15 $(pidof emudeck) & ~/Applications/EmuDeck.AppImage && exit
+mkdir -p ~/Applications && curl -L "$(curl -s https://api.github.com/repos/EmuDeck/emudeck-electron/releases/latest | grep -E 'browser_download_url.*AppImage' | cut -d '"' -f 4)" > ~/Applications/EmuDeck.AppImage && chmod +x ~/Applications/EmuDeck.AppImage && kill -15 $(pidof emudeck) & ~/Applications/EmuDeck.AppImage && exit
