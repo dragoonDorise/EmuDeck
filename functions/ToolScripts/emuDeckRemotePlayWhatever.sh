f27969df2968a4ec9f0e95e9e0cbadaa1fd039da
Rodrigo <rodrigosedano@me.com>
2022-09-17 12:19:50 +0200

    remoteplaywhatever configs

--- functions/ToolScripts/emuDeckRemotePlayWhatever.sh
+++ functions/ToolScripts/emuDeckRemotePlayWhatever.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-RemoteWhatever_install(){		
+RemotePlayWhatever_install(){		
 	mkdir -p "${toolsPath}/remoteplaywhatever"
 	installEmuAI "RemotePlayWhatever"  "$(getReleaseURLGH "m4dEngi/RemotePlayWhatever" "AppImage")" 
-	RemoteWhatever_init
+	RemotePlayWhatever_init
 }
 
-RemoteWhatever_init(){			
+RemotePlayWhatever_init(){			
 	setMSG "Configuring RemotePlayWhatever"
 	cp "$EMUDECKGIT/tools/remoteplaywhatever/remoteplaywhatever.sh" "${toolsPath}/remoteplaywhatever/"
 	chmod +x "${toolsPath}/remoteplaywhatever/remoteplaywhatever.sh"
