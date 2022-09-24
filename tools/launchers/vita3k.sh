c71a9aa52d0eaa04fbb289a23bdf77d2764368d7
godsbane <godsbane@gmail.com>
2022-09-24 04:53:06 -0400

    add vita launcher

---
+++ tools/launchers/vita3k.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+emuName="Vita3K" #parameterize me
+emufolder="$HOME/Applications/Vita3K" # has to be applications for ES-DE to find it
+
+#find full path to emu executable
+exe=$(find $emufolder -iname "${emuName}" | sort -n | cut -d' ' -f 2- | tail -n 1 2>/dev/null)
+
+#make sure that file is executable
+chmod +x $exe
+
+#run the executable with the params.
+#Fix first '
+param="${@}"
+substituteWith='"'
+param=${param/\'/"$substituteWith"}
+#Fix last ' on command
+param=$(echo "$param" | sed 's/.$/"/')
+eval "${exe} ${param}"
