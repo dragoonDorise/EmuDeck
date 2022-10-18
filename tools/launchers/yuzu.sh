6fbb013abca3c1a96b621419342b4c02cb346d61
AngelofWoe <jaia.one.winged.angel@gmail.com>
2022-10-18 13:53:35 -0500

    Fix some bugs in yuzu.sh

--- tools/launchers/yuzu.sh
+++ tools/launchers/yuzu.sh
@@ -1,24 +1,28 @@
-#!/bin/sh
+#!/usr/bin/bash
+
 emuName="yuzu" #parameterize me
-emufolder="$HOME/Applications" # has to be applications for ES-DE to find it
+emufolder="${HOME}/Applications" # has to be applications for ES-DE to find it
 
 #find full path to emu executable
-exe=$(find $emufolder -iname "${emuName}*.AppImage" | sort -n | cut -d' ' -f 2- | tail -n 1 2>/dev/null)
+exe=$(find "${emufolder}" -iname "${emuName}*.AppImage" | sort -n | cut -d' ' -f 2- | tail -n 1 2>/dev/null)
 
 #if appimage doesn't exist fall back to flatpak.
-if [[ $exe == '' ]]; then
+if [[ "${exe}" == '' ]]; then
     #flatpak
-    flatpakApp=$(flatpak list --app --columns=application | grep $emuName)
-    exe="/usr/bin/flatpak run "$flatpakApp
+    flatpakApp="$( flatpak list --app --columns=application | grep "${emuName}" )"
+    exe="/usr/bin/flatpak run ${flatpakApp}"
 else
 #make sure that file is executable
-    chmod +x $exe
+    chmod +x "${exe}"
 fi
-#run the executable with the params.
-#Fix first '
-param="${@}"
-substituteWith='"'
-param=${param/\'/"$substituteWith"}
-#Fix last ' on command
-param=$(echo "$param" | sed 's/.$/"/')
-eval "${exe} ${param}"
+
+# Check for single quotes around the last argument
+if [[ "${*:$#}" =~ ^\'.*\'$ ]]; then
+    ARGS=("${@}")
+    LASTARG="${ARGS[-1]#\'}"
+    ARGS[-1]="${LASTARG%\'}"
+    set -- "${ARGS[@]}"
+fi
+
+# Run the program
+"${exe}" "${@}"
