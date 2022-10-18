20e04051996d16694d9bac4c3186f80c25219155
AngelofWoe <jaia.one.winged.angel@gmail.com>
2022-10-17 19:52:29 -0500

    Add output to log if install -d fails

--- tools/proton-launch.sh
+++ tools/proton-launch.sh
@@ -51,7 +51,10 @@ set_env () {
     fi
     # Create prefix if it doesn't exist
     if ! [ -d "${STEAM_COMPAT_DATA_PATH}" ]; then
-        install -d "${STEAM_COMPAT_DATA_PATH}" || exit 1
+        install -d "${STEAM_COMPAT_DATA_PATH}" || {
+            echo "Error: Failed to create STEAM_COMPAT_DATA_PATH: ${STEAM_COMPAT_DATA_PATH}" >> "${LOGFILE}"
+            exit 1 # Output this error
+        }
     fi
     {
         echo "STEAM_COMPAT_DATA_PATH: ${STEAM_COMPAT_DATA_PATH}"
