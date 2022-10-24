e60c20505750b2127e8e46c896678892d3c1b8bd
Riccardo Pittau <elfosardo@gmail.com>
2022-10-24 16:28:20 +0200

    Split one liner for better readability
    Add URLs variables to simplify even more the commands part.

--- install.sh
+++ install.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-mkdir -p ~/Applications && curl -L "$(curl -s https://api.github.com/repos/EmuDeck/emudeck-electron/releases/latest | grep -E 'browser_download_url.*AppImage' | cut -d '"' -f 4)" > ~/Applications/EmuDeck.AppImage && chmod +x ~/Applications/EmuDeck.AppImage && ~/Applications/EmuDeck.AppImage
+
+set -e
+
+EMUDECK_GITHUB_URL="https://api.github.com/repos/EmuDeck/emudeck-electron/releases/latest"
+EMUDECK_URL="$(curl -s ${EMUDECK_GITHUB_URL} | grep -E 'browser_download_url.*AppImage' | cut -d '"' -f 4)"
+
+mkdir -p ~/Applications
+curl -L "${EMUDECK_URL}" > ~/Applications/EmuDeck.AppImage
+chmod +x ~/Applications/EmuDeck.AppImage
+~/Applications/EmuDeck.AppImage
