2581a5c3d86406c652c587a0a54e4a2161f31388
Rodrigo <rodrigosedano@me.com>
2022-12-12 20:17:34 +0100

    Uninstall HomeBrew

--- functions/ToolScripts/emuDeckInstallHomebrewGames.sh
+++ functions/ToolScripts/emuDeckInstallHomebrewGames.sh
@@ -6,8 +6,7 @@ emuDeckInstallHomebrewGames(){
 	git clone https://github.com/EmuDeck/emudeck-homebrew.git "$toolsPath/homebrew/roms/" --depth=1
 	cd "$toolsPath/homebrew/roms/" && git reset --hard HEAD && git clean -f -d && git pull && echo  "Homebrew Games up to date!" || echo "problem pulling Homebrew Games"
 	
-	#Symlinks
-	
+	#Symlinks	
 	ln -sn "$toolsPath/homebrew/roms/gamegear"  "${romsPath}/gamegear/homebrew"
 	ln -sn "$toolsPath/homebrew/roms/gb"  "${romsPath}/gb/homebrew"
 	ln -sn "$toolsPath/homebrew/roms/gba"  "${romsPath}/gba/homebrew"
@@ -17,6 +16,22 @@ emuDeckInstallHomebrewGames(){
 	ln -sn "$toolsPath/homebrew/roms/nes"  "${romsPath}/nes/homebrew"
 	ln -sn "$toolsPath/homebrew/roms/snes" "${romsPath}/snes/homebrew"
 	
-	rsync -r --ignore-existing "${toolsPath}//homebrew/roms/downloaded_media/" "${toolsPath}/downloaded_media/" && rm -rf "${toolsPath}//homebrew/roms/downloaded_media/"
+	rsync -r --ignore-existing "${toolsPath}//homebrew/roms/downloaded_media/" "${toolsPath}/downloaded_media/" && rm -rf "${toolsPath}//homebrew/roms/downloaded_media/" && echo "true"
 	
+}
+
+emuDeckUninstallHomebrewGames(){	
+	rm -rf -p "$toolsPath/homebrew/roms/"
+	
+	#Symlinks	
+	unlink "${romsPath}/gamegear/homebrew"
+	unlink "${romsPath}/gb/homebrew"
+	unlink "${romsPath}/gba/homebrew"
+	unlink "${romsPath}/gbc/homebrew"
+	unlink "${romsPath}/genesis/homebrew"
+	unlink "${romsPath}/mastersystem/homebrew"
+	unlink "${romsPath}/nes/homebrew"
+	unlink "${romsPath}/snes/homebrew"
+
+	rm -rf -p "$toolsPath/homebrew/roms/" && echo "true"
 }
