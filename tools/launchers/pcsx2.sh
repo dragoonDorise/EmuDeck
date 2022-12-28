4cf364a0def15ec46853369fe07f1f959db67d91
Rodrigo <rodrigosedano@me.com>
2022-12-28 22:13:55 +0100

    Revert "Angel emu-launch"
    This reverts commit 5dcf071f2748a725ac50b45b69b38e327b10242d.

--- tools/launchers/pcsx2.sh
+++ tools/launchers/pcsx2.sh
@@ -1,13 +1,2 @@
-#!/usr/bin/bash
-
-# shellcheck disable=SC1091
-. "${HOME}/emudeck/settings.sh"
-
-# shellcheck disable=SC2154
-LAUNCH="${toolsPath}/emu-launch.sh"
-
-# Set emulator name
-EMU="pcsx2"
-
-# Launch emu-launch.sh
-"${LAUNCH}" -e "${EMU}" -- "${@}"
+#!/bin/sh
+/usr/bin/flatpak run net.pcsx2.PCSX2
