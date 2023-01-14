496371457f80b4ad84b2f1a01dd9410d603dd2c3
KingIzzymon <94716517+KingIzzymon@users.noreply.github.com>
2023-01-13 19:04:18 -0500

    Added cloud services parser in SRM config. Added scripts with matching SteamGridDB names for proper scraping.

---
+++ tools/cloud/Discord.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+
+LINK="https://discord.com/"
+
+"/usr/bin/flatpak" run --branch=stable --arch=x86_64 --command=/app/bin/chrome --file-forwarding com.google.Chrome @@u @@ --window-size=1024,640 --force-device-scale-factor=1.25 --device-scale-factor=1.25 --kiosk "${LINK}"
