e5d6a0893f9d5c4f0b96ded5372208c70b148cdb
Livedeht <godsbane@gmail.com>
2023-01-23 14:22:28 -0500

    Fix temp file cleanup for installEmuBI

--- functions/installEmuBI.sh
+++ functions/installEmuBI.sh
@@ -13,7 +13,7 @@ installEmuBI(){
     echo $url
     echo $altName
 
-    rm -f "$HOME/Applications/$altName" 
+    rm -f "$HOME/Applications/$altName.$format"
     mkdir -p "$HOME/Applications"
     curl -L "$url" -o "$HOME/Applications/$altName.$format.temp"  && mv "$HOME/Applications/$altName.$format.temp" "$HOME/Applications/$altName.$format"
     
