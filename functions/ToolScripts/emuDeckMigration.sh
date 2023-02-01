cb1bf8524d2ffd7d41f0316cc3b2a9b1ff4a1473
Rodrigo <rodrigosedano@me.com>
2023-02-01 17:23:13 +0100

    emuDeckMigration wip

---
+++ functions/ToolScripts/emuDeckMigration.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#variables
+
+#We check the current Emulation folder space, and the destination
+Migration_init(){
+	destination=$1
+	#File Size on target
+	neededSpace=$(du -s "$emulationPath" | cut -f1)
+
+	#File Size on target
+	freeSpace=$(df -s "$emulationPath" |  cut -d' ' -f8)
+	difference=$(($freeSpace - $neededSpace))
+	if [ $difference -gt 0 ]; then
+		Migration_move "$emulationPath" "$destination"
+	fi 
+	
+}
+
+#We rsync, only when rsync is completed we delete the old folder.
+Migration_move(){
+	target=$1
+	destination=$2
+	rsync -avzh --dry-run "$target" "$destination" && echo "true"
+}
