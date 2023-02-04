799434ff0bd149404b59beaba25d3ef0ff60f122
Livedeht <godsbane@gmail.com>
2023-02-03 20:08:39 -0500

    Add cloud gaming (#611)
    * Create PutLaunchersHere.txt
    
    * Added cloud services parser in SRM config.
    Added scripts with matching SteamGridDB names for proper scraping.
    
    * Update userConfigurations.json
    
    Disabled "fuzzyMatch->removeCharacters" so that items such as "Disney+" and "Paramount+" with special characters can match correctly.
    
    * Created "cloud.conf" and edited the scripts.
    This allows for users to change the conf to use a different web browser.
    
    * Update README.md
    
    * Added cloudServicesManager.sh.
    Features:
    * Auto-update list based on github repo.
    * Updates exsisting scripts without touching user custom added scripts (based on file name).
    * Edit `cloud.conf` file to change default web browser for scripts.
    
    * Use the already pulled data. Since the cloud services manager will be
    executed from EmuDeck and it updates the backend we can use it.
    
    * add Chiaki Script
    
    * Streamline menu for cloudManager
    
    ---------
    
    Co-authored-by: Isaiah Koontz <isaiah.koontz@gmail.com>

---
+++ tools/cloud/Spotify.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+
+LINK="https://open.spotify.com/"
+
+. ./cloud.conf
+"/usr/bin/flatpak" run --branch=stable --arch=x86_64 --command=${COMMAND} --file-forwarding ${FILEFORWARDING} @@u @@ --window-size=${WINDOWSIZE} --force-device-scale-factor=${DEVICESCALEFACTOR} --device-scale-factor=${DEVICESCALEFACTOR} --kiosk "${LINK}"
---
+++ tools/cloud/chiaki.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/usr/bin/flatpak run re.chiaki.Chiaki
