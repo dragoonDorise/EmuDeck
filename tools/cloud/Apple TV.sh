28ff221930f7de566b79c8f5f3a278f073ea4ddd
Rodrigo <rodrigosedano@me.com>
2023-02-04 02:12:10 +0100

    Revert "Add cloud gaming (#611)"
    This reverts commit 9aa17126cf34073b6f370ecd9520a3ee4e5676bf.

--- tools/cloud/Apple TV.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-
-LINK="https://tv.apple.com/"
-
-. ./cloud.conf
-"/usr/bin/flatpak" run --branch=stable --arch=x86_64 --command=${COMMAND} --file-forwarding ${FILEFORWARDING} @@u @@ --window-size=${WINDOWSIZE} --force-device-scale-factor=${DEVICESCALEFACTOR} --device-scale-factor=${DEVICESCALEFACTOR} --kiosk "${LINK}"
--- tools/cloud/Hulu.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-
-LINK="https://www.hulu.com/"
-
-. ./cloud.conf
-"/usr/bin/flatpak" run --branch=stable --arch=x86_64 --command=${COMMAND} --file-forwarding ${FILEFORWARDING} @@u @@ --window-size=${WINDOWSIZE} --force-device-scale-factor=${DEVICESCALEFACTOR} --device-scale-factor=${DEVICESCALEFACTOR} --kiosk "${LINK}"
