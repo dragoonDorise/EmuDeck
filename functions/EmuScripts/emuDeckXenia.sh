e030ced9a6449370a85c5f65107d6f557cc08e3f
godsbane <godsbane@gmail.com>
2023-02-06 18:57:46 -0500

    fix xenia desktop naming

--- functions/EmuScripts/emuDeckXenia.sh
+++ functions/EmuScripts/emuDeckXenia.sh
@@ -45,7 +45,7 @@ Xenia_install(){
 	
 
 	createDesktopShortcut   "$HOME/.local/share/applications/xenia.desktop" \
-							"xenia EmuDeck" \
+							"Xenia Binary" \
 							"${toolsPath}/launchers/xenia.sh" \
 							"False"
 }
