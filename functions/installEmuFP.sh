5d45ad124e00f9c0a3adbd02851177b86b8fb136
godsbane <godsbane@gmail.com>
2023-02-05 23:37:03 -0500

    use local for variables in install functions fix some shellcheck warnings

--- functions/installEmuFP.sh
+++ functions/installEmuFP.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 installEmuFP(){		
 	
-	name=$1
-	ID=$2	
+	local name=$1
+	local ID=$2	
 	
 	setMSG "Installing $name"
 	
-	flatpak install flathub $ID -y --system	
-	flatpak override $ID --filesystem=host --user
-	flatpak override $ID --share=network --user	
+	flatpak install flathub "$ID" -y --system	
+	flatpak override "$ID" --filesystem=host --user
+	flatpak override "$ID" --share=network --user	
 	
 	shName=$(echo "$name" | awk '{print tolower($0)}')
 	
