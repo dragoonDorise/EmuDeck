33017537839eeb0d699e6ffc47bbc373bc720056
Rodrigo <rodrigosedano@me.com>
2023-02-25 23:15:54 +0100

    migration wip

--- functions/ToolScripts/emuDeckMigration.sh
+++ functions/ToolScripts/emuDeckMigration.sh
@@ -11,14 +11,47 @@ Migration_init(){
 	freeSpace=$(df -s "$destination" |  cut -d' ' -f8)
 	difference=$(($freeSpace - $neededSpace))
 	if [ $difference -gt 0 ]; then
-		Migration_move "$emulationPath" "$destination"
+		Migration_move "$emulationPath" "$destination"	
+	else
+		text="$(printf "<b>Not enough space</b>\nYou need to have at least ${neededSpace} on ${destination}")"
+	 	zenity --error \
+	 			--title="EmuDeck" \
+	 			--width=400 \
+	 			--text="${text}" 2>/dev/null		
 	fi 
 	
 }
 
 #We rsync, only when rsync is completed we delete the old folder.
 Migration_move(){
-	target=$1
+	origin=$1
 	destination=$2
-	rsync -avzh --dry-run "$target" "$destination" && echo "true"
+	rsync -avzh --dry-run "$origin" "$destination" && Migration_updatePaths $origin $destination
+
+}
+
+
+Migration_updatePaths(){
+	origin=$1
+	destination=$2
+	text="$(printf "<b>Success</b>\nYour library has been moved to ${destination}")"		
+	
+	#New settings
+	setSetting emulationPath "${origin}Emulation"
+	setSetting toolsPath "${origin}Emulation/tools"
+	setSetting romsPath "${origin}Emulation/roms"
+	setSetting biosPath "${origin}Emulation/bios"
+	setSetting savesPath "${origin}Emulation/saves"
+	setSetting storagePath "${origin}Emulation/storage"
+	setSetting ESDEscrapData "${origin}Emulation/tools/downloaded_media"
+
+	#Emu configs
+	#Redo all inits?	
+	# Cemu_init && Citra_init && Dolphin_init && ....
+	
+	
+    zenity --info \
+		 --title="EmuDeck" \
+		 --width=400 \
+		 --text="${text}" 2>/dev/null	
 }
