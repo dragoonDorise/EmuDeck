c377cf767693cee6e249799731e36eb96f550ba7
Rodrigo <rodrigosedano@me.com>
2023-02-25 23:15:54 +0100

    migration wip

--- functions/ToolScripts/emuDeckMigration.sh
+++ functions/ToolScripts/emuDeckMigration.sh
@@ -11,14 +11,83 @@ Migration_init(){
 	freeSpace=$(df -s "$destination" |  cut -d' ' -f8)
 	difference=$(($freeSpace - $neededSpace))
 	if [ $difference -gt 0 ]; then
-		Migration_move "$emulationPath" "$destination"
+		Migration_move "$emulationPath" "$destination"	
+	else
+		text="$(printf "<b>Not enough space</b>\nYou need to have at least ${neededSpace} on ${destination}")"
+	 	zenity --error \
+	 			--title="EmuDeck" \
+	 			--width=400 \
+	 			--text="${text}" 2>/dev/null		
 	fi 
 	
 }
 
 #We rsync, only when rsync is completed we delete the old folder.
 Migration_move(){
-	target=$1
+	origin=$1
 	destination=$2
-	rsync -avzh --dry-run "$target" "$destination" && echo "true"
+	rsync -avzh --dry-run "$origin" "$destination" && Migration_updatePaths "$origin" "$destination"
+}
+
+
+Migration_updatePaths(){
+	origin=$1
+	destination=$2
+		
+	
+	#New settings
+	setSetting emulationPath "${origin}Emulation"
+	setSetting toolsPath "${origin}Emulation/tools"
+	setSetting romsPath "${origin}Emulation/roms"
+	setSetting biosPath "${origin}Emulation/bios"
+	setSetting savesPath "${origin}Emulation/saves"
+	setSetting storagePath "${origin}Emulation/storage"
+	setSetting ESDEscrapData "${origin}Emulation/tools/downloaded_media"
+
+	#Emu configs
+	#Cemu
+	sed -i "/${origin}/c\\${destination}" "$Cemu_cemuSettings"
+	#Citra
+	sed -i "/${origin}/c\\${destination}" "$Citra_configFile"
+	#Dolphin
+	sed -i "/${origin}/c\\${destination}" "$HOME/.var/app/org.DolphinEmu.dolphin-emu/config/dolphin-emu/Dolphin.ini"
+	#Duckstation
+	sed -i "/${origin}/c\\${destination}" "$DuckStation_configFileNew"
+	#Mame
+	sed -i "/${origin}/c\\${destination}" "$MAME_configFile"
+	#MelonDS
+	sed -i "/${origin}/c\\${destination}" "$melonDS_configFile"
+	#MGBA
+	sed -i "/${origin}/c\\${destination}" "$mGBA_configFile"
+	#PCSX2QT
+	sed -i "/${origin}/c\\${destination}" "$PCSX2QT_configFile"
+	#PPSSPP
+	sed -i "/${origin}/c\\${destination}" "$HOME/.var/app/${PPSSPP_emuPath}/config/ppsspp/PSP/SYSTEM/ppsspp.ini"
+	#Primehack
+	sed -i "/${origin}/c\\${destination}" ""$HOME/.var/app/${Primehack_emuPath}/config/dolphin-emu/Dolphin.ini""
+	#RetroArch
+	sed -i "/${origin}/c\\${destination}" "$RetroArch_configFile"
+	#RMG
+	sed -i "/${origin}/c\\${destination}" "$RMG_configFile"
+	#RPCS3
+	sed -i "/${origin}/c\\${destination}" "$HOME/.var/app/${RPCS3_emuPath}/config/rpcs3/vfs.yml"
+	#Ryujinx
+	sed -i "/${origin}/c\\${destination}" "$$HOME/.config/Ryujinx/Config.json"
+	#ScummVM
+	sed -i "/${origin}/c\\${destination}" "$ScummVM_configFile"
+	#Vita3K
+	sed -i "/${origin}/c\\${destination}" "$Vita3K_configFile"
+	#Xemu
+	sed -i "/${origin}/c\\${destination}" "$HOME/.var/app/app.xemu.xemu/data/xemu/xemu/xemu.toml"
+	#Xenia
+	sed -i "/${origin}/c\\${destination}" "$Xenia_XeniaSettings"
+	#Yuzu
+	sed -i "/${origin}/c\\${destination}" "$HOME/.config/yuzu/qt-config.ini"
+	
+	
+	text="$(printf "<b>Success</b>\nYour library has been moved to ${destination}")"	
+    zenity --info \
+		 --title="EmuDeck" \
+		 --width=400 \
+		 --text="${text}" 2>/dev/null	
 }
