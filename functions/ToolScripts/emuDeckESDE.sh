2bd88c372bc9e6617ccebbfa103a4e5e2c57480e
rawdatafeel <108900299+rawdatafeel@users.noreply.github.com>
2023-03-03 19:16:47 -0500

    ES-DE Steam Input Profile

--- functions/ToolScripts/emuDeckESDE.sh
+++ functions/ToolScripts/emuDeckESDE.sh
@@ -271,6 +271,11 @@ ESDE_setEmu(){
 	fi
 }
 
+ESDE_addSteamInputProfile(){
+	setMSG "Adding $ESDE_toolName Steam Input Profile."
+	rsync -r "$EMUDECKGIT/configs/steam-input/emulationstation-de_controller_config.vdf" "$HOME/.steam/steam/controller_base/templates/"
+}
+
 ESDE_IsInstalled(){
 	if [ -e "$ESDE_toolPath" ]; then
 		echo "true"
