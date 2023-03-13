570fa40e0d70a03dfbf851508798330bb5f42b4a
Godsbane <godsbane@gmail.com>
2023-03-13 00:35:58 -0400

    Duck: Remove old flatpak configs update configs to be closer to stock removed multitap added more hotkeys fix field name for cheevos

--- functions/EmuScripts/emuDeckDuckStation.sh
+++ functions/EmuScripts/emuDeckDuckStation.sh
@@ -4,7 +4,6 @@
 DuckStation_emuName="DuckStation"
 DuckStation_emuType="FlatPak"
 DuckStation_emuPath="org.duckstation.DuckStation"
-DuckStation_configFile="$HOME/.var/app/org.duckstation.DuckStation/data/duckstation/settings.ini"
 DuckStation_configFileNew="$HOME/.var/app/org.duckstation.DuckStation/config/duckstation/settings.ini"
 
 #cleanupOlderThings
@@ -45,13 +44,13 @@ DuckStation_setEmulationFolder(){
 	setMSG "Setting $DuckStation_emuName Emulation Folder"	
     gameDirOpt='RecursivePaths = '
     newGameDirOpt="${gameDirOpt}""${romsPath}/psx"
+
 	biosDir='SearchDirectory = '
 	biosDirSetting="${biosDir}""${biosPath}"
-    sed -i "/${gameDirOpt}/c\\${newGameDirOpt}" "$DuckStation_configFile"
-    sed -i "/${biosDir}/c\\${biosDirSetting}" "$DuckStation_configFile"
 
 	statesDir='SaveStates = '
 	statesDirSetting="${statesDir}""${savesPath}/duckstation/states"
+
 	memCardDir='Directory = '
 	memCardDirSetting="${memCardDir}""${savesPath}/duckstation/saves"
 
@@ -108,8 +107,6 @@ DuckStation_wideScreenOn(){
     wideScreenHackSetting='WidescreenHack = true'
     aspectRatio='AspectRatio = '
     aspectRatioSetting='AspectRatio = 16:9'
-    sed -i "/${wideScreenHack}/c\\${wideScreenHackSetting}" "$DuckStation_configFile"
-	sed -i "/${aspectRatio}/c\\${aspectRatioSetting}" "$DuckStation_configFile"
 	sed -i "/${wideScreenHack}/c\\${wideScreenHackSetting}" "$DuckStation_configFileNew"
 	sed -i "/${aspectRatio}/c\\${aspectRatioSetting}" "$DuckStation_configFileNew"
 }
@@ -122,8 +119,6 @@ DuckStation_wideScreenOff(){
     wideScreenHackSetting='WidescreenHack = false'
     aspectRatio='AspectRatio = '
     aspectRatioSetting='AspectRatio = 4:3'
-    sed -i "/${wideScreenHack}/c\\${wideScreenHackSetting}" "$DuckStation_configFile"
-	sed -i "/${aspectRatio}/c\\${aspectRatioSetting}" "$DuckStation_configFile"
 	sed -i "/${wideScreenHack}/c\\${wideScreenHackSetting}" "$DuckStation_configFileNew"
 	sed -i "/${aspectRatio}/c\\${aspectRatioSetting}" "$DuckStation_configFileNew"
 	
@@ -188,8 +183,8 @@ DuckStation_retroAchievementsSetLogin(){
 		echo "--No username."
 	else
 		echo "Valid Retroachievements Username and Password length"
-		iniFieldUpdate "$DuckStation_configFileNew" "Achievements" "Username" "$rau"
-		iniFieldUpdate "$DuckStation_configFileNew" "Achievements" "Token" "$rat"
+		iniFieldUpdate "$DuckStation_configFileNew" "Cheevos" "Username" "$rau"
+		iniFieldUpdate "$DuckStation_configFileNew" "Cheevos" "Token" "$rat"
 		PCSX2QT_retroAchievementsOn
 	fi
 }
