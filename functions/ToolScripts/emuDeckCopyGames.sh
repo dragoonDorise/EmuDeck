4e40e80d8921a8520410e60a7baa180cd0e3e54e
Rodrigo <rodrigosedano@me.com>
2023-03-13 15:58:45 +0100

    auto-close ftw

--- functions/ToolScripts/emuDeckCopyGames.sh
+++ functions/ToolScripts/emuDeckCopyGames.sh
@@ -6,7 +6,7 @@ CreateStructureUSB(){
 	rsync -rav --ignore-existing "$EMUDECKGIT/roms/" "$destination/roms/"|
 	awk -f $HOME/.config/EmuDeck/backend/rsync.awk |
 	zenity --progress --title "Creating Rom Structure on $destination" \
-	--text="Scanning..." --width=400 --percentage=0 --auto-kill
+	--text="Scanning..." --width=400 --percentage=0 --auto-close
 	
 	text="`printf " <b>Folders created</b>\n\nEject your USB Drive and go to your computer and copy your roms to the folders created on $destination/roms/ and your bios on $destination/bios/)"`"
 	 zenity --info \
@@ -30,12 +30,12 @@ CopyGames(){
 		rsync -rav --ignore-existing --progress "$origin/roms/" "$romsPath/" |
 		awk -f $HOME/.config/EmuDeck/backend/rsync.awk |
 		zenity --progress --title "Importing your games to $romsPath" \
-		--text="Scanning..." --width=400 --percentage=0 --auto-kill
+		--text="Scanning..." --width=400 --percentage=0 --auto-close
 		
 		rsync -rav --ignore-existing --progress "$origin/bios/" "$biosPath/" |
 		awk -f $HOME/.config/EmuDeck/backend/rsync.awk |
 		zenity --progress --title "Importing your games to $biosPath" \
-		--text="Scanning..." --width=400 --percentage=0 --auto-kill
+		--text="Scanning..." --width=400 --percentage=0 --auto-close
 	else
 		text="$(printf "<b>Not enough space</b>\nYou need to have at least ${neededSpaceInHuman} on ${emulationPath}\nYou only have ${freeSpaceInHuman}")"
 		 zenity --error \
