cea7d8da563b6d6f4956f8da1756bca628924dc6
SilentException <mitja.skuver@gmail.com>
2023-04-09 23:54:35 +0200

    flatpakupdate - user feedback
    added emu selection and zenity progress inf flatpakupdate for better user feedback on what's going on
    small change in updateEmuFP - verbose cp

--- functions/updateEmuFP.sh
+++ functions/updateEmuFP.sh
@@ -13,7 +13,7 @@ updateEmuFP(){
 	shName=$(echo "$name" | awk '{print tolower($0)}')
 	
 	find "${toolsPath}"/launchers/ -type f -iname $shName.sh | while read f; do echo "deleting $f"; rm -f "$f"; done;
-	cp "${EMUDECKGIT}"/tools/launchers/"${shName}".sh "${toolsPath}"/launchers/"${shName}".sh
+	cp -v "${EMUDECKGIT}"/tools/launchers/"${shName}".sh "${toolsPath}"/launchers/"${shName}".sh
 	chmod +x "${toolsPath}"/launchers/"${shName}".sh
 	
 }
