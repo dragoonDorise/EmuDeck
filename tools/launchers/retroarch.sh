cfdcc330260d6562e2f4159bcef31b180248d76a
Rodrigo <rodrigosedano@me.com>
2023-04-23 23:40:36 +0200

    using sh

--- tools/launchers/retroarch.sh
+++ tools/launchers/retroarch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 source $HOME/.config/EmuDeck/backend/functions/all.sh
-rclone_downloadEmu retroarch
+rclone_downloadEmu retroarch "${@}"
 /usr/bin/flatpak run org.libretro.RetroArch
 rclone_uploadEmu retroarch
