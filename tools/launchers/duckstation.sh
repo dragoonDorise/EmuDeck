9f5a3ffe5b4b05ae82e7501d54773bae45e79f2b
Rodrigo <rodrigosedano@me.com>
2023-04-24 00:19:55 +0200

    "${@}"

--- tools/launchers/duckstation.sh
+++ tools/launchers/duckstation.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 source $HOME/.config/EmuDeck/backend/functions/all.sh
 rclone_downloadEmu duckstation
-/usr/bin/flatpak run org.duckstation.DuckStation -bigpicture -fullscreen
+/usr/bin/flatpak run org.duckstation.DuckStation "${@}"
 rclone_uploadEmu duckstation
--- tools/launchers/emulationstationde.sh
+++ tools/launchers/emulationstationde.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 source $HOME/.config/EmuDeck/backend/functions/all.sh
 rclone_downloadEmu esde
-$toolsPath/EmulationStation-DE-x64_SteamDeck.AppImage
+$toolsPath/EmulationStation-DE-x64_SteamDeck.AppImage "${@}"
 rclone_uploadEmu esde
