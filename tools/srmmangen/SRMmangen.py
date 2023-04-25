57de502648c2171c9d44145e485739b954433539
d0k3 <tore.anon@gmail.com>
2023-04-25 13:18:41 +0200

    Improved SRMmangen.py, fetches full config from SRM

--- tools/srmmangen/SRMmangen.py
+++ tools/srmmangen/SRMmangen.py
@@ -3,7 +3,18 @@ import os
 import re
 import json
 import argparse
-import xml.etree.ElementTree as et
+import xml.etree.ElementTree as ET
+
+
+# default paths for EmuDeck - we may need to edit those for compatibility with other setups
+
+def_output = './manifest.json'
+def_gamelist_root = '/home/deck/.emulationstation/gamelists'
+def_srm_userdata = '/home/deck/.config/steam-rom-manager/userData'
+
+
+# no edits should be required below this line
+# -------------------------------------------
 
 def log(type, message, quiet = False):
     if not quiet:
@@ -13,15 +24,15 @@ def log(type, message, quiet = False):
 cfg = {}
 
 # say hello
-print('<SRM manifest.json generator 0.2 (c) 2023 d0k3>')
+print('<SRM manifest.json generator 0.3 (c) 2023 d0k3>')
 
 # parse arguments
 parser = argparse.ArgumentParser()
 parser.add_argument('-c', '--config', type=str, help='specify configuration to load')
+parser.add_argument('-s', '--srm-userdata', type=str, default=def_srm_userdata, help='specify SRM userData path')
 parser.add_argument('-r', '--roms-root', type=str, help='specify roms root path')
-parser.add_argument('-g', '--gamelist-root', type=str, help='specify gamelist root path (can be same as roms root)')
-parser.add_argument('-o', '--output', type=str, default='./manifest.json', help='specify output destination for manifest.json')
-parser.add_argument('-i', '--import-srm-parsers', type=str, help='specify SRM userConfigurations.json to import (alpha!)')
+parser.add_argument('-g', '--gamelist-root', type=str, default=def_gamelist_root, help='specify gamelist root path (can be same as roms root)')
+parser.add_argument('-o', '--output', type=str, default=def_output, help='specify output destination for manifest.json')
 parser.add_argument('-d', '--dump-config', type=str, help='dump config to specied destination and exit')
 parser.add_argument('-p', '--pause', help='wait after finishing process', action='store_true')
 parser.add_argument('-q', '--quiet', help='suppress most on screen output', action='store_true')
@@ -36,31 +47,40 @@ if args.roms_root:
 if args.gamelist_root:
     cfg['gamelist_root'] = args.gamelist_root
 output_path = args.output
+
+# if specified: take over manifest templates from SRM userConfigurations.json
+if args.srm_userdata and not args.config:
+    retroarchPath = ''
+    raCoresDirectory = ''
+    with open(os.path.join(args.srm_userdata, 'userSettings.json'), 'r', encoding = 'utf-8') as fp:
+        srmset = json.load(fp)
+        srmenv = srmset['environmentVariables']
+        retroarchPath = srmenv['retroarchPath']
+        raCoresDirectory = srmenv['raCoresDirectory']
+        cfg['roms_root'] = srmenv['romsDirectory']
+        
+    with open(os.path.join(args.srm_userdata, 'userConfigurations.json'), 'r', encoding = 'utf-8') as fp:
+        srmconfig = json.load(fp)
+        cfg['manifest_templates'] = {}
+        for p in srmconfig:
+            s = re.search('\${romsdirglobal}\/([^\/]+)', p['romDirectory'])
+            if not p['executableArgs'] or not s:
+                # most likely not an emulator
+                continue
+            e = {}
+            e['subdir'] = s.group(1)
+            e['emulator'] = p ['executable']['path'].replace('${retroarchpath}', retroarchPath)
+            e['args'] = p['executableArgs'].replace('${racores}', raCoresDirectory)
+            cfg['manifest_templates'][p['configTitle']] = e
                 
 # sanity check
 if not (('roms_root' in cfg) and cfg['roms_root']):
     parser.print_help()
     sys.exit(0)
 
-# mssing gamelist root? take it over from roms_root
+# missing gamelist root? take it over from roms_root
 if not (('gamelist_root' in cfg) and cfg['gamelist_root']):
     cfg['gamelist_root'] = cfg['roms_root']
-
-# if specified: take over manifest templates from SRM userConfigurations.json
-if args.import_srm_parsers:
-        with open(args.import_srm_parsers, 'r', encoding = 'utf-8') as fp:
-            srmconfig = json.load(fp)
-            cfg['manifest_templates'] = {}
-            for p in srmconfig:
-                s = re.search('\${romsdirglobal}\/([^\/]+)', p['romDirectory'])
-                if not p['executableArgs'] or not s:
-                    # most likely not an emulator
-                    continue
-                e = {}
-                e['subdir'] = s.group(1)
-                e['emulator'] = p ['executable']['path']
-                e['args'] = p['executableArgs']
-                cfg['manifest_templates'][p['configTitle']] = e
 
 # config summary
 log('config', f'{cfg["roms_root"]} (roms root)', args.quiet)
@@ -74,13 +94,16 @@ if args.dump_config:
     log('config', f'config dumped to {args.dump_config}')
     sys.exit(0)
 
-manifests = []
+manifests = [] # one entry per game
+processed_subddirs = [] # used to keep track of already processed subdirs
 for system in cfg["manifest_templates"]:
     # basic parameters
     template = cfg["manifest_templates"][system]
     subdir = template['subdir']
     emulator = template['emulator']
     options = template['args']
+    if subdir in processed_subddirs:
+        continue
     cfaves = 0
     log(f'{subdir}', f'System: {system}', args.quiet)
     
@@ -90,7 +113,7 @@ for system in cfg["manifest_templates"]:
         gamelist_path = os.path.join(cfg['gamelist_root'], subdir, 'gamelist.xml')
         with open(gamelist_path, mode = 'r', encoding = 'utf-8') as fp:
             fp.readline()
-            gamelist = et.fromstring('<?xml version="1.0"?>\n<root>' + fp.read() + '\n</root>').find('gameList').findall('game')
+            gamelist = ET.fromstring('<?xml version="1.0"?>\n<root>' + fp.read() + '\n</root>').find('gameList').findall('game')
     except Exception as e:
         pass    
     if not gamelist:
@@ -116,6 +139,7 @@ for system in cfg["manifest_templates"]:
         cfaves = cfaves + 1
 
     log(f'{subdir}', f'{cfaves} favorites found', args.quiet and not cfaves)
+    processed_subddirs.append(subdir)
     
 # write JSON file
 with open(output_path, 'w', encoding = 'utf-8', newline = '') as fp:
