b369e196d02ccaf3f8759d0f460a6a2e88d058e9
godsbane <godsbane@gmail.com>
2023-04-26 00:10:47 -0400

    move variable setting below trap, just in case

--- install.sh
+++ install.sh
@@ -2,9 +2,6 @@
 
 set -eo pipefail
 
-EMUDECK_GITHUB_URL="https://api.github.com/repos/EmuDeck/emudeck-electron/releases/latest"
-EMUDECK_URL="$(curl -s ${EMUDECK_GITHUB_URL} | grep -E 'browser_download_url.*AppImage' | cut -d '"' -f 4)"
-
 report_error() {
     FAILURE="$(caller): ${BASH_COMMAND}"
     echo "Something went wrong!"
@@ -12,6 +9,9 @@ report_error() {
 }
 
 trap report_error ERR
+
+EMUDECK_GITHUB_URL="https://api.github.com/repos/EmuDeck/emudeck-electron/releases/latest"
+EMUDECK_URL="$(curl -s ${EMUDECK_GITHUB_URL} | grep -E 'browser_download_url.*AppImage' | cut -d '"' -f 4)"
 
 mkdir -p ~/Applications
 curl -L "${EMUDECK_URL}" -o ~/Applications/EmuDeck.AppImage 2>&1 | stdbuf -oL tr '\r' '\n' | sed -u 's/^ *\([0-9][0-9]*\).*\( [0-9].*$\)/\1\n#Download Speed\:\2/' | zenity --progress --title "Downloading EmuDeck" --width 600 --auto-close --no-cancel 2>/dev/null
