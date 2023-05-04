b1b994bae6b13671cdeae5f38c0168de2cb157c0
Rodrigo <rodrigosedano@me.com>
2023-05-04 11:41:29 +0200

    no need

--- functions/ToolScripts/emuDeckCloudSync.sh
+++ functions/ToolScripts/emuDeckCloudSync.sh
@@ -51,7 +51,7 @@ cloud_sync_config(){
           echo "Cancel Nextcloud Login" 
       fi
   else
-      $cloud_sync_bin config update "$cloud_sync_provider" --rc-serve
+      $cloud_sync_bin config update "$cloud_sync_provider" && echo "true"
   fi
 
   #OLD TOKEN upload, not needed for now
