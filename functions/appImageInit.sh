9db597d68aa009d14b1ccbdb5e7f24dc94987056
Rodrigo <rodrigosedano@me.com>
2023-05-24 19:09:37 +0200

    appimage fix
    # Conflicts:
    #	functions/appImageInit.sh

--- functions/appImageInit.sh
+++ functions/appImageInit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 appImageInit() {
-	if ! declare -p variable &>/dev/null; then
-		setSetting cloud_sync_status "true"
+	if ! declare -p cloud_sync_status &>/dev/null && [ -f "$cloud_sync_bin" ]; then
+		setSetting cloud_sync_status true
 	fi
 }
