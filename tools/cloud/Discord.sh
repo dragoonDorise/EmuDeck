a4089978bc09b602e7fb91f9cf303b8d1dd58743
rawdatafeel <108900299+rawdatafeel@users.noreply.github.com>
2023-05-23 22:02:38 -0400

    Update URL to directly link to Discord
    Current URL links to website
    
    Old URL: https://discord.com
    
    New URL: https://discord.com/app

--- tools/cloud/Discord.sh
+++ tools/cloud/Discord.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-LINK="https://discord.com/"
+LINK="https://discord.com/app"
 
 source ./cloud.conf
 "/usr/bin/flatpak" run ${FLATPAKOPTIONS} ${BROWSERAPP} @@u @@ ${BROWSEROPTIONS} ${LINK}
