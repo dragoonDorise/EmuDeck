c846a3ad5c1b831420d457a403d210c3cac3724d
rawdatafeel <108900299+rawdatafeel@users.noreply.github.com>
2023-05-26 20:06:18 -0400

    Fix uninstall.sh script

--- uninstall.sh
+++ uninstall.sh
@@ -21,9 +21,8 @@ doUninstallSRM=true
 doUninstallESDE=true
 doUninstallMGBA=true
 doUninstallRMG=true
-doUninstallVita3K=true
 doUninstallares=true
-
+doUninstallVita3K=true
 
 
 
@@ -100,8 +99,10 @@ if [ "$doUninstall" == true ]; then
 				13 "Cemu Native" \
 				14 "Mame"  \
 				15 "RMG"  \
-				16 "Vita3K"  )
+				16 "Vita3K"  \
 				17 "ares"  )
+
+	ans=$?	
 	if [ $ans -eq 0 ]; then
 		
 		if [[ "$emusToUninstall" == *"RetroArch"* ]]; then
@@ -155,12 +156,12 @@ if [ "$doUninstall" == true ]; then
 		if [[ "$emusToUninstall" == *"RMG"* ]]; then
 			doUninstallRMG=false
 		fi
-		if [[ "$emusToUninstall" == *"Vita3K"* ]]; then
-			doUninstallVita3K=false
-		fi
 		if [[ "$emusToUninstall" == *"ares"* ]]; then
 			doUninstallares=false
-		fi					
+		fi
+		if [[ "$emusToUninstall" == *"Vita3K"* ]]; then
+			doUninstallVita3K=false
+		fi				
 		
 	else
 		exit
@@ -241,17 +242,13 @@ if [ "$doUninstall" == true ]; then
 		rm -rf ~/.config/mgba
 		rm -rf ~/.local/share/applications/mGBA.desktop &>> /dev/null
 	fi
-	if [[ "$doUninstallRMG" == true ]]; then
-		flatpak uninstall org.com.github.Rosalie241.RMG --system -y
-		rm -rf ~/.var/app/com.github.Rosalie241.RMG &>> /dev/null
+	if [[ "$doUninstallares" == true ]]; then
+		flatpak uninstall org.dev.ares.ares --system -y
+		rm -rf ~/.var/app/dev.ares.ares &>> /dev/null
 	fi
 	if [[ "$doUninstallVita3K" == true ]]; then
 		rm -rf ~/Applications/Vita3K &>> /dev/null
 		rm -rf ~/.local/share/applications/Vita3K.desktop &>> /dev/null
-	fi
-	if [[ "$doUninstallares" == true ]]; then
-		flatpak uninstall dev.ares.ares --system -y
-		rm -rf ~/.var/app/dev.ares.ares &>> /dev/null
 	fi
 	# if [[ "$doUninstallXenia" == true ]]; then
 	# 	rm -rf ~/Applications/Vita3K &>> /dev/null
@@ -281,8 +278,7 @@ if [ "$doUninstall" == true ]; then
 	rm -rf ~/.steam/steam/controller_base/templates/mGBA_controller_config.vdf  &>> /dev/null
 	rm -rf ~/.steam/steam/controller_base/templates/pcsx2_controller_config.vdf  &>> /dev/null
 	rm -rf ~/.steam/steam/controller_base/templates/ppsspp_controller_config.vdf  &>> /dev/null
-	rm -rf ~/.steam/steam/controller_base/templates/rmg_controller_config.vdf  &>> /dev/null
-	rm -rf ~/.steam/steam/controller_base/templates/ares_controller_config.vdf  &>> /dev/null		
+	rm -rf ~/.steam/steam/controller_base/templates/ares_controller_config.vdf  &>> /dev/null	
 	find  "$HOME/.steam/steam/tenfoot/resource/images/library/controller/binding_icons" -name 'EmuDeck*' -exec rm {} \;
 	
 	echo "65"
