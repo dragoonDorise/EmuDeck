78c9bc34b67f1c12b9bfdef990f504b018d5d4ae
Rodrigo <rodrigosedano@me.com>
2023-06-10 00:45:09 +0200

    Revert "Merge branch 'dev' into early"
    This reverts commit 2c727ee23dee18014dbcd601e3f28cd1ee889632, reversing
    changes made to d654fd0226abcef977ce3e5bdf8bb453c29fe9eb.

--- functions/all.sh
+++ functions/all.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-
-if [ -d "/Library" ]; then
-    deviceOS="macos"
-else
-    deviceOS=$(lsb_release -si)
-fi
-
 if [[ "$EMUDECKGIT" == "" ]]; then
     EMUDECKGIT="$HOME/.config/EmuDeck/backend"
 fi
