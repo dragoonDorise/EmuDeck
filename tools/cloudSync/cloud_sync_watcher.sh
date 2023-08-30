e5d3a218ecbfadf15d9f95988f373f8130d6098a
Rodrigo <rodrigosedano@me.com>
2023-08-30 17:39:56 +0200

    fix

--- tools/cloudSync/cloud_sync_watcher.sh
+++ tools/cloudSync/cloud_sync_watcher.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-echo "SERVICE - START"
-
+echo "SERVICE - START" > $HOME/emudeck/CloudSync.log
 source "$HOME/emudeck/settings.sh"
 source "$HOME/.config/EmuDeck/backend/functions/helperFunctions.sh"
 source "$HOME/.config/EmuDeck/backend/functions/ToolScripts/emuDeckCloudSync.sh"
@@ -9,7 +8,7 @@ source "$HOME/.config/EmuDeck/backend/functions/ToolScripts/emuDeckCloudSync.sh"
 touch "$savesPath/.watching"
 
 # Declare an array to store current hashes
-echo "SERVICE - declare"
+echo "SERVICE - declare" >> $HOME/emudeck/CloudSync.log
 declare -A current_hashes
 
 # Function to calculate the hash of a directory
@@ -21,7 +20,7 @@ calculate_hash() {
 
 # Extract the name of the folder immediately behind "saves"
 get_parent_folder_name() {
-  echo "SERVICE - get_parent_folder_name"
+  echo "SERVICE - get_parent_folder_name" >> $HOME/emudeck/CloudSync.log
   dir="$1"
   parent_dir=$(dirname "$dir")
   folder_name=$(basename "$parent_dir")
@@ -31,51 +30,54 @@ get_parent_folder_name() {
 # Initialize current hashes
 for dir in "$savesPath"/*/*; do
   if [ -d "$dir" ]; then    
-    current_hashes["$dir"]=$(calculate_hash "$dir")
+  current_hashes["$dir"]=$(calculate_hash "$dir")
   fi
 done
 
 # Loop that runs every second
 while [ 1 == 1 ]
 do
-  echo "SERVICE - LOOP"
+
   # Check for changes in hashes
   lastSavedDir=''
+  
   for dir in "${!current_hashes[@]}"; do
-    new_hash=$(calculate_hash "$dir")
-    #echo "$dir - ${current_hashes[$dir]}"
-    #echo "$dir - $new_hash"
-    
-    if [ "${current_hashes[$dir]}" != "$new_hash" ]; then
-      # Show the name of the folder immediately behind "saves"
-      
-      if [ "$lastSavedDir" != "$dir" ]; then  
-        emuName=$(get_parent_folder_name "$dir")
-        #cloud_sync_update
-        timestamp=$(date +%s)
-        echo "SERVICE - $emuName CHANGES DETECTED"
-        echo $timestamp > "$savesPath/$emuName/.pending_upload"
-        echo "SERVICE - UPLOAD"
-        cloud_sync_uploadEmu $emuName && rm -rf "$savesPath/$emuName/.pending_upload"
-        echo "SERVICE - UPLOADED"   
-        lastSavedDir=$dir      
-      else    
-        echo "SERVICE - IGNORED"   
-        lastSavedDir=''
-      fi
-      current_hashes["$dir"]=$new_hash
-    fi    
+  #echo -ne "." >> $HOME/emudeck/CloudSync.log
+  new_hash=$(calculate_hash "$dir")
+  # if [[ $dir == *"retroarch/states"* ]]; then
+  #   echo "$dir - ${current_hashes[$dir]}" >> $HOME/emudeck/CloudSync.log
+  #   echo "$dir - $new_hash" >> $HOME/emudeck/CloudSync.log
+  # fi
+  if [ "${current_hashes[$dir]}" != "$new_hash" ]; then
+    # Show the name of the folder immediately behind "saves"
+     echo "SERVICE - CHANGES DETECTED, LETS CHECK IF ITS A DUPLICATE" >> $HOME/emudeck/CloudSync.log
+    if [ "$lastSavedDir" != "$dir" ]; then  
+    emuName=$(get_parent_folder_name "$dir")
+    #cloud_sync_update
+    timestamp=$(date +%s)
+    echo "SERVICE - $emuName CHANGES DETECTED" >> $HOME/emudeck/CloudSync.log
+    echo $timestamp > "$savesPath/$emuName/.pending_upload"
+    echo "SERVICE - UPLOAD" >> $HOME/emudeck/CloudSync.log
+    cloud_sync_uploadEmu $emuName
+    rm -rf "$savesPath/$emuName/.pending_upload"
+    echo "SERVICE - UPLOADED" >> $HOME/emudeck/CloudSync.log   
+    lastSavedDir=$dir      
+    else    
+      echo "SERVICE - IGNORED" >> $HOME/emudeck/CloudSync.log   
+      lastSavedDir=''
+    fi
+    current_hashes["$dir"]=$new_hash
+  fi    
   done
 
   #Autostop service when everything has finished
   if [ ! -f "$savesPath/.watching" ]; then 
-    echo "SERVICE - NO WATCHING"   
-    if [ ! -f "$HOME/emudeck/cloud.lock" ]; then 
-    echo "SERVICE - NO LOCK"     
-      cloud_sync_stopService      
-    fi
+  echo "SERVICE - NO WATCHING" >> $HOME/emudeck/CloudSync.log   
+  if [ ! -f "$HOME/emudeck/cloud.lock" ]; then 
+  echo "SERVICE - NO LOCK - KILLING SERVICE" >> $HOME/emudeck/CloudSync.log     
+    cloud_sync_stopService      
+  fi
   fi
   
   sleep 1  # Wait for 1 second before the next iteration
 done
-echo "SERVICE - END"
