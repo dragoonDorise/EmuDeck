8a09bbe6258d46730c5a98f472c1d8cfbab2bbc7
Rodrigo <rodrigosedano@me.com>
2023-10-06 17:01:03 +0200

    fix shellcheck

--- darwin/functions/overrides/configEmuFP.sh
+++ darwin/functions/overrides/configEmuFP.sh
@@ -1,7 +1,8 @@
-configEmuFP(){		
-	
+#!/bin/bash
+configEmuFP(){
+
 	local name=$1
-	setMSG "Updating $name Config"	
+	setMSG "Updating $name Config"
 	rsync -avhpL --mkpath "$EMUDECKGIT/darwin/configs/${name}/" "$HOME/Library/Application Support/${name}/"
 
 }
