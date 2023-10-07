c00a188419dea89830e6e870d84e5cf3b1301116
Rodrigo <rodrigosedano@me.com>
2023-10-08 01:45:11 +0200

    =

--- functions/all.sh
+++ functions/all.sh
@@ -63,7 +63,7 @@ source "$EMUDECKGIT"/functions/EmuScripts/emuDeckCemuNative.sh
 source "$EMUDECKGIT"/functions/EmuScripts/emuDeckPCSX2.sh
 source "$EMUDECKGIT"/functions/EmuScripts/emuDeckRPCS3.sh
 source "$EMUDECKGIT"/functions/EmuScripts/emuDeckCitra.sh
-source "$EMUDECKGIT"/functions/EmuScripts/emuDeckDolphin.sh 
+source "$EMUDECKGIT"/functions/EmuScripts/emuDeckDolphin.sh
 source "$EMUDECKGIT"/functions/EmuScripts/emuDeckPrimehack.sh
 source "$EMUDECKGIT"/functions/EmuScripts/emuDeckRetroArch.sh
 source "$EMUDECKGIT"/functions/EmuScripts/emuDeckRyujinx.sh
@@ -92,9 +92,8 @@ source "$EMUDECKGIT"/functions/RemotePlayClientScripts/remotePlayGreenlight.sh
 #source "$EMUDECKGIT"/EmuScripts/emuDeckMAMEProton.sh
 
 # Darwin ovewrites
-if [ "$system" == "darwin" ]; then
-    
+if [ "$system" = "darwin" ]; then
     source "$EMUDECKGIT/darwin/functions/helperFunctions.sh"
     source "$EMUDECKGIT/darwin/functions/overrides.sh"
-    
+
 fi
