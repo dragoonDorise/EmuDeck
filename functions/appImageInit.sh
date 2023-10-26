b8d0c48fda9f6d6893f6f7c5709e1324610973b7
Rodrigo <rodrigosedano@me.com>
2023-10-26 13:53:23 +0200

    juck

--- functions/appImageInit.sh
+++ functions/appImageInit.sh
@@ -3,7 +3,7 @@ appImageInit() {
 
 	#Autofixes, put here functions that make under the hood fixes.
 	autofix_duplicateESDE
-	autofix_raSavesFolders
+	#autofix_raSavesFolders
 	autofix_lnk
 	SRM_migration
 	ESDE_migration
