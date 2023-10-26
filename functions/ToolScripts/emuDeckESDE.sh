011b775383145bebfd40b8b968caa211a0e2ecc6
Rodrigo <rodrigosedano@me.com>
2023-10-26 23:20:54 +0200

    chimeraOS

--- functions/ToolScripts/emuDeckESDE.sh
+++ functions/ToolScripts/emuDeckESDE.sh
@@ -111,6 +111,19 @@ ESDE_init(){
 	ESDE_finalize
 	ESDE_migrateEpicNoir
 
+
+	if [ "$system" == "chimeraOS" ] || [ "$system" == "ChimeraOS" ]; then
+		ESDE_chimeraOS
+	fi
+
+}
+
+ESDE_chimeraOS(){
+	if [ ! -f $es_rulesFile ]; then
+		rsync -avhp --mkpath "$EMUDECKGIT/chimeraOS/configs/emulationstation/custom_systems/es_find_rules.xml" "$(dirname "$es_rulesFile")" --backup --suffix=.bak
+	else
+		xmlstarlet ed -d '//entry[contains(., "~/Applications/RetroArch-Linux*.AppImage") or contains(., "~/.local/share/applications/RetroArch-Linux*.AppImage") or contains(., "~/.local/bin/RetroArch-Linux*.AppImage") or contains(., "~/bin/RetroArch-Linux*.AppImage")]' $es_rulesFile > rules_temp.xml && mv rules_temp.xml $es_rulesFile
+	fi
 }
 
 
