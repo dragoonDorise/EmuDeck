2bc2dfe9c644a8842615a46b50467d6e26251ca9
Rodrigo <rodrigosedano@me.com>
2023-11-02 12:14:49 +0100

    chimera install

--- functions/appImageInit.sh
+++ functions/appImageInit.sh
@@ -1,18 +1,6 @@
 #!/bin/bash
 appImageInit() {
 
-	if [ "$system" == "chimeraos" ]; then
-		ESDE_chimeraOS
-		mkdir -p $HOME/Applications
-
-		downloads_dir="$HOME/Downloads"
-		destination_dir="$HOME/Applications"
-		file_name="EmuDeck"
-
-		find "$downloads_dir" -type f -name "*$file_name*.AppImage" -exec mv {} "$destination_dir/$file_name.AppImage" \;
-
-	fi
-
 	#Autofixes, put here functions that make under the hood fixes.
 	autofix_duplicateESDE
 	#autofix_raSavesFolders
