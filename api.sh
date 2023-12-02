33b90a89d3b89a6e9840a3b50a66a4c65bdbad72
Rodrigo <rodrigosedano@me.com>
2023-12-03 00:13:05 +0100

    API + fixes decky

---
+++ api.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+. "$HOME/.config/EmuDeck/backend/functions/all.sh"
+
+API_pull(){
+	local branch = $1
+	cd ~/.config/EmuDeck/backend && touch ~/emudeck/logs/git.log && script ~/emudeck/logs/git.log -c 'git reset --hard && git clean -fd && git checkout $branch && git pull' && . ~/.config/EmuDeck/backend/functions/all.sh && appImageInit
+}
+
+API_autoSave(){
+	RetroArch_autoSave
+}
+
+API_bezels(){
+	RetroArch_setBezels
+}
+
+API_shaders_LCD(){
+	RetroArch_setShadersMAT
+}
+
+API_shaders_2D(){
+	RetroArch_setShadersCRT
+}
+
+API_shaders_3D(){
+	RetroArch_setShaders3DCRT
+}
+
+API_ar_snes(){
+	if [ "$arSnes" == 87 ]; then
+		RetroArch_snes_ar87
+		RetroArch_nes_ar87
+	else
+		RetroArch_snes_ar43
+		RetroArch_nes_ar43
+	fi
+}
+
+
+API_setAR(){
+	RetroArch_setCustomizations
+	Xemu_setCustomizations
+	DuckStation_setCustomizations
+	PCSX2QT_setCustomizations
+	Dolphin_setCustomizations
+}
+
+API_setCloud(){
+  if [ $cloud_sync_status == "false" ]; then
+	setSetting cloud_sync_status "true" > /dev/null
+  else
+	  setSetting cloud_sync_status "false" > /dev/null
+  fi
+}
