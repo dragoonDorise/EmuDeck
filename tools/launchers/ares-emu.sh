d1ecf812f016cfa1c69fa77ce772500e17d50c81
rawdatafeel <108900299+rawdatafeel@users.noreply.github.com>
2024-01-01 12:06:01 -0500

    [ares] * Updated ares config to v134 * Removed data folder download (obsolete in v134) * Added quark shader download * Added back ares Steam ROM Manager parsers * Added SNES peripheral parsers for Steam ROM Manager * Added SNES peripheral custom systems for EmulationStation-DE

--- tools/launchers/ares-emu.sh
+++ tools/launchers/ares-emu.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 source $HOME/.config/EmuDeck/backend/functions/all.sh
 cloud_sync_downloadEmu "ares" && cloud_sync_startService
+
+cd $biosPath
+
 /usr/bin/flatpak run dev.ares.ares "${@}"
 rm -rf "$savesPath/.gaming"
