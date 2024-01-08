f303e7b6b2c06dc157a3842e3a51b95b3d9689f5
Dragoon Dorise <rodrigosedano@me.com>
2024-01-08 14:42:15 +0100

    fix PCSX2

--- tools/launchers/pcsx2-qt.sh
+++ tools/launchers/pcsx2-qt.sh
@@ -9,15 +9,15 @@ exe=$(find $emufolder -iname "${emuName}*.AppImage" | sort -n | cut -d' ' -f 2- 
 
 #if appimage doesn't exist fall back to flatpak.
 if [[ $exe == '' ]]; then
-    #flatpak
-    flatpakApp=$(flatpak list --app --columns=application | grep $emuName)
-    exe="/usr/bin/flatpak run "$flatpakApp
+	#flatpak
+	flatpakApp=$(flatpak list --app --columns=application | grep $emuName)
+	exe="/usr/bin/flatpak run "$flatpakApp
 else
 #make sure that file is executable
-    chmod +x $exe
+	chmod +x $exe
 fi
 #run the executable with the params.
 #Fix first '
 param="${@}"
-param=$(echo "$param" | sed "s|'|/\"|g")
+param=$(echo "$param" | sed "s|'|\"|g")
 eval "${exe} ${param} -bigpicture -fullscreen -batch"
