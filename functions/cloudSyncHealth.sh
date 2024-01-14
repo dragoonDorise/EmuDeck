158cbc323364293e7a166e8ca2e7e5f0f0fcf8ea
Dragoon Dorise <rodrigosedano@me.com>
2024-01-14 01:19:24 +0100

    cloudSyncHealth

--- functions/cloudSyncHealth.sh
+++ functions/cloudSyncHealth.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-NONE='\033[00m'
-RED='\033[01;31m'
-GREEN='\033[01;32m'
-YELLOW='\033[01;33m'
-CYAN='\033[01;36m'
 cloud_sync_upload_test(){
 	local emuName=$1
 
@@ -31,82 +26,48 @@ cloud_sync_dowload_test(){
 
 cloudSyncHealth(){
 
-	echo -e "<span class=\"cyan\">CloudSync Status Report</span>"
-	echo ""
+	#Check installation
+	if [ ! -f "$cloud_sync_bin" ]; then
+  		echo "nobinary"
+  		exit
+	elif [ ! -f "$cloud_sync_config" ]; then
+  		echo "noconfig"
+  		exit
+	elif [ $cloud_sync_provider = '' ]; then
+  		echo "noprovider"
+  		exit
+	fi
 
+	#Test emulators
 	miArray=("Cemu" "citra" "dolphin" "duckstation" "MAME" "melonds" "mgba" "pcsx2" "ppsspp" "primehack" "retroarch" "rpcs3" "scummvm" "Vita3K" "yuzu" "ryujinx" )
 
-	upload="true"
-	download="true"
-	launchers="true"
-	echo -e "<span class=\"yellow\">Checking launchers</span>"
-	for entry in "$toolsPath/launchers/"*.sh
-	do
-		if [ -f "$entry" ]; then
-			if grep -q "cloud_sync_startService" $entry; then
-				echo -e "$entry: <span class=\"green\">Success</span>"
-			else
-				echo -e "$entry: <span class=\"red\">Failure</span>"
-				launchers="false"
-			fi
-		fi
-	done
-
-	if grep -q "cloud_sync_startService" "$toolsPath/launchers/esde/emulationstationde.sh"; then
-		echo -e "$toolsPath/launchers/esde/emulationstationde.sh: <span class=\"green\">Success</span>"
-	else
-		echo -e "$toolsPath/launchers/esde/emulationstationde.sh: <span class=\"red\">Failure</span>"
-	fi
-
-	found_files="false"
-	echo -e "<span class=\"yellow\">Checking for Windows old .lnk files</span>"
-	find "$savesPath" -type f -name "*.lnk" | while read -r entry
-	do
-		rm -rf $entry
-		echo "found and deleted: $entry"
-		found_files="true"
-	done
-
-	if [ "$found_files" = "false" ]; then
-		echo "No files with the '.lnk' extension found."
-	fi
-
-	echo -e ""
-	echo -e "<span class=\"yellow\">Testing uploading</span>"
-	# Recorrer el array y ejecutar la función cloud_sync_upload_test para cada elemento
+#	echo -e "<span class=\"yellow\">Testing uploading</span>"
 	for elemento in "${miArray[@]}"; do
-		echo -ne "Testing $elemento upload..."
+#		echo -ne "Testing $elemento upload..."
 		if cloud_sync_upload_test $elemento;then
-			echo -e "<span class=\"green\">Success</span>"
+			echo ""
 		elif [ $? = 2 ]; then
-			echo -e "<span class=\"yellow\">Save folder not found</span>"
+			echo ""
+			#echo "Error: Testing $elemento upload, save folder not found"
 		else
-			echo -e "<span class=\"red\">Failure</span>"
-			upload="false"
+			echo "Error: Testing $elemento upload"
+			exit
 		fi
 	done
-	echo ""
-	echo -e "<span class=\"yellow\">Testing downloading</span>"
-	# Recorrer el array y ejecutar la función cloud_sync_upload_test para cada elemento
+
+#	echo -e "<span class=\"yellow\">Testing downloading</span>"
 	for elemento in "${miArray[@]}"; do
-		echo -ne "Testing $elemento download..."
+#		echo -ne "Testing $elemento download..."
 		if cloud_sync_dowload_test $elemento;then
-			echo -e "<span class=\"green\">Success</span>"
+			echo ""
 		elif [ $? = 2 ]; then
-			echo -e "<span class=\"yellow\">Save folder not found</span>"
+			echo ""
+			#echo "Error: Testing $elemento download, save folder not found"
 		else
-			echo -e "<span class=\"red\">Failure</span>"
-			download="false"
+			echo "Error: Testing $elemento download"
+			exit
 		fi
 	done
-	echo -e ""
-	echo -e "<span class=\"cyan\">Recommendations</span>"
 
-
-
-	if [ $download = "true" ] && [ $upload = "true" ] && [ $launchers = "true" ]; then
-		echo -e "<span class=\"yellow\">Everything seems to be in proper order, at least on Linux</span>"
-	else
-		echo -e "<span class=\"yellow\">Open EmuDeck, go to Manage Emulators and reset SteamRomManager Configuration. Then test some games and if it keeps failing open Steam Rom Manager and parse all your games again to get the proper launchers</span>"
-	fi
+	echo "true"
 }
