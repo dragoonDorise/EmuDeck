325bf60045f6af6bf1b3dd2e9d98451c8232ca5e
Dragoon Dorise <rodrigosedano@me.com>
2024-01-29 13:46:31 +0100

    emudeckgit

--- android/setup.sh
+++ android/setup.sh
@@ -73,7 +73,9 @@ curl -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-
 ## set backend location
 ##
 #
-
+if [[ "$EMUDECKGIT" == "" ]]; then
+	EMUDECKGIT="$HOME/.config/EmuDeck/backend"
+fi
 #
 ##
 ## Start of installation
