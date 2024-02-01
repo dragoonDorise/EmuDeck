0d1817eeee1e57a56d627fd2e2495a016ff50eb7
Dragoon Dorise <rodrigosedano@me.com>
2024-02-01 14:04:15 +0100

    supermodel launcher fix

--- tools/launchers/supermodel.sh
+++ tools/launchers/supermodel.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 source $HOME/.config/EmuDeck/backend/functions/all.sh
 cloud_sync_downloadEmu "supermodel" && cloud_sync_startService
-/usr/bin/flatpak run com.supermodel3.Supermodel "${@}"
+param="${@}"
+param=$(echo "$param" | sed "s|'|\"|g")
+/usr/bin/flatpak run com.supermodel3.Supermodel "${param}"
 rm -rf "$savesPath/.gaming"
