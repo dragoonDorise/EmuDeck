923361a18638485bf898f1d1acec580a22e3a86b
Dragoon Dorise <rodrigosedano@me.com>
2024-02-09 11:23:24 +0100

    darwin overrides

--- darwin/functions/overrides.sh
+++ darwin/functions/overrides.sh
@@ -30,3 +30,195 @@ source "$EMUDECKGIT/darwin/functions/overrides/installToolAI.sh"
 source "$EMUDECKGIT/darwin/functions/overrides/emuDeckRetroArch.sh"
 source "$EMUDECKGIT/darwin/functions/overrides/emuDeckPegasus.sh"
 
+
+ares_install(){
+	echo "NYI"
+}
+BigPEmu_install(){
+	echo "NYI"
+}
+Cemu_install(){
+	echo "NYI"
+}
+CemuProton_install(){
+	echo "NYI"
+}
+Citra_install(){
+	echo "NYI"
+}
+CitraLegacy_install(){
+	echo "NYI"
+}
+Dolphin_install(){
+	echo "NYI"
+}
+DuckStation_install(){
+	echo "NYI"
+}
+Flycast_install(){
+	echo "NYI"
+}
+MAME_install(){
+	echo "NYI"
+}
+MelonDS_install(){
+	echo "NYI"
+}
+MGBA_install(){
+	echo "NYI"
+}
+Model2_install(){
+	echo "NYI"
+}
+PCSX2QT_install(){
+	echo "NYI"
+}
+PPSSPP_install(){
+	echo "NYI"
+}
+Primehack_install(){
+	echo "NYI"
+}
+RMG_install(){
+	echo "NYI"
+}
+RPCS3_install(){
+	echo "NYI"
+}
+RPCS3Legacy_install(){
+	echo "NYI"
+}
+Ryujinx_install(){
+	echo "NYI"
+}
+ScummVM_install(){
+	echo "NYI"
+}
+Supermodel_install(){
+	echo "NYI"
+}
+Vita3K_install(){
+	echo "NYI"
+}
+Xemu_install(){
+	echo "NYI"
+}
+Xenia_install(){
+	echo "NYI"
+}
+Yuzu_install(){
+	echo "NYI"
+}
+
+
+
+BINUP_install(){
+	echo "NYI"
+}
+CHD_install(){
+	echo "NYI"
+}
+CloudBackup_install(){
+	echo "NYI"
+}
+CloudSync_install(){
+	echo "NYI"
+}
+CopyGames_install(){
+	echo "NYI"
+}
+InstallHomebrewGames_install(){
+	echo "NYI"
+}
+Migration_install(){
+	echo "NYI"
+}
+Pegasus_install(){
+	echo "NYI"
+}
+Plugins_install(){
+	echo "NYI"
+}
+RemotePlayWhatever_install(){
+	echo "NYI"
+}
+
+
+ares_init(){
+	echo "NYI"
+}
+BigPEmu_init(){
+	echo "NYI"
+}
+Cemu_init(){
+	echo "NYI"
+}
+CemuProton_init(){
+	echo "NYI"
+}
+Citra_init(){
+	echo "NYI"
+}
+CitraLegacy_init(){
+	echo "NYI"
+}
+Dolphin_init(){
+	echo "NYI"
+}
+DuckStation_init(){
+	echo "NYI"
+}
+Flycast_init(){
+	echo "NYI"
+}
+MAME_init(){
+	echo "NYI"
+}
+MelonDS_init(){
+	echo "NYI"
+}
+MGBA_init(){
+	echo "NYI"
+}
+Model2_init(){
+	echo "NYI"
+}
+PCSX2QT_init(){
+	echo "NYI"
+}
+PPSSPP_init(){
+	echo "NYI"
+}
+Primehack_init(){
+	echo "NYI"
+}
+RMG_init(){
+	echo "NYI"
+}
+RPCS3_init(){
+	echo "NYI"
+}
+RPCS3Legacy_init(){
+	echo "NYI"
+}
+Ryujinx_init(){
+	echo "NYI"
+}
+ScummVM_init(){
+	echo "NYI"
+}
+Supermodel_init(){
+	echo "NYI"
+}
+Vita3K_init(){
+	echo "NYI"
+}
+Xemu_init(){
+	echo "NYI"
+}
+Xenia_init(){
+	echo "NYI"
+}
+Yuzu_init(){
+	echo "NYI"
+}
