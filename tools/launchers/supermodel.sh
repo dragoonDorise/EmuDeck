e30680a018b1e20ab95628438aaa051588bd11eb
Dragoon Dorise <rodrigosedano@me.com>
2024-02-15 19:50:15 +0100

    supermodel param

--- tools/launchers/supermodel.sh
+++ tools/launchers/supermodel.sh
@@ -2,6 +2,6 @@
 source $HOME/.config/EmuDeck/backend/functions/all.sh
 cloud_sync_downloadEmu "supermodel" && cloud_sync_startService
 param="${@}"
-param=$(echo "$param" | sed "s|'|\"|g")
+param=$(echo "$param" | sed "s|'|/\"|g")
 /usr/bin/flatpak run com.supermodel3.Supermodel "${param}"
 rm -rf "$savesPath/.gaming"
