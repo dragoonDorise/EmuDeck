2b85c052bc42bab285acce769245193bb35237aa
rawdatafeel <108900299+rawdatafeel@users.noreply.github.com>
2024-03-07 11:57:43 -0500

    RetroArch/Model 2: Silence Model 2 install and RA buildbot (#1072)

--- functions/EmuScripts/emuDeckRetroArch.sh
+++ functions/EmuScripts/emuDeckRetroArch.sh
@@ -276,55 +276,64 @@ RetroArch_buildbotDownloader(){
 	# Common Shaders
 	if [[ ! -d "$shaderscgDir" ]] ; then
 		mkdir "$shaderscgDir"
-		curl -L "$RetroArch_shaderscgURL" -o "$shaderscgDir/shaders_cg.zip" && nice -n 10 unzip -q -o "$shaderscgDir/shaders_cg.zip" -d "$shaderscgDir" && rm "$shaderscgDir/shaders_cg.zip"
+		{ curl -L "$RetroArch_shaderscgURL" -o "$shaderscgDir/shaders_cg.zip" && nice -n 5 unzip -q -o "$shaderscgDir/shaders_cg.zip" -d "$shaderscgDir" && rm "$shaderscgDir/shaders_cg.zip"; } &> /dev/null
+
 	fi
 
 	# GLSL Shaders
 	if [[ ! -d "$shadersglslDir" ]] ; then
 		mkdir "$shadersglslDir"
-		curl -L "$RetroArch_shadersglslURL" -o "$shadersglslDir/shaders_glsl.zip" && nice -n 10 unzip -q -o "$shadersglslDir/shaders_glsl.zip" -d "$shadersglslDir" && rm "$shadersglslDir/shaders_glsl.zip"
+		{ curl -L "$RetroArch_shadersglslURL" -o "$shadersglslDir/shaders_glsl.zip" && nice -n 5 unzip -q -o "$shadersglslDir/shaders_glsl.zip" -d "$shadersglslDir" && rm "$shadersglslDir/shaders_glsl.zip"; } &> /dev/null
+
 	fi
 
 	# Slang Shaders
 	if [[ ! -d "$shadersslangDir" ]] ; then
 		mkdir "$shadersslangDir"
-		curl -L "$RetroArch_shadersslangURL" -o "$shadersslangDir/shaders_glsl.zip" && nice -n 10 unzip -q -o "$shadersslangDir/shaders_glsl.zip" -d "$shadersslangDir" && rm "$shadersslangDir/shaders_glsl.zip"
+		{ curl -L "$RetroArch_shadersslangURL" -o "$shadersslangDir/shaders_glsl.zip" && nice -n 5 unzip -q -o "$shadersslangDir/shaders_glsl.zip" -d "$shadersslangDir" && rm "$shadersslangDir/shaders_glsl.zip"; } &> /dev/null
+
 	fi
 
 	# Assets
 	if [[ ! -d "$assetsDir" ]] ; then
 		mkdir -p "$assetsDir"
-		curl -L "$RetroArch_assetsURL" -o "$assetsDir/assets.zip" && nice -n 10 unzip -q -o "$assetsDir/assets.zip" -d "$assetsDir" && rm "$assetsDir/assets.zip"
+		{ curl -L "$RetroArch_assetsURL" -o "$assetsDir/assets.zip" && nice -n 5 unzip -q -o "$assetsDir/assets.zip" -d "$assetsDir" && rm "$assetsDir/assets.zip"; } &> /dev/null
+
 	fi
 
 	# Overlays
 	if [[ ! -d "$overlaysDir/borders" ]] ; then
 		mkdir -p "$overlaysDir"
-		curl -L "$RetroArch_overlaysURL" -o "$overlaysDir/overlays.zip" && nice -n 10 unzip -q -o "$overlaysDir/overlays.zip" -d "$overlaysDir" && rm "$overlaysDir/overlays.zip"
+		{ curl -L "$RetroArch_overlaysURL" -o "$overlaysDir/overlays.zip" && nice -n 5 unzip -q -o "$overlaysDir/overlays.zip" -d "$overlaysDir" && rm "$overlaysDir/overlays.zip"; } &> /dev/null
+
 	fi
 
 	# Autoconfig - for controllers (primarily helps non-Steam Deck setups)
 	if [[ ! -d "$autoconfigDir" ]] ; then
 		mkdir -p "$autoconfigDir"
-		curl -L "$RetroArch_autoconfigURL" -o "$autoconfigDir/autoconfig.zip" && nice -n 10 unzip -q -o "$autoconfigDir/autoconfig.zip" -d "$autoconfigDir" && rm "$autoconfigDir/autoconfig.zip"
+		{ curl -L "$RetroArch_autoconfigURL" -o "$autoconfigDir/autoconfig.zip" && nice -n 5 unzip -q -o "$autoconfigDir/autoconfig.zip" -d "$autoconfigDir" && rm "$autoconfigDir/autoconfig.zip"; } &> /dev/null
+
 	fi
 
 	# Info
 	if [[ ! -d "$infoDir" ]] ; then
 	 	mkdir -p "$infoDir"
-		curl -L "$RetroArch_infoURL" -o "$infoDir/info.zip" && nice -n 10 unzip -q -o "$infoDir/info.zip" -d "$infoDir" && rm "$infoDir/info.zip"
+		{ curl -L "$RetroArch_infoURL" -o "$infoDir/info.zip" && nice -n 5 unzip -q -o "$infoDir/info.zip" -d "$infoDir" && rm "$infoDir/info.zip"; } &> /dev/null
+
 	fi
 
 	# Cheats
 	if [[ ! -d "$cheatsDir" ]] ; then
 	 	mkdir -p "$cheatsDir"
-		curl -L "$RetroArch_cheatsURL" -o "$cheatsDir/cheats.zip" && nice -n 10 unzip -q -o "$cheatsDir/cheats.zip" -d "$cheatsDir" && rm "$cheatsDir/cheats.zip"
+		{ curl -L "$RetroArch_cheatsURL" -o "$cheatsDir/cheats.zip" && nice -n 5 unzip -q -o "$cheatsDir/cheats.zip" -d "$cheatsDir" && rm "$cheatsDir/cheats.zip"; } &> /dev/null
+
 	fi
 
 
 	# PPSSPP
 	if [[ ! -d "$ppssppDir" ]] ; then
-		curl -L "$RetroArch_ppssppURL" -o "$biosPath/PPSSPP.zip" && nice -n 10 unzip -q -o "$biosPath/PPSSPP.zip" -d "$biosPath" && rm "$biosPath/PPSSPP.zip"
+		{ curl -L "$RetroArch_ppssppURL" -o "$biosPath/PPSSPP.zip" && nice -n 5 unzip -q -o "$biosPath/PPSSPP.zip" -d "$biosPath" && rm "$biosPath/PPSSPP.zip"; } &> /dev/null
+
 	fi
 
 
