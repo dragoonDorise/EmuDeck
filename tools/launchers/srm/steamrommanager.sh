bfbc918a63c4b08238fc761b3b6e2f70f31f7c48
Dragoon Dorise <rodrigosedano@me.com>
2024-03-07 15:58:10 +0100

    fix srm

--- tools/launchers/srm/steamrommanager.sh
+++ tools/launchers/srm/steamrommanager.sh
@@ -2,7 +2,14 @@
 source "$HOME/.config/EmuDeck/backend/functions/all.sh"
 SRM_toolPath="${toolsPath}/Steam ROM Manager.AppImage"
 
-#SRM_createParsers
+if grep -q "mmcblk0p1" "$SRM_userData_configDir/userConfigurations.json"; then
+	SRM_init
+	text="$(printf "<b>Parsers fixed</b>\nWe've detected an issue with Steam Rom Manager, but it's been fixed")"
+	zenity --info \
+		--title="EmuDeck" \
+		--width="450" \
+		--text="${text}" 2>/dev/null
+fi
 SRM_addSteamInputProfiles
 
 zenity --question \
