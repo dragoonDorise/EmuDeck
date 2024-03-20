ebb36c8f0c8f1ad9773c20c3ab563532757b0778
Dragoon Dorise <rodrigosedano@me.com>
2024-03-20 16:38:55 +0100

    no RA assets dl

--- functions/EmuScripts/emuDeckRetroArch.sh
+++ functions/EmuScripts/emuDeckRetroArch.sh
@@ -219,7 +219,7 @@ RetroArch_update(){
 #ConfigurePaths
 RetroArch_setEmulationFolder(){
     setMSG "Setting $RetroArch_emuName Emulation Folder"
-	
+
     RetroArch_setConfigOverride 'system_directory' "${biosPath}" "$RetroArch_configFile"
     RetroArch_setConfigOverride 'rgui_browser_directory' "${romsPath}" "$RetroArch_configFile"
     RetroArch_setConfigOverride 'cheat_database_path' "${storagePath}/retroarch/cheats" "$RetroArch_configFile"
@@ -301,10 +301,10 @@ RetroArch_buildbotDownloader(){
 	fi
 
 	# Assets
-	if  [[ ! "$( ls -A "$assetsDir")" ]] ; then
-		{ curl -L "$RetroArch_assetsURL" -o "$assetsDir/assets.zip" && nice -n 5 unzip -q -o "$assetsDir/assets.zip" -d "$assetsDir" && rm "$assetsDir/assets.zip"; } &> /dev/null
-
-	fi
+#	if  [[ ! "$( ls -A "$assetsDir")" ]] ; then
+#		{ curl -L "$RetroArch_assetsURL" -o "$assetsDir/assets.zip" && nice -n 5 unzip -q -o "$assetsDir/assets.zip" -d "$assetsDir" && rm "$assetsDir/assets.zip"; } &> /dev/null
+#
+#	fi
 
 	# Overlays
 	if [[ ! "$( ls -A "$overlaysDir/borders")" ]] ; then
@@ -354,11 +354,11 @@ RetroArch_uninstall(){
 
 #setABXYstyle
 RetroArch_setABXYstyle(){
-	mv "$RetroArch_remapsDir/mGBA/mGBA.rmp.disabled" "$RetroArch_remapsDir/mGBA/mGBA.rmp"  
-	mv "$RetroArch_remapsDir/Gambatte/Gambatte.rmp.disabled" "$RetroArch_remapsDir/Gambatte/Gambatte.rmp" 
+	mv "$RetroArch_remapsDir/mGBA/mGBA.rmp.disabled" "$RetroArch_remapsDir/mGBA/mGBA.rmp"
+	mv "$RetroArch_remapsDir/Gambatte/Gambatte.rmp.disabled" "$RetroArch_remapsDir/Gambatte/Gambatte.rmp"
 	mv "$RetroArch_remapsDir/bsnes-hd beta/bsnes-hd beta.rmp.disabled" "$RetroArch_remapsDir/bsnes-hd beta/bsnes-hd beta.rmp"
-	mv "$RetroArch_remapsDir/melonDS DS/melonDS DS.rmp.disabled" "$RetroArch_remapsDir/melonDS DS/melonDS DS.rmp" 
-	mv "$RetroArch_remapsDir/Mupen64Plus-Next/Mupen64Plus-Next.rmp.disabled" "$RetroArch_remapsDir/Mupen64Plus-Next/Mupen64Plus-Next.rmp" 
+	mv "$RetroArch_remapsDir/melonDS DS/melonDS DS.rmp.disabled" "$RetroArch_remapsDir/melonDS DS/melonDS DS.rmp"
+	mv "$RetroArch_remapsDir/Mupen64Plus-Next/Mupen64Plus-Next.rmp.disabled" "$RetroArch_remapsDir/Mupen64Plus-Next/Mupen64Plus-Next.rmp"
 	mv "$RetroArch_remapsDir/SameBoy/SameBoy.rmp.disabled" "$RetroArch_remapsDir/SameBoy/SameBoy.rmp"
 	mv "$RetroArch_remapsDir/Snes9x/Snes9x.rmp.disabled" "$RetroArch_remapsDir/Snes9x/Snes9x.rmp"
 	mv "$RetroArch_remapsDir/Mesen/Mesen.rmp.disabled" "$RetroArch_remapsDir/Mesen/Mesen.rmp"
@@ -367,8 +367,8 @@ RetroArch_setABXYstyle(){
 
 }
 RetroArch_setBAYXstyle(){
-	mv "$RetroArch_remapsDir/mGBA/mGBA.rmp" "$RetroArch_remapsDir/mGBA/mGBA.rmp.disabled"  
-	mv "$RetroArch_remapsDir/Gambatte/Gambatte.rmp" "$RetroArch_remapsDir/Gambatte/Gambatte.rmp.disabled"  
+	mv "$RetroArch_remapsDir/mGBA/mGBA.rmp" "$RetroArch_remapsDir/mGBA/mGBA.rmp.disabled"
+	mv "$RetroArch_remapsDir/Gambatte/Gambatte.rmp" "$RetroArch_remapsDir/Gambatte/Gambatte.rmp.disabled"
 	mv "$RetroArch_remapsDir/bsnes-hd beta/bsnes-hd beta.rmp" "$RetroArch_remapsDir/bsnes-hd beta/bsnes-hd beta.rmp.disabled"
 	mv "$RetroArch_remapsDir/melonDS DS/melonDS DS.rmp" "$RetroArch_remapsDir/melonDS DS/melonDS DS.rmp.disabled"
 	mv "$RetroArch_remapsDir/Mupen64Plus-Next/Mupen64Plus-Next.rmp" "$RetroArch_remapsDir/Mupen64Plus-Next/Mupen64Plus-Next.rmp.disabled"
