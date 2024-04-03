e8dc0935ee0c5c7574254c3576f364eb8188d37d
rawdatafeel <108900299+rawdatafeel@users.noreply.github.com>
2024-04-03 02:14:08 -0400

    chddeck: Use ZSTD for createdvd compression (#1172)
    * Not using ZSTD can cause issues since EmuDeck is using a higher hunk size

--- tools/chdconv/chddeck.sh
+++ tools/chdconv/chddeck.sh
@@ -99,7 +99,7 @@ compressCHD() {
 compressCHDDVD() {
 	local file=$1
 	local successful='' 
-	chdman5 createdvd --hunksize 16384 -i "$file" -o "${file%.*}.chd" && successful="true"
+	chdman5 createdvd -i "$file" -o "${file%.*}.chd" -c zstd && successful="true"
 	if [[ $successful == "true" ]]; then
 		echo "Converting $file to CHD using the createdvd flag and hunksize 16348."
 		echo "$file succesfully converted to ${file%.*}.chd"
