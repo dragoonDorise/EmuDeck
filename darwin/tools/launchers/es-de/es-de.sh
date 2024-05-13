ae83312644cfb5406185680722b8c691bcb17bf1
Dragoon Dorise <rodrigosedano@me.com>
2024-05-13 22:46:29 +0200

    esde launhcer

--- darwin/tools/launchers/es-de/es-de.sh
+++ darwin/tools/launchers/es-de/es-de.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 source $HOME/.config/EmuDeck/backend/functions/all.sh
-cloud_sync_downloadEmuAll && cloud_sync_startService
-"$ESDE_toolPath" "${@}"
+#cloud_sync_downloadEmuAll && cloud_sync_startService
+open "$ESDE_toolPath" "${@}"
 rm -rf "$savesPath/.gaming"
