b6b0264ec8b2de64bcc0bd81dc8b792fdcfc9214
Dragoon Dorise <rodrigosedano@me.com>
2024-05-14 18:46:04 +0200

    fixes

--- darwin/functions/ToolsScripts/emuDeckCloudSync.sh
+++ darwin/functions/ToolsScripts/emuDeckCloudSync.sh
@@ -1,47 +1,24 @@
 #!/bin/bash
 cloud_sync_createService(){
-     local SERVICE_NAME="com.emudeck.cloudsync"
-     local PLIST_PATH="$HOME/Library/LaunchAgents/$SERVICE_NAME.plist"
-     local SCRIPT_PATH="$HOME/.config/EmuDeck/backend/tools/cloudSync/cloud_sync_watcher.sh"
-
-
-     mkdir -p "$(dirname "$PLIST_PATH")"
-
-     {
-     cat <<EOF > "$PLIST_PATH"
-     <?xml version="1.0" encoding="UTF-8"?>
-     <!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-     <plist version="1.0">
-      <dict>
-        <key>Label</key>
-        <string>$SERVICE_NAME</string>
-        <key>ProgramArguments</key>
-        <array>
-          <string>/bin/bash</string>
-          <string>$SCRIPT_PATH</string>
-        </array>
-        <key>RunAtLoad</key>
-        <true/>
-        <key>KeepAlive</key>
-        <true/>
-      </dict>
-     </plist>
-EOF
-     #chmod +x "$SCRIPT_PATH"
-     }
+     echo "NYI"
  }
 
-
 cloud_sync_startService(){
-   local SERVICE_NAME="com.emudeck.cloudsync"
-   local PLIST_PATH="$HOME/Library/LaunchAgents/$SERVICE_NAME.plist"
-   local SCRIPT_PATH="$HOME/.config/EmuDeck/backend/tools/cloudSync/cloud_sync_watcher.sh"
-   launchctl load "$PLIST_PATH"
+   echo "NYI"
  }
 
  cloud_sync_stopService(){
-   local SERVICE_NAME="com.emudeck.cloudsync"
-   local PLIST_PATH="$HOME/Library/LaunchAgents/$SERVICE_NAME.plist"
-   local SCRIPT_PATH="$HOME/.config/EmuDeck/backend/tools/cloudSync/cloud_sync_watcher.sh"
-   launchctl unload "$PLIST_PATH"
+   echo "NYI"
+ }
+
+cloud_sync_downloadEmuAll(){
+  osascript -e 'display notification "Downloading saves" with title "CloudSync"'
+  cloud_sync_download 'all'
+ }
+
+
+ cloud_sync_uploadEmuAll(){
+   osascript -e 'display notification "Uploading saves" with title "CloudSync"'
+   cloud_sync_upload 'all'
+   osascript -e 'display notification "Saves uploaded" with title "CloudSync"'
  }
