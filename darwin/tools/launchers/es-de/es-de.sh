16c7d0118ac79ffa0aceb45b07159a00e4ef28a6
Dragoon Dorise <rodrigosedano@me.com>
2024-05-14 13:56:33 +0200

    ESDE Cloudsync

--- darwin/tools/launchers/es-de/es-de.sh
+++ darwin/tools/launchers/es-de/es-de.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-source $HOME/.config/EmuDeck/backend/functions/all.sh
+source $HOME/.config/EmuDeck/backend/functions/all.sh 2>/dev/null
+osascript -e 'display notification "Downloading saves" with title "CloudSync"'
 cloud_sync_downloadEmuAll
-open "$ESDE_toolPathExe" "${@}"
-rm -rf "$savesPath/.gaming"
-cloud_sync_uploadEmuAll
+open "$ESDE_toolPathExe" "${@}" && rm -rf "$savesPath/.gaming"
+osascript -e 'display notification "Uploading saves" with title "CloudSync"'
+cloud_sync_uploadEmuAll && osascript -e 'display notification "Saves uploaded" with title "CloudSync"' && rm -rf "$savesPath/.gaming"
