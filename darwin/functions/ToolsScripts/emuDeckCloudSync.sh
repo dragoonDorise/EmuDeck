e3d0bea921176f7813625f4ee6e2f7fa7efaa8ac
Dragoon Dorise <rodrigosedano@me.com>
2024-05-14 13:15:01 +0200

    darwin cloudsync wip

--- darwin/functions/ToolsScripts/emuDeckCloudSync.sh
+++ darwin/functions/ToolsScripts/emuDeckCloudSync.sh
@@ -1,4 +1,42 @@
 #!/bin/bash
- cloud_sync_createService(){
-     echo "NYI"
+cloud_sync_createService(){
+     SERVICE_NAME="com.emudeck.cloudsync"
+     SCRIPT_PATH="$HOME/.config/EmuDeck/backend/tools/cloudSync/cloud_sync_watcher.sh"
+     PLIST_PATH="$HOME/Library/LaunchAgents/$SERVICE_NAME.plist"
+
+     mkdir -p "$(dirname "$PLIST_PATH")"
+
+     {
+     cat <<EOF > "$PLIST_PATH"
+     <?xml version="1.0" encoding="UTF-8"?>
+     <!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+     <plist version="1.0">
+      <dict>
+        <key>Label</key>
+        <string>$SERVICE_NAME</string>
+        <key>ProgramArguments</key>
+        <array>
+          <string>/bin/bash</string>
+          <string>$SCRIPT_PATH</string>
+        </array>
+        <key>RunAtLoad</key>
+        <true/>
+        <key>KeepAlive</key>
+        <true/>
+      </dict>
+     </plist>
+EOF
+     #chmod +x "$SCRIPT_PATH"
+     }
+ }
+
+
+cloud_sync_startService(){
+   local SCRIPT_PATH="$HOME/.config/EmuDeck/backend/tools/cloudSync/cloud_sync_watcher.sh"
+   launchctl load "$PLIST_PATH"
+ }
+
+ cloud_sync_stopService(){
+   local SCRIPT_PATH="$HOME/.config/EmuDeck/backend/tools/cloudSync/cloud_sync_watcher.sh"
+   launchctl unload "$PLIST_PATH"
  }
