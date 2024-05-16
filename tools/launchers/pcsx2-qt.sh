b399d397f2f97279cdc6b93df1fa337662099877
mykbit <96201443+mykbit@users.noreply.github.com>
2024-05-16 11:48:39 +0100

    Fix launch argument parsing

--- tools/launchers/pcsx2-qt.sh
+++ tools/launchers/pcsx2-qt.sh
@@ -20,9 +20,6 @@ fi
 #run the executable with the params.
 #Fix first '
 param="${@}"
-substituteWith='"'
-param=${param/\'/"$substituteWith"}
-#Fix last ' on command
-param=$(echo "$param" | sed 's/.$/"/')
+param=$(echo "$param" | sed "s/'/\"/g")
 eval "${exe} ${param} -fullscreen"
 rm -rf "$savesPath/.gaming"
