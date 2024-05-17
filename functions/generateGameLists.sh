1eeba5b884770782cd7947fbb8674fbd968d2099
Dragoon Dorise <rodrigosedano@me.com>
2024-05-17 23:24:51 +0200

    improved speed

--- functions/generateGameLists.sh
+++ functions/generateGameLists.sh
@@ -6,13 +6,17 @@ generateGameLists() {
     printf "["
 
     first_system=true
+    declare -a download_array
+    declare -a download_dest_paths
 
     for system_dir in "$ROMS_DIR"/*; do
         if [[ -d "$system_dir" && -f "$system_dir/metadata.txt" ]]; then
 
+
             if [[ "$system_dir" == *"/ps3" ]]; then
                 continue
             fi
+
             if [[ "$system_dir" == *"/xbox360" ]]; then
                 continue
             fi
@@ -30,7 +34,6 @@ generateGameLists() {
 
             collection=$(grep 'collection:' "$system_dir/metadata.txt" | cut -d ':' -f 2- | awk '{$1=$1};1')
             shortname=$(grep 'shortname:' "$system_dir/metadata.txt" | cut -d ':' -f 2- | awk '{$1=$1};1')
-#            launcher=$(grep 'launch:' "$system_dir/metadata.txt" | cut -d ':' -f 2- | awk '{$1=$1};1')
             launcher=$(grep 'launch:' "$system_dir/metadata.txt" | cut -d ':' -f 2- | awk '{$1=$1};1' | sed 's/"/\\"/g')
             extensions=$(grep 'extensions:' "$system_dir/metadata.txt" | cut -d ':' -f 2- | tr ',' ' ' | awk '{$1=$1};1')
 
@@ -45,17 +48,21 @@ generateGameLists() {
 
             # Use jq to create the JSON objects for each game and print them directly
             first_game=true
-            api_key="f80f92019254471cca9d62ff91c21eee"
 
-            find "$system_dir" -type f | while read -r file_path; do
+            # Process files and collect game data
+            game_data=""
+            while IFS= read -r -d '' file_path; do
                 filename=$(basename "$file_path")
                 extension="${filename##*.}"
                 name="${filename%.*}"
-                if [[ "$extensions" =~ "$extension" ]]; then
-                    if $first_game; then
-                        first_game=false
+                #echo "Processing file: $filename with extension: $extension"
+
+                if [[ " ${extensions[@]} " =~ " ${extension} " ]]; then
+                    #echo "File $filename matches the extensions filter"
+                    if ! $first_game; then
+                        game_data+=","
                     else
-                        printf ","
+                        first_game=false
                     fi
 
                     clean_name=$(echo "$name" | tr ' ' '_' | sed 's/[^a-zA-Z0-9_]//g')
@@ -66,26 +73,29 @@ generateGameLists() {
                     game_img_url=$(echo "$response" | jq -r '.img')
                     game_img=$(echo "$game_img_url" | sed 's|.*/||')
 
-
                     if [ "$game_img_url" == "null" ]; then
                         game_name=$name
-                        game_img='';
+                        game_img=''
                     else
                         accountfolder=$(ls -d $HOME/.steam/steam/userdata/*/ | head -n 1)
                         dest_folder="$accountfolder/config/grid/"
                         mkdir -p "$dest_folder"
                         filename=$(basename "$game_img_url")
                         dest_path="$dest_folder/$filename"
-                        if [ ! -f $dest_path ]; then
-                            curl -o "$dest_path" "$game_img_url"
+                        if [ ! -f "$dest_path" ]; then
+                            #echo "Adding $game_img_url to download array"
+                            download_array+=("$game_img_url")
+                            download_dest_paths+=("$dest_path")
                         fi
                         game_img="/customimages/$game_img"
                     fi
 
-                    jq -n --arg name "$game_name" --arg filename "$file_path" --arg game_img "$game_img" \
-                        '{"name": $name, "filename": $filename, "img": $game_img}'
+                    game_data+=$(jq -n --arg name "$game_name" --arg filename "$file_path" --arg game_img "$game_img" \
+                        '{"name": $name, "filename": $filename, "img": $game_img}')
                 fi
-            done
+            done < <(find "$system_dir" -type f -print0)
+
+            printf "$game_data"
             # End games array and system object
             printf "] }"
 
@@ -94,4 +104,22 @@ generateGameLists() {
 
     # End of JSON array
     printf "]"
+
+    # Ensure the download array is not empty
+    #echo "Download array length: ${#download_array[@]}"
+    if [ ${#download_array[@]} -eq 0 ]; then
+        #echo "No images to download."
+        return
+    fi
+
+    # Download images in parallel
+    #echo "Starting downloads..."
+    for i in "${!download_array[@]}"; do
+        {
+            #echo "Downloading ${download_array[$i]} to ${download_dest_paths[$i]}"
+            curl -o "${download_dest_paths[$i]}" "${download_array[$i]}"
+        } &
+    done
+    wait
+    #echo "Downloads completed."
 }
