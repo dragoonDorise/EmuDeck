36bfec94be7de4b5d31e148ddf75062fce2bc8f2
Dragoon Dorise <rodrigosedano@me.com>
2024-05-20 23:23:25 +0200

    fix

--- functions/generateGameLists.sh
+++ functions/generateGameLists.sh
@@ -13,47 +13,57 @@ generateGameListsJson() {
 }
 
 generateGameLists_artwork() {
-   json=$(cat "$HOME/emudeck/roms_games.json")
-   platforms=$(echo "$json" | jq -r '.[].id')
+    json=$(cat "$HOME/emudeck/roms_games.json")
+    platforms=$(echo "$json" | jq -r '.[].id')
 
-   accountfolder=$(ls -d $HOME/.steam/steam/userdata/* | head -n 1)
-   dest_folder="$accountfolder/config/grid/"
-   mkdir -p "$dest_folder"
+    accountfolder=$(ls -d $HOME/.steam/steam/userdata/* | head -n 1)
+    dest_folder="$accountfolder/config/grid/"
+    mkdir -p "$dest_folder"
 
-   for platform in $platforms; do
-       echo "Processing platform: $platform"
-       games=$(echo "$json" | jq -r --arg platform "$platform" '.[] | select(.id == $platform) | .games[]?.name')
+    declare -A processed_games
 
-       declare -a download_array
-       declare -a download_dest_paths
+    for platform in $platforms; do
+        echo "Processing platform: $platform"
+        games=$(echo "$json" | jq -r --arg platform "$platform" '.[] | select(.id == $platform) | .games[]?.name')
 
-       for game in $games; do
-           file_to_check="$dest_folder${game// /_}*"
+        declare -a download_array
+        declare -a download_dest_paths
 
-           if ! ls $file_to_check 1> /dev/null 2>&1; then
-               echo "$game"
+        for game in $games; do
+            file_to_check="$dest_folder${game// /_}*"
 
-               response=$(curl -s -G "https://bot.emudeck.com/steamdbimg.php?name=$game")
-               game_name=$(echo "$response" | jq -r '.name')
-               game_img_url=$(echo "$response" | jq -r '.img')
-               filename=$(basename "$game_img_url")
-               dest_path="$dest_folder$game.jpg"
-               if [ ! -f "$dest_path" ]; then
-                   download_array+=("$game_img_url")
-                   download_dest_paths+=("$dest_path")
-               fi
-           fi
-       done
+           if ! ls $file_to_check 1> /dev/null 2>&1 && [ -z "${processed_games[$game]}" ]; then
+                echo -ne "$game"
 
-       # Download images for the current platform
-       for i in "${!download_array[@]}"; do
-           {
-               curl -s -o "${download_dest_paths[$i]}" "${download_array[$i]}"
-           } &
-       done
-       wait
+                response=$(curl -s -G "https://bot.emudeck.com/steamdbimg.php?name=$game&platform=$platform")
+                game_name=$(echo "$response" | jq -r '.name')
+                game_img_url=$(echo "$response" | jq -r '.img')
+                filename=$(basename "$game_img_url")
+                dest_path="$dest_folder$game.jpg"
 
-       echo "Completed downloads for platform: $platform"
-   done
+
+                if [ ! -f "$dest_path" ] && [ $game_img_url != "null" ]; then
+                    echo -e " - $game_img_url" - $dest_path
+                    download_array+=("$game_img_url")
+                    download_dest_paths+=("$dest_path")
+                    processed_games[$game]=1
+                else
+                    echo -e " - No picture"
+                fi
+            fi
+        done
+
+        # Download images for the current platform
+        if [ ${#download_array[@]} -ne 0 ]; then
+            for i in "${!download_array[@]}"; do
+                {
+                    curl -s -o "${download_dest_paths[$i]}" "${download_array[$i]}"
+                } &
+            done
+        fi
+        wait
+
+        echo "Completed downloads for platform: $platform"
+    done
 }
 
