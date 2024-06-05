9ac71a88c64a0c2808cbeab74eb28e130f0249fe
Dragoon Dorise <rodrigosedano@me.com>
2024-06-05 19:49:18 +0200

    proper vars...

--- android/vars.sh
+++ android/vars.sh
@@ -1,5 +1,6 @@
-Android_ADB_path = "$HOME\emudeck\android\platform-tools"
-Android_ADB_url = "https://dl.google.com/android/repository/platform-tools-latest-linux.zip"
+#!/bin/bash
+Android_ADB_path="$HOME\emudeck\android\platform-tools"
+Android_ADB_url="https://dl.google.com/android/repository/platform-tools-latest-linux.zip"
 Android_folder="$HOME/emudeck/android/"
 Android_temp_android_data="$HOME/emudeck/android/temp/CopyToInternal/Android/data"
 Android_temp_internal="$HOME/emudeck/android/temp/CopyToInternal"
