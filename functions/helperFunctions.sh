a16da5746e6880577c52555eff17fe3114f5c47e
Dragoon Dorise <rodrigosedano@me.com>
2024-06-18 22:18:38 +0200

    fixes

--- functions/helperFunctions.sh
+++ functions/helperFunctions.sh
@@ -1055,6 +1055,7 @@ function controllerLayout_BAYX(){
 
 function server_install(){
 	cp "$EMUDECKGIT/tools/server.sh" "$toolsPath/"
+	cp "$EMUDECKGIT/tools/index.html" "$toolsPath/"
 
 	chmod +x "$toolsPath/server.sh"
 }
