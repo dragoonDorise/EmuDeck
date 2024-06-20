5c792c07835782bd3c15a2627e06b19ac25c5933
Mitja Skuver <SilentException@users.noreply.github.com>
2024-06-20 21:45:12 +0200

    Dev Launcher Fix (#1264)
    * Launcher fix
    
    Fix yuzu launcher + similar changes to other launchers (avoids eval)

--- tools/launchers/pcsx2-qt.sh
+++ tools/launchers/pcsx2-qt.sh
@@ -4,35 +4,41 @@ emulatorInit "pcsx2"
 emuName="pcsx2-Qt" #parameterize me
 emufolder="$HOME/Applications" # has to be applications for ES-DE to find it
 
+#initialize execute array
+exe=()
+
 #find full path to emu executable
-exe=$(find $emufolder -iname "${emuName}*.AppImage" | sort -n | cut -d' ' -f 2- | tail -n 1 2>/dev/null)
+exe_path=$(find "$emufolder" -iname "${emuName}*.AppImage" | sort -n | cut -d' ' -f 2- | tail -n 1 2>/dev/null)
 
 #if appimage doesn't exist fall back to flatpak.
-if [[ $exe == '' ]]; then
-	#flatpak
-	flatpakApp=$(flatpak list --app --columns=application | grep $emuName)
-	exe="/usr/bin/flatpak run "$flatpakApp
+if [[ -z "$exe_path" ]]; then
+    #flatpak
+    flatpakApp=$(flatpak list --app --columns=application | grep "$emuName")
+    #fill execute array
+    exe=("flatpak" "run" "$flatpakApp")
 else
-#make sure that file is executable
-	chmod +x $exe
+    #make sure that file is executable
+    chmod +x "$exe_path"
+    #fill execute array
+    exe=("$exe_path")
 fi
 
 #run the executable with the params.
 launch_args=()
 for rom in "${@}"; do
-	# Parsers previously had single quotes ("'/path/to/rom'" ), this allows those shortcuts to continue working.
-	removedLegacySingleQuotes=$(echo "$rom" | sed "s/^'//; s/'$//")
-	launch_args+=("$removedLegacySingleQuotes")
+    # Parsers previously had single quotes ("'/path/to/rom'" ), this allows those shortcuts to continue working.
+    removedLegacySingleQuotes=$(echo "$rom" | sed "s/^'//; s/'$//")
+    launch_args+=("$removedLegacySingleQuotes")
 done
 
-echo "Launching: "${exe}" "${launch_args[*]}""
+echo "Launching: ${exe[*]} ${launch_args[*]}"
 
 if [[ -z "${*}" ]]; then
     echo "ROM not found. Launching $emuName directly"
-    "${exe}"
+    "${exe[@]}"
 else
     echo "ROM found, launching game"
-    "${exe}" "${launch_args[@]}"
+    "${exe[@]}" "${launch_args[@]}"
 fi
 
 rm -rf "$savesPath/.gaming"
