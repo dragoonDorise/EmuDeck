d8bfb3bba5e3b527abb615d38ae05d2fac18d375
Dragoon Dorise <rodrigosedano@me.com>
2024-06-26 09:56:01 +0200

    lime3ds install + init

--- test.sh
+++ test.sh
@@ -1,22 +1,12 @@
 #!/bin/bash
+. $HOME/.config/EmuDeck/backend/functions/all.sh
 
-user=$(zenity --entry --title="ScreenScrapper" --text="User:")
-password=$(zenity --password --title="ScreenScrapper" --text="Password:")
-
-encryption_key=$(openssl rand -base64 32)
-encrypted_password=$(echo "$password" | openssl enc -aes-256-cbc -pbkdf2 -base64 -pass "pass:$encryption_key")
-
-echo "$encryption_key" > "$HOME/.config/EmuDeck/logs/.key"
-echo "$encrypted_password" > "$HOME/.config/EmuDeck/.passSS"
-echo "$user" > "$HOME/.config/EmuDeck/.userSS"
-
-
-#############
-
-encryption_key=$(cat "$HOME/.config/EmuDeck/logs/.key")
-
-encrypted_password=$(cat "$HOME/.config/EmuDeck/.passSS")
-
-decrypted_password=$(echo "$encrypted_password" | openssl enc -d -aes-256-cbc -pbkdf2 -base64 -pass "pass:$encryption_key")
-
-echo $decrypted_password
+Lime3DS_IsInstalled(){
+    if [ -e "$Lime3DS_emuPath" ]; then
+        echo "true"
+    else
+        echo "false"
+    fi
+}
+echo $Lime3DS_emuPath
+Lime3DS_IsInstalled
