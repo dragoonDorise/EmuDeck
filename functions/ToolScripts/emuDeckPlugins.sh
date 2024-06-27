aa042b3781e4883b9a3cad7833c533a4ff6c4685
Dragoon Dorise <rodrigosedano@me.com>
2024-06-27 20:59:10 +0200

    echo $password |  sudo -S

--- functions/ToolScripts/emuDeckPlugins.sh
+++ functions/ToolScripts/emuDeckPlugins.sh
@@ -68,7 +68,7 @@ Plugins_installPowerTools(){
       Plugins_installPluginLoader $password
 	  rm -rf "$HOME/homebrew/plugins/PowerTools"
 	  curl -l "$url" --output "$HOME/homebrew/PowerTools.zip.temp" && mv "$HOME/homebrew/PowerTools.zip.temp" "$HOME/homebrew/PowerTools.zip"
-	  unzip "$HOME/homebrew/PowerTools.zip" -d "$HOME/homebrew/plugins/" && rm "$HOME/homebrew/PowerTools.zip"
+	  echo $password |  sudo -S unzip "$HOME/homebrew/PowerTools.zip" -d "$HOME/homebrew/plugins/" && rm "$HOME/homebrew/PowerTools.zip"
    fi
 }
 
@@ -89,7 +89,7 @@ Plugins_installPowerControl(){
       Plugins_installPluginLoader $password
 	  rm -rf $destinationFolder
 	  curl -L "$PowerControl_releaseURL" -o "$HOME/homebrew/plugins/PowerControl.tar.gz"
-	  unzip "$HOME/homebrew/plugins/PowerControl.tar.gz" -d "$HOME/homebrew/plugins/" && rm "$HOME/homebrew/plugins/PowerControl.tar.gz"
+	  echo $password |  sudo -S unzip "$HOME/homebrew/plugins/PowerControl.tar.gz" -d "$HOME/homebrew/plugins/" && rm "$HOME/homebrew/plugins/PowerControl.tar.gz"
 	  chown $USER:$USER -R $HOME/homebrew/plugins/PowerControl
    fi
 
@@ -116,7 +116,7 @@ Plugins_installEmuDecky(){
          Plugins_installPluginLoader $password
 		 rm -rf $destinationFolder
 		 curl -L "$DeckyControls_releaseURL" -o "$HOME/homebrew/plugins/EmuDecky.zip"
-		 unzip "$HOME/homebrew/plugins/EmuDecky.zip" -d "$HOME/homebrew/plugins/" && rm "$HOME/homebrew/plugins/EmuDecky.zip"
+		 echo $password |  sudo -S unzip "$HOME/homebrew/plugins/EmuDecky.zip" -d "$HOME/homebrew/plugins/" && rm "$HOME/homebrew/plugins/EmuDecky.zip"
 		 chown $USER:$USER -R $HOME/homebrew/plugins/EmuDecky
    fi
 
@@ -139,7 +139,7 @@ Plugins_installDeckyRomLibrary(){
      Plugins_installPluginLoader $password
      rm -rf $destinationFolder
      curl -L "$DeckyControls_releaseURL" -o "$HOME/homebrew/plugins/decky-rom-library.zip"
-     unzip "$HOME/homebrew/plugins/decky-rom-library.zip" -d "$HOME/homebrew/plugins/" && rm "$HOME/homebrew/plugins/decky-rom-library.zip"
+     echo $password |  sudo -S unzip "$HOME/homebrew/plugins/decky-rom-library.zip" -d "$HOME/homebrew/plugins/" && rm "$HOME/homebrew/plugins/decky-rom-library.zip"
      chown $USER:$USER -R $HOME/homebrew/plugins/decky-rom-library
    fi
 
