9d482ded43c67e2d0b2744f81339d0022427763c
Dragoon Dorise <rodrigosedano@me.com>
2024-08-08 22:32:57 +0200

    no suyu

--- functions/all.sh
+++ functions/all.sh
@@ -74,7 +74,7 @@ source "$EMUDECKGIT"/functions/ToolScripts/emuDeckNetPlay.sh
 
 
 #emuscripts
-source "$EMUDECKGIT"/functions/EmuScripts/emuDeckSuyu.sh
+#source "$EMUDECKGIT"/functions/EmuScripts/emuDeckSuyu.sh
 source "$EMUDECKGIT"/functions/EmuScripts/emuDeckYuzu.sh
 source "$EMUDECKGIT"/functions/EmuScripts/emuDeckCemu.sh
 source "$EMUDECKGIT"/functions/EmuScripts/emuDeckCemuProton.sh
