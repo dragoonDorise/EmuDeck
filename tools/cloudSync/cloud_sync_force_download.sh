7185f5f666f2316f5bfa4cff8c428b40c216198a
Dragoon Dorise <rodrigosedano@me.com>
2024-09-07 21:51:54 +0200

    pulsate zenity

--- tools/cloudSync/cloud_sync_force_download.sh
+++ tools/cloudSync/cloud_sync_force_download.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source $HOME/.config/EmuDeck/backend/functions/all.sh
-cloud_sync_downloadEmuAll | zenity --progress --title "Downloading Saved games" --width 600 --auto-close --no-cancel 2>/dev/null
+cloud_sync_downloadEmuAll | zenity --progress --pulsate --title "Downloading Saved games" --width 600 --auto-close --no-cancel 2>/dev/null
 rm -rf "$savesPath/.gaming"
