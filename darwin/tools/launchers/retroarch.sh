86d537ead03e973b7577ccfb10054c984a6e3352
Dragoon Dorise <rodrigosedano@me.com>
2024-09-08 21:08:43 +0200

    cleanup

--- darwin/tools/launchers/retroarch.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#source $HOME/.config/EmuDeck/backend/functions/all.sh
-#emulatorInit "retroarch"
-open "$HOME/Applications/EmuDeck/RetroArch.app" --args "${@}"
-#rm -rf "$savesPath/.gaming"
