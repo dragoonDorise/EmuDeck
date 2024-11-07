3665d939696bcedaa3245d6d068ccb31f2022be0
Dragoon Dorise <rodrigosedano@me.com>
2024-11-07 21:33:03 +0100

    python fix

--- tools/fuzzy_search_rom.py
+++ tools/fuzzy_search_rom.py
@@ -52,11 +52,11 @@ if not os.path.exists(json_file_path) or is_file_older_than(json_file_path, 5):
 
 else:
     for root, _, files in os.walk(images_path):
-    for file in files:
-        file_path = os.path.join(root, file)
-        # Verifica si el archivo tiene un tamaño de 0 bytes
-        if os.path.getsize(file_path) == 0:
-            os.remove(file_path)
+        for file in files:
+            file_path = os.path.join(root, file)
+            # Verifica si el archivo tiene un tamaño de 0 bytes
+            if os.path.getsize(file_path) == 0:
+                os.remove(file_path)
 
     # Cargar el JSON desde el disco duro
     with open(json_file_path, "r") as json_file:
