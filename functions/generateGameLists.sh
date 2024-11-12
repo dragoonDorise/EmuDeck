4bff2be08e6050e8b31c546e220809b47bb32368
Dragoon Dorise <rodrigosedano@me.com>
2024-11-12 13:23:53 +0100

    PID

--- functions/generateGameLists.sh
+++ functions/generateGameLists.sh
@@ -51,19 +51,7 @@ generateGameLists_artwork() {
     local PIDFILE="$HOME/.config/EmuDeck/retrolibrary_$number_log.pid"
 
     if [ -f "$PIDFILE" ]; then
-      PID=$(cat "$PIDFILE")
-      ps -p "$PID" > /dev/null 2>&1
-      if [ $? -eq 0 ]; then
-        echo "Process already running"
-        exit 1
-      else
-        ## Process not found assume not running
-        echo $$ > "$PIDFILE"
-        if [ $? -ne 0 ]; then
-          echo "Could not create PID file"
-          exit 1
-        fi
-      fi
+      exit 1
     else
       echo $$ > "$PIDFILE"
       if [ $? -ne 0 ]; then
