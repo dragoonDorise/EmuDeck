97a9018aaf67dddef5904121dea6f11b7e555b52
Dragoon Dorise <rodrigosedano@me.com>
2024-11-13 15:19:23 +0100

    new parser retrolibrary

---
+++ tools/retro-library/download_art.py
@@ -0,0 +1,76 @@
+import json
+import requests
+import os
+import sys
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
+# Path del JSON y carpeta de destino desde los argumentos
+save_folder = sys.argv[1]
+number = sys.argv[2]
+json_path = os.path.expanduser(f'~/emudeck/cache/missing_artwork{number}.json')
+
+def create_empty_image(name, save_folder):
+    # Crear la carpeta si no existe
+    os.makedirs(save_folder, exist_ok=True)
+    # Definir la ruta de guardado para el archivo vacío
+    img_path = os.path.join(save_folder, f"{name}.jpg")
+
+    # Crear un archivo vacío
+    with open(img_path, 'wb') as file:
+        pass  # No escribimos nada para que quede vacío
+    print(f"Archivo vacío creado para {name}")
+
+def download_image(name, img_url, save_folder):
+    # Crear la carpeta si no existe
+    os.makedirs(save_folder, exist_ok=True)
+    # Definir la ruta de guardado
+    img_path = os.path.join(save_folder, f"{name}.jpg")
+
+    # Descargar y guardar la imagen
+    response = requests.get(img_url)
+    if response.status_code == 200:
+        with open(img_path, 'wb') as file:
+            file.write(response.content)
+        print(f"Imagen guardada como {img_path}")
+    else:
+        print(f"Error al descargar la imagen para {name}")
+
+def fetch_image_data(game):
+    name = game['name']
+    url = f"https://bot.emudeck.com/steamdbimg.php?name={name}"
+
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            img_url = data.get('img')  # Usar get para evitar errores si 'img' no existe o es None
+            if img_url:
+                download_image(name, img_url, save_folder)
+            else:
+                print(f"No se encontró una URL de imagen válida para {name}. Creando archivo vacío.")
+                create_empty_image(name, save_folder)
+        else:
+            print(f"Error al llamar al servicio para {name}")
+            create_empty_image(name, save_folder)
+    except requests.RequestException as e:
+        print(f"Excepción al procesar {name}: {e}")
+        create_empty_image(name, save_folder)
+
+def process_json(save_folder):
+    # Leer el JSON original
+    with open(json_path, 'r') as file:
+        games = json.load(file)
+
+    # Usar ThreadPoolExecutor para descargar en paralelo
+    with ThreadPoolExecutor(max_workers=15) as executor:
+        futures = [executor.submit(fetch_image_data, game) for game in games]
+        for future in as_completed(futures):
+            future.result()  # Esperar a que cada tarea termine
+
+if __name__ == "__main__":
+    # Verificar que se pasaron los argumentos necesarios
+    if len(sys.argv) != 3:
+        print("Uso: python script.py <carpeta_destino> <número>")
+        sys.exit(1)
+
+    process_json(save_folder)
---
+++ tools/retro-library/missing_artwork.py
@@ -0,0 +1,113 @@
+import os
+import json
+import sys
+import re
+
+def generate_game_lists(roms_path, images_path, number):
+    def collect_game_data(system_dir, extensions):
+        game_data = []
+        for root, _, files in os.walk(system_dir):
+            for file in files:
+                file_path = os.path.join(root, file)
+                if os.path.islink(file_path):
+                    continue
+
+                filename = os.path.basename(file)
+                extension = filename.split('.')[-1]
+                name = '.'.join(filename.split('.')[:-1])
+                if extension in extensions:
+                    if "wiiu" in system_dir:
+                        if extension == "wux":
+                            name = name
+                        elif extension == "wua":
+                            name = name
+                        else:
+                            parts = root.split(os.sep)
+                            if len(parts) >= 2:
+                                name = parts[-2]
+                            else:
+                                name = name
+
+                            if name == "roms":
+                               name = name
+
+                            if name == "wiiu":
+                               name = parts[-1]
+
+                    if "ps3" in system_dir:
+                            parts = root.split(os.sep)
+                            if len(parts) >= 3:
+                                name = parts[-3]
+                            else:
+                                name = name
+
+                            if name == "roms":
+                               name = name
+
+                            if name == "ps3":
+                               name = parts[-1]
+
+                    platform = os.path.basename(system_dir)
+
+                    name_cleaned = re.sub(r'\(.*?\)', '', name)
+                    name_cleaned = re.sub(r'\[.*?\]', '', name_cleaned)
+                    name_cleaned = name_cleaned.strip()
+                    name_cleaned = name_cleaned.replace(' ', '_').replace('-', '_')
+                    name_cleaned = re.sub(r'_+', '_', name_cleaned)
+                    name_cleaned = name_cleaned.replace('+', '').replace('&', '').replace('!', '').replace("'", '').replace('.', '')
+
+                    game_img = f"/customimages/emudeck/{name_cleaned}.jpg"
+
+                    # Verificar si la imagen existe en el images_path
+
+                    img_path = os.path.join(images_path, f"{name_cleaned}.jpg")
+                    #print(img_path)
+                    if not os.path.exists(img_path):
+                        game_info = {
+                            "name": name_cleaned,
+                            "platform": platform
+                        }
+                        game_data.append(game_info)
+
+        game_data_sorted = sorted(game_data, key=lambda x: x['name'])
+        return game_data_sorted
+
+    roms_dir = roms_path
+    valid_system_dirs = []
+
+    for system_dir in os.listdir(roms_dir):
+        if system_dir == "xbox360":
+            system_dir = "xbox360/roms"
+        full_path = os.path.join(roms_dir, system_dir)
+        if os.path.isdir(full_path) and not os.path.islink(full_path) and os.path.isfile(os.path.join(full_path, 'metadata.txt')):
+            file_count = sum([len(files) for r, d, files in os.walk(full_path) if not os.path.islink(r)])
+            if file_count > 2:
+                valid_system_dirs.append(full_path)
+
+    game_list = []
+
+    for system_dir in valid_system_dirs:
+        if any(x in system_dir for x in ["/model2", "/genesiswide", "/mame", "/emulators", "/desktop"]):
+            continue
+
+        with open(os.path.join(system_dir, 'metadata.txt')) as f:
+            metadata = f.read()
+        extensions = next((line.split(':')[1].strip().replace(',', ' ') for line in metadata.splitlines() if line.startswith('extensions:')), '').split()
+
+        games = collect_game_data(system_dir, extensions)
+        if games:
+            game_list.extend(games)
+
+    json_output = json.dumps(game_list, indent=4)
+    home_directory = os.path.expanduser("~")
+    output_file = os.path.join(home_directory, 'emudeck', 'cache', f'missing_artwork{number}.json')
+    os.makedirs(os.path.dirname(output_file), exist_ok=True)
+    with open(output_file, 'w') as f:
+        f.write(json_output)
+
+# Pasar la ruta de las ROMs y de las imágenes desde los argumentos de línea de comandos
+roms_path = sys.argv[1]
+images_path = sys.argv[2]
+number = sys.argv[3]
+
+generate_game_lists(roms_path, images_path, number)
