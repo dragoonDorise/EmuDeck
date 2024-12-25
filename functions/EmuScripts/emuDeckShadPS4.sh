9cd9cefce0dc3742cf0010996a11db4996ac0097
Dragoon Dorise <rodrigosedano@me.com>
2024-12-25 23:35:34 +0100

    shadPS4 cleanup

--- functions/EmuScripts/emuDeckShadPS4.sh
+++ functions/EmuScripts/emuDeckShadPS4.sh
@@ -2,103 +2,13 @@
 
 # Script to install, initialize and configure ShadPS4 on EmuDeck
 # Note: No Bios/Keys symlinks necessary
-
-# External helper functions (defined outside this script)
-#- installEmuBI()
-#- getReleaseURLGH()
-#- configEmuAI()
-#- linkToSaveFolder()
-#- uninstallGeneric()
-#- migrateAndLinkConfig()
-#- flushEmulatorLaunchers()
-#- setMSG()
-
 # Variables
+
 ShadPS4_emuName="ShadPS4"
 ShadPS4_emuType="$emuDeckEmuTypeAppImage"
 ShadPS4_emuPath="$HOME/Applications"
 ShadPS4_dir="$HOME/.local/share/shadPS4"
 ShadPS4_configFile="$ShadPS4_dir/config.toml"
-
-
-# Language keys using [ISO 639-1: Language codes] & [ISO 3166-1 alpha-2: Country codes]
-# NOTE: Keep in sync with https://github.com/shadps4-emu/shadPS4/tree/main/src/qt_gui/translations
-# even though project still just uses some two character codes e.g. 'nl' instead of 'nl_NL'
-declare -A ShadPS4_languages
-ShadPS4_languages=(
-    ["ar"]="Arabic"
-    ["da_DK"]="Danish"
-    ["de"]="German Deutsch"
-    ["el"]="Greek"
-    ["el_GR"]="Greek"
-    ["en"]="English"
-    ["en_US"]="English (US)"
-    ["en_IE"]="English (Irish)"
-    ["es_ES"]="Spanish"
-    ["fa_IR"]="Farsi (Iran)"
-    ["fi"]="Finnish"
-    ["fi_FI"]="Finnish"
-    ["fr"]="French"
-    ["fr_FR"]="French"
-    ["hu_HU"]="Hungarian"
-    ["id"]="Indonesian"
-    ["it"]="Italian"
-    ["ja_JP"]="Japanese"
-    ["ko_KR"]="Korean"
-    ["lt_LT"]="Lithuanian"
-    ["nb"]="Norwegian Bokmål"
-    ["nl"]="Dutch"
-    ["nl_NL"]="Dutch (Netherlands)"
-    ["pl_PL"]="Polish"
-    ["pt_BR"]="Portuguese"
-    ["ro_RO"]="Romanian"
-    ["ru_RU"]="Russian"
-    ["sq"]="Albanian"
-    ["ti_ER"]="Tigrinya"
-    ["tr_TR"]="Turkish"
-    ["uk_UA"]="Ukrainian"
-    ["vi_VN"]="Vietnamese"
-    ["zh_CN"]="Chinese (Simplified)"
-    ["zh_TW"]="Traditional Chinese (Taiwan)"
-)
-
-declare -A ShadPS4_regions
-ShadPS4_regions=(
-    ["ar"]="Arabic"
-    ["da_DK"]="Denmark"
-    ["de"]="Deutsch"
-    ["el"]="Greece"
-    ["el_GR"]="Greece"
-    ["en"]="Global English"
-    ["en_US"]="United States"
-    ["en_IE"]="Ireland"
-    ["es_ES"]="Spain"
-    ["fa_IR"]="Iran"
-    ["fi"]="Finland"
-    ["fi_FI"]="Finland"
-    ["fr"]="France"
-    ["fr_FR"]="France"
-    ["hu_HU"]="Hungary"
-    ["id"]="Indonesia"
-    ["it"]="Italian"
-    ["ja_JP"]="Japan"
-    ["ko_KR"]="South Korea"
-    ["lt_LT"]="Lithuania"
-    ["nb"]="Norway"
-    ["nl"]="Netherlands"
-    ["nl_NL"]="Netherlands"
-    ["pl_PL"]="Poland"
-    ["pt_BR"]="Brazil"
-    ["ro_RO"]="Romania"
-    ["ru_RU"]="Russia"
-    ["sq"]="Albania"
-    ["ti_ER"]="Eritrea"
-    ["tr_TR"]="Turkey"
-    ["uk_UA"]="Ukraine"
-    ["vi_VN"]="Vietnam"
-    ["zh_CN"]="China"
-    ["zh_TW"]="Taiwan"
-)
 
 ShadPS4_cleanup(){
     echo "Begin ShadPS4 Cleanup"
