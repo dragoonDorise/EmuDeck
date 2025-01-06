4c9ad9b07e52869219f8bf8ec6a6f45b43d0239b
Dragoon Dorise <rodrigosedano@me.com>
2025-01-06 21:15:22 +0100

    fix " '

--- tools/retro-library/download_art_nohash.py
+++ tools/retro-library/download_art_nohash.py
@@ -9,10 +9,12 @@ import subprocess
 def getSettings():
     pattern = re.compile(r'([A-Za-z_][A-Za-z0-9_]*)=(.*)')
     user_home = os.path.expanduser("~")
+
     if os.name == 'nt':
         config_file_path = os.path.join(user_home, 'emudeck', 'settings.ps1')
     else:
         config_file_path = os.path.join(user_home, 'emudeck', 'settings.sh')
+
     configuration = {}
 
     with open(config_file_path, 'r') as file:
@@ -20,13 +22,16 @@ def getSettings():
             match = pattern.search(line)
             if match:
                 variable = match.group(1)
-                value = match.group(2).strip('"')
-                configuration[variable] = value
+                value = match.group(2).strip().strip('"')
+                expanded_value = os.path.expandvars(value.replace('"', '').replace("'", ""))
+                configuration[variable] = expanded_value
 
+    # Obtener rama actual del repositorio backend
     if os.name == 'nt':
-        bash_command = f"cd {appdata_roaming_path}/EmuDeck/backend/ && git rev-parse --abbrev-ref HEAD"
+        bash_command = f"cd {os.path.join(user_home, 'AppData', 'Roaming', 'EmuDeck', 'backend')} && git rev-parse --abbrev-ref HEAD"
     else:
         bash_command = "cd $HOME/.config/EmuDeck/backend/ && git rev-parse --abbrev-ref HEAD"
+
     result = subprocess.run(bash_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
     configuration["branch"] = result.stdout.strip()
 
