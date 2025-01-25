755bb7c092afa62183445cda4e2c61d2dec786bc
Dragoon Dorise <rodrigosedano@me.com>
2025-01-26 00:38:22 +0100

    fix rom path bigpemu

--- functions/EmuScripts/emuDeckBigPEmu.sh
+++ functions/EmuScripts/emuDeckBigPEmu.sh
@@ -51,6 +51,8 @@ BigPEmu_install(){
 BigPEmu_init(){
 	setMSG "Initializing $BigPEmu_emuName settings."
 	rsync -avhp "$emudeckBackend/configs/bigpemu/" "$BigPEmu_appData" --backup --suffix=.bak
+	sed -E -i "s|/run/media/mmcblk0p1/Emulation|$emulationPath|g" "$BigPEmu_BigPEmuSettings"
+
 	BigPEmu_setEmulationFolder
 	BigPEmu_setupSaves
 	BigPEmu_flushEmulatorLauncher
@@ -67,6 +69,8 @@ BigPEmu_init(){
 BigPEmu_update(){
 	setMSG "Updating $BigPEmu_emuName settings."
 	rsync -avhp "$emudeckBackend/configs/bigpemu/" "$BigPEmu_appData" --ignore-existing
+	sed -E -i "s|/run/media/mmcblk0p1/Emulation|$emulationPath|g" "$BigPEmu_BigPEmuSettings"
+
 	BigPEmu_setEmulationFolder
 	BigPEmu_setupSaves
 	BigPEmu_flushEmulatorLauncher
