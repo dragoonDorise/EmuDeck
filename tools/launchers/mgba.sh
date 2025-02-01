0d82fb65759b0bc2ead83aa70a99ce8a8d4d921b
SilentException <mitja.skuver@gmail.com>
2025-02-01 03:30:21 +0100

    thank you shellcheck - fix for double quoting array expansions

--- tools/launchers/mgba.sh
+++ tools/launchers/mgba.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 . "$HOME/.config/EmuDeck/backend/functions/all.sh"
-emulatorInit "mgba" "io.mgba.mGBA" "" "mGBA*.AppImage" "--" ${@}
+emulatorInit "mgba" "io.mgba.mGBA" "" "mGBA*.AppImage" "--" "${@}"
 rm -rf "$savesPath/.gaming"
--- tools/launchers/pcsx2-qt.sh
+++ tools/launchers/pcsx2-qt.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 . "$HOME/.config/EmuDeck/backend/functions/all.sh"
-emulatorInit "pcsx2" "net.pcsx2.PCSX2" "" "pcsx2-Qt*.AppImage" "--" ${@}
+emulatorInit "pcsx2" "net.pcsx2.PCSX2" "" "pcsx2-Qt*.AppImage" "--" "${@}"
 rm -rf "$savesPath/.gaming"
