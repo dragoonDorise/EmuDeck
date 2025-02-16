cb584e94232ba045e288c2ae2a3d00ec87aa9a32
Dragoon Dorise <rodrigosedano@me.com>
2025-02-16 10:49:29 +0100

    no ask for pass

--- functions/ToolScripts/emuDeckPlugins.sh
+++ functions/ToolScripts/emuDeckPlugins.sh
@@ -16,17 +16,7 @@ Plugins_checkPassword(){
    local password=$1
    if [ "$password" = "EmuDecky!" ]; then ## AppImage install when no sudo
       #We create the password
-      yes "$password" | passwd $(whoami) &>/dev/null || {
-        read -r PASS <<< $(zenity --title="Decky Installer" --width=300 --height=100 --entry --hide-text --text="Enter your sudo/admin password so we can install Decky with the best plugins for emulation")
-        if [[ $? -eq 1 ]] || [[ $? -eq 5 ]]; then
-          exit 1
-        fi
-        if ( echo "$PASS" | sudo -S -k true ); then
-          password=$PASS
-        else
-          zenity --title="Decky Installer" --width=150 --height=40 --info --text "Incorrect Password"
-        fi
-      }
+      yes "$password" | passwd $(whoami) &>/dev/null
    elif [ "$system" == "chimeraos" ]; then
     password="gamer"
    elif [ "$system" == "bazzite" ]; then
