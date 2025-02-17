fc0ebb882088afd0c2a6e147c827c0ed196477eb
Dragoon Dorise <rodrigosedano@me.com>
2025-02-17 08:36:21 +0100

    paths updated

--- functions/ToolScripts/emuDeckStore.sh
+++ functions/ToolScripts/emuDeckStore.sh
@@ -8,9 +8,15 @@ Store_installGame(){
     filename=$(basename "$url")
     name="${filename%.*}"
 
-    wget -O "${romsPath}/${system}/${name}.zip" "${url}" && \
-    wget -O "${storagePath}/retrolibrary/artwork/${system}/media/screenshot/${name}.jpg" "https://raw.githubusercontent.com/EmuDeck/emudeck-homebrew/main/downloaded_media/${system}/screenshots/homebrew/${name}.png" && \
-    wget -O "${storagePath}/retrolibrary/artwork/${system}/media/box2dfront/${name}.jpg" "https://raw.githubusercontent.com/EmuDeck/emudeck-homebrew/main/downloaded_media/${system}/titlescreens/homebrew/${name}.png" && \
+    name_cleaned=$(echo "$name" | sed -E 's/\(.*?\)//g' | sed -E 's/\[.*?\]//g')
+    name_cleaned=$(echo "$name_cleaned" | tr ' ' '_' | tr '-' '_')
+    name_cleaned=$(echo "$name_cleaned" | sed -E 's/_+/_/g')
+    name_cleaned=$(echo "$name_cleaned" | tr -d '+&!'\''.' | sed 's/_decrypted//g' | sed 's/decrypted//g' | sed 's/.ps3//g')
+    name_cleaned=$(echo "$name_cleaned" | tr '[:upper:]' '[:lower:]')
+
+    wget -O "${romsPath}/${system}/${name}.zip" "https://f005.backblazeb2.com/file/emudeck-store/${system}/${name}.zip" && \
+    wget -O "${storagePath}/retrolibrary/artwork/${system}/media/screenshot/${name}.jpg" "https://raw.githubusercontent.com/EmuDeck/emudeck-homebrew/main/downloaded_media/${system}/screenshots/homebrew/${name_cleaned}.png" && \
+    wget -O "${storagePath}/retrolibrary/artwork/${system}/media/box2dfront/${name}.jpg" "https://raw.githubusercontent.com/EmuDeck/emudeck-homebrew/main/downloaded_media/${system}/titlescreens/homebrew/${name_cleaned}.png" && \
     echo "true" || echo "false"
 }
 
@@ -22,9 +28,16 @@ Store_uninstallGame(){
     filename=$(basename "$url")
     name="${filename%.*}"
 
+    name_cleaned=$(echo "$name" | sed -E 's/\(.*?\)//g' | sed -E 's/\[.*?\]//g')
+    name_cleaned=$(echo "$name_cleaned" | tr ' ' '_' | tr '-' '_')
+    name_cleaned=$(echo "$name_cleaned" | sed -E 's/_+/_/g')
+    name_cleaned=$(echo "$name_cleaned" | tr -d '+&!'\''.' | sed 's/_decrypted//g' | sed 's/decrypted//g' | sed 's/.ps3//g')
+    name_cleaned=$(echo "$name_cleaned" | tr '[:upper:]' '[:lower:]')
+
+
     rm -rf "${romsPath}/${system}/${name}.zip" && \
-    rm -rf  "${storagePath}/retrolibrary/artwork/${system}/media/screenshot/${name}.jpg" && \
-    "${storagePath}/retrolibrary/artwork/${system}/media/box2dfront/${name}.jpg" && echo "true" || echo "false"
+    rm -rf  "${storagePath}/retrolibrary/artwork/${system}/media/screenshot/${name_cleaned}.jpg" && \
+    "${storagePath}/retrolibrary/artwork/${system}/media/box2dfront/${name_cleaned}.jpg" && echo "true" || echo "false"
 
 }
 
