8f6f66216032eb71fce9602f7b878ab365300d86
Dragoon Dorise <rodrigosedano@me.com>
2025-02-25 14:57:38 +0100

    updated dl

--- functions/ToolScripts/emuDeckStore.sh
+++ functions/ToolScripts/emuDeckStore.sh
@@ -11,9 +11,9 @@ Store_installGame(){
     name_cleaned=$(echo "$name_cleaned" | tr -d '+&!'\''.' | sed 's/_decrypted//g' | sed 's/decrypted//g' | sed 's/.ps3//g')
     name_cleaned=$(echo "$name_cleaned" | tr '[:upper:]' '[:lower:]')
 
-    wget -O "${romsPath}/${system}/${name}.zip" "https://f005.backblazeb2.com/file/emudeck-store/${system}/${name}.zip" && \
-    wget -O "${storagePath}/retrolibrary/artwork/${system}/media/screenshot/${name_cleaned}.jpg" "https://f005.backblazeb2.com/file/emudeck-store/${system}/media/screenshot/${name}.png" && \
-    wget -O "${storagePath}/retrolibrary/artwork/${system}/media/box2dfront/${name_cleaned}.jpg" "https://f005.backblazeb2.com/file/emudeck-store/${system}/media/box2dfront/${name}.png" && \
+    wget -O "${romsPath}/${system}/${name}.zip" "${url}" && \
+    wget -O "${storagePath}/retrolibrary/artwork/${system}/media/screenshot/${name_cleaned}.jpg" "https://f005.backblazeb2.com/file/emudeck-store/artwork/${system}/media/screenshot/${name}.png" && \
+    wget -O "${storagePath}/retrolibrary/artwork/${system}/media/box2dfront/${name_cleaned}.jpg" "https://f005.backblazeb2.com/file/emudeck-store/artwork/${system}/media/box2dfront/${name}.png" && \
     echo "true" || echo "false"
 }
 
