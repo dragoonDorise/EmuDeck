a17e312c7ad9b6655d70552f949417f448696f42
Angel of Woe <jaia.one.winged.angel@gmail.com>
2025-03-04 12:32:44 -0500

    Switch to universal shebang. Disable SC2034. Prevent some globbing and expansion. Should $cs_user have a '/' after it?

--- cloudSyncHealth.sh
+++ cloudSyncHealth.sh
@@ -1,6 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+# shellcheck disable=2034
+
 clear
-. "$HOME/.config/EmuDeck/backend/functions/all.sh"
+# shellcheck disable=1091
+. "${HOME}/.config/EmuDeck/backend/functions/all.sh"
 
 NONE='\033[00m'
 RED='\033[01;31m'
@@ -12,77 +16,81 @@ WHITE='\033[01;37m'
 BOLD='\033[1m'
 UNDERLINE='\033[4m'
 BLINK='\x1b[5m'
-cloud_sync_upload_test(){
-	local emuName=$1
 
-	if [ ! -d $savesPath/$emuName ];then
+cloud_sync_upload_test(){
+	local emuName="${1}"
+
+	# shellcheck disable=2154
+	if [ ! -d "${savesPath}/${emuName}" ];then
 		return 2
 	fi
 
-	echo "test" > "$savesPath/$emuName/.temp"
-	filePath="$savesPath/$emuName/.temp"
-	"$cloud_sync_bin"  -q  copyto --fast-list --checkers=50 --transfers=50 --low-level-retries 1 --retries 1 "$filePath" "$cloud_sync_provider":"$cs_user"Emudeck/saves/$emuName/.temp  && rm -rf "$savesPath/$emuName/.temp" && return 0 || return 1
+	echo "test" > "${savesPath}/${emuName}/.temp"
+	filePath="${savesPath}/${emuName}/.temp"
+	# shellcheck disable=2154
+	"${cloud_sync_bin}"  -q  copyto --fast-list --checkers=50 --transfers=50 --low-level-retries 1 --retries 1 "${filePath}" "${cloud_sync_provider}":"${cs_user}Emudeck/saves/${emuName}/.temp"  && rm -rf "${savesPath}/${emuName}/.temp" && return 0 || return 1
 }
 
 cloud_sync_dowload_test(){
-	local emuName=$1
+	local emuName="${1}"
 
-	if [ ! -d $savesPath/$emuName ];then
+	if [ ! -d "${savesPath}/${emuName}" ];then
 		return 2
 	fi
 
-	echo "test" > "$savesPath/$emuName/.temp"
-	filePath="$savesPath/$emuName/.temp"
-	"$cloud_sync_bin"  -q  copyto --fast-list --checkers=50 --transfers=50 --low-level-retries 1 --retries 1 "$cloud_sync_provider":"$cs_user"Emudeck/saves/$emuName/.temp "$filePath" && rm -rf "$savesPath/$emuName/.temp" && return 0 || return 1
+	echo "test" > "${savesPath}/${emuName}/.temp"
+	filePath="${savesPath}/${emuName}/.temp"
+	"${cloud_sync_bin}"  -q  copyto --fast-list --checkers=50 --transfers=50 --low-level-retries 1 --retries 1 "${cloud_sync_provider}":"${cs_user}Emudeck/saves/${emuName}/.temp" "$filePath" && rm -rf "${savesPath}/${emuName}/.temp" && return 0 || return 1
 
 }
 
 echo -e "${CYAN}CloudSync Status Report${NONE}"
 echo ""
 
-miArray=("Cemu" "citra" "dolphin" "duckstation" "MAME" "melonds" "mgba" "pcsx2" "ppsspp" "primehack" "retroarch" "rpcs3" "scummvm" "Vita3K" "yuzu" "ryujinx" )
+miArray=( "Cemu" "citra" "dolphin" "duckstation" "MAME" "melonds" "mgba" "pcsx2" "ppsspp" "primehack" "retroarch" "rpcs3" "scummvm" "Vita3K" "yuzu" "ryujinx" )
 
 upload="true"
 download="true"
 launchers="true"
 echo -e "${YELLOW}Checking launchers${NONE}"
-for entry in "$toolsPath/launchers/"*.sh
+# shellcheck disable=2154
+for entry in "${toolsPath}/launchers/"*.sh
 do
 	if [ -f "$entry" ]; then
-		if grep -q "cloud_sync_startService" $entry; then
-			echo -e "$entry: ${GREEN}Success${NONE}"
+		if grep -q "cloud_sync_startService" "${entry}"; then
+			echo -e "${entry}: ${GREEN}Success${NONE}"
 		else
-			echo -e "$entry: ${RED}Failure${NONE}"
+			echo -e "${entry}: ${RED}Failure${NONE}"
 			launchers="false"
 		fi
 	fi
 done
 
-if grep -q "cloud_sync_startService" "$toolsPath/launchers/esde/emulationstationde.sh"; then
-	echo -e "$toolsPath/launchers/esde/emulationstationde.sh: ${GREEN}Success${NONE}"
+if grep -q "cloud_sync_startService" "${toolsPath}/launchers/esde/emulationstationde.sh"; then
+	echo -e "${toolsPath}/launchers/esde/emulationstationde.sh: ${GREEN}Success${NONE}"
 else
-	echo -e "$toolsPath/launchers/esde/emulationstationde.sh: ${RED}Failure${NONE}"
+	echo -e "${toolsPath}/launchers/esde/emulationstationde.sh: ${RED}Failure${NONE}"
 fi
 
 echo -e "${YELLOW}Checking for Windows old .lnk files${NONE}"
-find "$savesPath" -type f -name "*.lnk" | while read -r entry
+find "${savesPath}" -type f -name "*.lnk" | while read -r entry
 do
-	rm -rf $entry
-	echo "found and deleted: $entry"
+	rm -rf "${entry}"
+	echo "found and deleted: ${entry}"
 done
 
 found_files="false"
 
-for entry in "$savesPath"/**/*.lnk
+for entry in "${savesPath}"/**/*.lnk
 do
 	if [ -f "$entry" ]; then
-		rm -rf $entry
-		echo "found and deleted: $entry"
+		rm -rf "${entry}"
+		echo "found and deleted: ${entry}"
 		found_files="true"
 	fi
 done
 
-if [ "$found_files" = "false" ]; then
+if [ "${found_files}" = "false" ]; then
 	echo "No files with the '.lnk' extension found."
 fi
 
@@ -90,8 +98,8 @@ echo -e ""
 echo -e "${YELLOW}Testing uploading${NONE}"
 # Recorrer el array y ejecutar la función cloud_sync_upload_test para cada elemento
 for elemento in "${miArray[@]}"; do
-	echo -ne "Testing $elemento upload..."
-	if cloud_sync_upload_test $elemento;then
+	echo -ne "Testing ${elemento} upload..."
+	if cloud_sync_upload_test "${elemento}"; then
 		echo -e "${GREEN}Success${NONE}"
 	elif [ $? = 2 ]; then
 		echo -e "${YELLOW}Save folder not found${NONE}"
@@ -104,8 +112,8 @@ echo ""
 echo -e "${YELLOW}Testing downloading${NONE}"
 # Recorrer el array y ejecutar la función cloud_sync_upload_test para cada elemento
 for elemento in "${miArray[@]}"; do
-	echo -ne "Testing $elemento download..."
-	if cloud_sync_dowload_test $elemento;then
+	echo -ne "Testing ${elemento} download..."
+	if cloud_sync_dowload_test "${elemento}";then
 		echo -e "${GREEN}Success${NONE}"
 	elif [ $? = 2 ]; then
 		echo -e "${YELLOW}Save folder not found${NONE}"
