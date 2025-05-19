2e88d78403d4c2380232e953bedec3ab2f5f9354
Dragoon Dorise <rodrigosedano@me.com>
2025-05-19 18:17:35 +0200

    app init

--- functions/app_init.py
+++ functions/app_init.py
@@ -1,4 +1,10 @@
-from core.all import *
+from functions.env import generate_python_env
+generate_python_env()
 
 def app_init():
-    print("Init")
+    install_pip("vdf")
+    install_pip("requests")
+    install_pip("screeninfo")
+    install_pip("py7zr")
+    install_pip("PySide6")
+    install_pip("pygame")
