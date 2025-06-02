81735ab8b0c3d79c2abf14a1d88ddea184282990
Dragoon Dorise <rodrigosedano@me.com>
2025-06-02 14:08:28 +0200

    emudecky

--- functions/tools_scripts/emudeck_decky.py
+++ functions/tools_scripts/emudeck_decky.py
@@ -2,14 +2,14 @@ from core.all import *
 
 #Wrappers for old functions names
 
-def Decky_autoSave():
+def decky_autoSave():
     retroarch_auto_save()
     if settings.autosave == False:
         set_setting("autosave",True)
     else:
         set_setting("autosave",False)
 
-def Decky_bezels():
+def decky_bezels():
     if settings.bezels == False:
         retroarch_bezel_on_all()
         set_setting("bezels",True)
@@ -17,7 +17,7 @@ def Decky_bezels():
         retroarch_bezel_off_all()
         set_setting("bezels",False)
 
-def Decky_shaders_LCD():
+def decky_shaders_LCD():
     if settings.shaders.handhelds == False:
         retroarch_matrix_shaders_on_all()
         set_setting("shaders.handhelds",True)
@@ -25,7 +25,7 @@ def Decky_shaders_LCD():
         retroarch_matrix_shaders_on_all()
         set_setting("shaders.handhelds",False)
 
-def Decky_shaders_2D():
+def decky_shaders_2D():
     if settings.shaders.classic == False:
         retroarch_crt_shader_on_all()
         set_setting("shaders.classic",True)
@@ -33,7 +33,7 @@ def Decky_shaders_2D():
         retroarch_crt_shader_off_all()
         set_setting("shaders.classic",False)
 
-def Decky_shaders_3D():
+def decky_shaders_3D():
     if settings.shaders.classic3d == False:
         retroarch_3d_crt_shader_on_all()
         set_setting("shaders.classic3d",True)
@@ -47,9 +47,22 @@ def Dolphin_setCustomizations():
 def RetroArch_setCustomizations():
     retroarch_set_customizations()
 
-def Decky_setAR():
+def decky_setAR():
     retroarch_set_customizations()
     xemu_widescreen()
     duckstation_widescreen()
     #PCSX2QT_setCustomizations
     #Dolphin_setCustomizations
+
+def decky_netplay():
+    if settings.netPlay == False:
+        set_setting("netPlay",True)
+    else:
+        set_setting("netPlay",False)
+
+def decky_cloud_sync_status():
+    if settings.decky_cloud_sync_status == False:
+        set_setting("decky_cloud_sync_status",True)
+    else:
+        set_setting("decky_cloud_sync_status",False)
+
