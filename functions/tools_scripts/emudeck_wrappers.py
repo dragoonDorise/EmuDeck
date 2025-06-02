86ebb411cd3a745c5284e1af674e9ef01429b62b
Dragoon Dorise <rodrigosedano@me.com>
2025-06-02 14:34:23 +0200

    emudeck_backend

--- functions/tools_scripts/emudeck_wrappers.py
+++ functions/tools_scripts/emudeck_wrappers.py
@@ -2,6 +2,6 @@ from core.all import *
 
 def wrappers_install():
     sh_path_dest=f"{tools_path}/wrappers/update-emulators.sh"
-    shutil.copy2(f"{emudeck_folder}/tools/wrappers/update-emulators.sh", sh_path_dest)
+    shutil.copy2(f"{emudeck_backend}/tools/wrappers/update-emulators.sh", sh_path_dest)
     current_mode = sh_path_dest.stat().st_mode
     sh_path_dest.chmod(current_mode | stat.S_IXUSR | stat.S_IXGRP | stat.S_IXOTH)
