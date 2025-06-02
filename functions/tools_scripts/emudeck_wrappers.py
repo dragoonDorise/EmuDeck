853e897c06bdfc242a82dab5896a124690835cda
Dragoon Dorise <rodrigosedano@me.com>
2025-06-02 14:52:02 +0200

    wrappers_install

--- functions/tools_scripts/emudeck_wrappers.py
+++ functions/tools_scripts/emudeck_wrappers.py
@@ -1,7 +1,15 @@
 from core.all import *
 
-def wrappers_install():
-    sh_path_dest=Path(f"{tools_path}/wrappers/update-emulators.sh")
-    shutil.copy2(f"{emudeck_backend}/tools/wrappers/update-emulators.sh", sh_path_dest)
+def wrappers_do_install(script):
+    sh_path_dest=Path(f"{tools_path}/wrappers/{script}")
+    shutil.copy2(f"{emudeck_backend}/tools/wrappers/{script}", sh_path_dest)
     current_mode = sh_path_dest.stat().st_mode
     sh_path_dest.chmod(current_mode | stat.S_IXUSR | stat.S_IXGRP | stat.S_IXOTH)
+
+
+
+def wrappers_install():
+    #Update Emus
+    wrappers_do_install("cloud_sync_force_download.sh")
+    #CloudSync
+    wrappers_do_install("cloud_sync_force_upload.sh")
