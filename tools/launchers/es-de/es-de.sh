3c605ff7d6db48978b40e446998f250e8b3fbefc
Dragoon Dorise <rodrigosedano@me.com>
2025-12-02 11:24:40 +0100

    cloud_sync_downloadESDE

--- tools/launchers/es-de/es-de.sh
+++ tools/launchers/es-de/es-de.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 . "$HOME/.config/EmuDeck/backend/functions/all.sh"
-cloud_sync_downloadEmuAll && cloud_sync_startService
+cloud_sync_downloadESDE && cloud_sync_startService
 "$ESDE_toolPath" "${@}"
 cloud_sync_uploadForced
 rm -rf "$savesPath/.gaming";
