8e9d4fe0a194a97e571d2e45899f73aaf1d818e4
Kabab33 <kabab33@kabab33.nl>
2026-04-11 18:06:07 +0200

    Fix path in tools/launchers/azahar.sh
    This expects an trailing slash which vars,sh does not contain

--- tools/launchers/azahar.sh
+++ tools/launchers/azahar.sh
@@ -2,7 +2,7 @@
 . "$HOME/.config/EmuDeck/backend/functions/all.sh"
 emulatorInit "azahar"
 emuName="azahar" #parameterize me
-emufolder="$emusFolder" # has to be applications for ES-DE to find it
+emufolder="$emusFolder/" # has to be applications for ES-DE to find it
 
 #initialize execute array
 exe=()
